4d3a9042728bf917d3910aba41a46f4b959f1eba
Theophilus Eriata <theoeriata@gmail.com>
2023-03-05 14:50:53 -0700

    Apply fixes to command encoder.

--- Source/Engine/RendererDiligent/CommandEncoder/CommandEncoderImplDiligent.h
+++ Source/Engine/RendererDiligent/CommandEncoder/CommandEncoderImplDiligent.h
@@ -180,6 +180,8 @@ private:
   Diligent::ITextureView* m_pBoundDepthStencilTarget                            = nullptr;
 
   Diligent::IBuffer*    m_pBoundVertexBuffers[XII_GAL_MAX_VERTEX_BUFFER_COUNT] = {};
-  Diligent::Uint64      m_VertexBufferStrides[XII_GAL_MAX_VERTEX_BUFFER_COUNT] = {};
   xiiGAL::ModifiedRange m_BoundVertexBuffersRange;
+
+  Diligent::Uint64 m_VertexBufferStrides[XII_GAL_MAX_VERTEX_BUFFER_COUNT] = {};
+  Diligent::Uint64 m_VertexBufferOffsets[XII_GAL_MAX_VERTEX_BUFFER_COUNT] = {};
 };
