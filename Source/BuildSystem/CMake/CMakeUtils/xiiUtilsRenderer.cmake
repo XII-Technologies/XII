e7452a9b12e5b76ac85c427c317788911d392b98
Theophilus Eriata <theoeriata@gmail.com>
2023-03-05 16:02:00 -0700

    Remove References To Renderer DX11 In Build System.

--- Source/BuildSystem/CMake/CMakeUtils/xiiUtilsRenderer.cmake
+++ Source/BuildSystem/CMake/CMakeUtils/xiiUtilsRenderer.cmake
@@ -3,9 +3,7 @@
 # #####################################
 
 macro(xii_requires_renderer)
-	if(XII_CMAKE_PLATFORM_WINDOWS)
-		xii_requires_d3d()
-	endif()
+	xii_requires_diligent()
 endmacro()
 
 # #####################################
@@ -14,14 +12,6 @@ endmacro()
 # #####################################
 
 function(xii_add_renderers TARGET_NAME)
-	if(XII_CMAKE_PLATFORM_WINDOWS)
-		target_link_libraries(${TARGET_NAME}
-			PRIVATE
-			RendererDX11
-		)
-		xii_link_target_dx11(${TARGET_NAME})
-	endif()
-	
 	if (XII_BUILD_DILIGENT)
 		target_link_libraries(${TARGET_NAME}
 			PRIVATE
