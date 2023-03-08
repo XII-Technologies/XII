3c8ac99112e123b437c696a3275a8865b3cc235f
XII Automation <theoeriata@gmail.com>
2023-03-08 15:04:06 +0000

    Apply Code Format Rules.

--- Source/Engine/ShaderCompiler/ThirdParty/dxcapi.h
+++ Source/Engine/ShaderCompiler/ThirdParty/dxcapi.h
@@ -152,7 +152,7 @@ CROSS_PLATFORM_UUIDOF(IDxcBlobEncoding, "7241d424-2646-4191-97c0-98e96e42fc68")
 struct IDxcBlobEncoding : public IDxcBlob
 {
 public:
-  virtual HRESULT STDMETHODCALLTYPE GetEncoding(_Out_ BOOL*   pKnown,
+  virtual HRESULT STDMETHODCALLTYPE GetEncoding(_Out_ BOOL* pKnown,
                                                 _Out_ UINT32* pCodePage) = 0;
 };
 
@@ -189,7 +189,7 @@ CROSS_PLATFORM_UUIDOF(IDxcIncludeHandler, "7f61fc7d-950d-467f-b3e3-3c02fb49187c"
 struct IDxcIncludeHandler : public IUnknown
 {
   virtual HRESULT STDMETHODCALLTYPE LoadSource(
-    _In_z_ LPCWSTR                           pFilename,      // Candidate filename.
+    _In_z_ LPCWSTR                pFilename,                 // Candidate filename.
     _COM_Outptr_result_maybenull_ IDxcBlob** ppIncludeSource // Resultant source object for included file, nullptr if not found.
     ) = 0;
 };
@@ -241,48 +241,48 @@ struct IDxcLibrary : public IUnknown
 {
   virtual HRESULT STDMETHODCALLTYPE SetMalloc(_In_opt_ IMalloc* pMalloc) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateBlobFromBlob(
-    _In_ IDxcBlob*          pBlob,
-    UINT32                  offset,
-    UINT32                  length,
+    _In_ IDxcBlob* pBlob,
+    UINT32         offset,
+    UINT32         length,
     _COM_Outptr_ IDxcBlob** ppResult) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateBlobFromFile(
-    _In_z_ LPCWSTR                  pFileName,
-    _In_opt_ UINT32*                codePage,
+    _In_z_ LPCWSTR pFileName,
+    _In_opt_ UINT32* codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateBlobWithEncodingFromPinned(
-    _In_bytecount_(size) LPCVOID    pText,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pText,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateBlobWithEncodingOnHeapCopy(
-    _In_bytecount_(size) LPCVOID    pText,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pText,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateBlobWithEncodingOnMalloc(
-    _In_bytecount_(size) LPCVOID    pText,
-    IMalloc*                        pIMalloc,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pText,
+    IMalloc*                     pIMalloc,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateIncludeHandler(
     _COM_Outptr_ IDxcIncludeHandler** ppResult) = 0;
   virtual HRESULT STDMETHODCALLTYPE CreateStreamFromBlobReadOnly(
-    _In_ IDxcBlob*         pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IStream** ppStream) = 0;
   virtual HRESULT STDMETHODCALLTYPE GetBlobAsUtf8(
-    _In_ IDxcBlob*                  pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
   // Renamed from GetBlobAsUtf16 to GetBlobAsWide
   virtual HRESULT STDMETHODCALLTYPE GetBlobAsWide(
-    _In_ IDxcBlob*                  pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
 #ifdef _WIN32
   // Alias to GetBlobAsWide on Win32
   inline HRESULT GetBlobAsUtf16(
-    _In_ IDxcBlob*                  pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding)
   {
     return this->GetBlobAsWide(pBlob, pBlobEncoding);
@@ -321,10 +321,10 @@ struct IDxcCompiler : public IUnknown
     _In_opt_count_(argCount) LPCWSTR* pArguments,     // Array of pointers to arguments
     _In_ UINT32                       argCount,       // Number of arguments
     _In_count_(defineCount)
-      const DxcDefine*                 pDefines,        // Array of defines
-    _In_ UINT32                        defineCount,     // Number of defines
-    _In_opt_ IDxcIncludeHandler*       pIncludeHandler, // user-provided interface to handle #include directives (optional)
-    _COM_Outptr_ IDxcOperationResult** ppResult         // Compiler output status, buffer, and errors
+      const DxcDefine* pDefines,                  // Array of defines
+    _In_ UINT32        defineCount,               // Number of defines
+    _In_opt_ IDxcIncludeHandler* pIncludeHandler, // user-provided interface to handle #include directives (optional)
+    _COM_Outptr_ IDxcOperationResult** ppResult   // Compiler output status, buffer, and errors
     ) = 0;
 
   // Preprocess source text
@@ -334,15 +334,15 @@ struct IDxcCompiler : public IUnknown
     _In_opt_count_(argCount) LPCWSTR* pArguments,  // Array of pointers to arguments
     _In_ UINT32                       argCount,    // Number of arguments
     _In_count_(defineCount)
-      const DxcDefine*                 pDefines,        // Array of defines
-    _In_ UINT32                        defineCount,     // Number of defines
-    _In_opt_ IDxcIncludeHandler*       pIncludeHandler, // user-provided interface to handle #include directives (optional)
-    _COM_Outptr_ IDxcOperationResult** ppResult         // Preprocessor output status, buffer, and errors
+      const DxcDefine* pDefines,                  // Array of defines
+    _In_ UINT32        defineCount,               // Number of defines
+    _In_opt_ IDxcIncludeHandler* pIncludeHandler, // user-provided interface to handle #include directives (optional)
+    _COM_Outptr_ IDxcOperationResult** ppResult   // Preprocessor output status, buffer, and errors
     ) = 0;
 
   // Disassemble a program.
   virtual HRESULT STDMETHODCALLTYPE Disassemble(
-    _In_ IDxcBlob*                  pSource,      // Program to disassemble.
+    _In_ IDxcBlob* pSource,                       // Program to disassemble.
     _COM_Outptr_ IDxcBlobEncoding** ppDisassembly // Disassembly text.
     ) = 0;
 };
@@ -360,12 +360,12 @@ struct IDxcCompiler2 : public IDxcCompiler
     _In_opt_count_(argCount) LPCWSTR* pArguments,     // Array of pointers to arguments
     _In_ UINT32                       argCount,       // Number of arguments
     _In_count_(defineCount)
-      const DxcDefine*                 pDefines,        // Array of defines
-    _In_ UINT32                        defineCount,     // Number of defines
-    _In_opt_ IDxcIncludeHandler*       pIncludeHandler, // user-provided interface to handle #include directives (optional)
-    _COM_Outptr_ IDxcOperationResult** ppResult,        // Compiler output status, buffer, and errors
-    _Outptr_opt_result_z_ LPWSTR*      ppDebugBlobName, // Suggested file name for debug blob. (Must be CoTaskMemFree()'d!)
-    _COM_Outptr_opt_ IDxcBlob**        ppDebugBlob      // Debug blob
+      const DxcDefine* pDefines,                   // Array of defines
+    _In_ UINT32        defineCount,                // Number of defines
+    _In_opt_ IDxcIncludeHandler* pIncludeHandler,  // user-provided interface to handle #include directives (optional)
+    _COM_Outptr_ IDxcOperationResult** ppResult,   // Compiler output status, buffer, and errors
+    _Outptr_opt_result_z_ LPWSTR* ppDebugBlobName, // Suggested file name for debug blob. (Must be CoTaskMemFree()'d!)
+    _COM_Outptr_opt_ IDxcBlob** ppDebugBlob        // Debug blob
     ) = 0;
 };
 
@@ -376,7 +376,7 @@ public:
   // Register a library with name to ref it later.
   virtual HRESULT RegisterLibrary(
     _In_opt_ LPCWSTR pLibName, // Name of the library.
-    _In_ IDxcBlob*   pLib      // Library blob.
+    _In_ IDxcBlob* pLib        // Library blob.
     ) = 0;
 
   // Links the shader and produces a shader blob that the Direct3D runtime can
@@ -404,9 +404,9 @@ struct IDxcUtils : public IUnknown
 {
   // Create a sub-blob that holds a reference to the outer blob and points to its memory.
   virtual HRESULT STDMETHODCALLTYPE CreateBlobFromBlob(
-    _In_ IDxcBlob*          pBlob,
-    UINT32                  offset,
-    UINT32                  length,
+    _In_ IDxcBlob* pBlob,
+    UINT32         offset,
+    UINT32         length,
     _COM_Outptr_ IDxcBlob** ppResult) = 0;
 
   // For codePage, use 0 (or DXC_CP_ACP) for raw binary or ANSI code page
@@ -415,18 +415,18 @@ struct IDxcUtils : public IUnknown
   // User must manage the memory lifetime separately.
   // (was: CreateBlobWithEncodingFromPinned)
   virtual HRESULT STDMETHODCALLTYPE CreateBlobFromPinned(
-    _In_bytecount_(size) LPCVOID    pData,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pData,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
   // Create blob, taking ownership of memory allocated with supplied allocator.
   // (was: CreateBlobWithEncodingOnMalloc)
   virtual HRESULT STDMETHODCALLTYPE MoveToBlob(
-    _In_bytecount_(size) LPCVOID    pData,
-    IMalloc*                        pIMalloc,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pData,
+    IMalloc*                     pIMalloc,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
   ////
@@ -435,19 +435,19 @@ struct IDxcUtils : public IUnknown
   // Copy blob contents to memory owned by the new blob.
   // (was: CreateBlobWithEncodingOnHeapCopy)
   virtual HRESULT STDMETHODCALLTYPE CreateBlob(
-    _In_bytecount_(size) LPCVOID    pData,
-    UINT32                          size,
-    UINT32                          codePage,
+    _In_bytecount_(size) LPCVOID pData,
+    UINT32                       size,
+    UINT32                       codePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
   // (was: CreateBlobFromFile)
   virtual HRESULT STDMETHODCALLTYPE LoadFile(
-    _In_z_ LPCWSTR                  pFileName,
-    _In_opt_ UINT32*                pCodePage,
+    _In_z_ LPCWSTR pFileName,
+    _In_opt_ UINT32* pCodePage,
     _COM_Outptr_ IDxcBlobEncoding** pBlobEncoding) = 0;
 
   virtual HRESULT STDMETHODCALLTYPE CreateReadOnlyStreamFromBlob(
-    _In_ IDxcBlob*         pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IStream** ppStream) = 0;
 
   // Create default file-based include handler
@@ -456,18 +456,18 @@ struct IDxcUtils : public IUnknown
 
   // Convert or return matching encoded text blobs
   virtual HRESULT STDMETHODCALLTYPE GetBlobAsUtf8(
-    _In_ IDxcBlob*              pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobUtf8** pBlobEncoding) = 0;
 
   // Renamed from GetBlobAsUtf16 to GetBlobAsWide
   virtual HRESULT STDMETHODCALLTYPE GetBlobAsWide(
-    _In_ IDxcBlob*              pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobWide** pBlobEncoding) = 0;
 
 #ifdef _WIN32
   // Alias to GetBlobAsWide on Win32
   inline HRESULT GetBlobAsUtf16(
-    _In_ IDxcBlob*              pBlob,
+    _In_ IDxcBlob* pBlob,
     _COM_Outptr_ IDxcBlobWide** pBlobEncoding)
   {
     return this->GetBlobAsWide(pBlob, pBlobEncoding);
@@ -478,7 +478,7 @@ struct IDxcUtils : public IUnknown
     _In_ const DxcBuffer*                pShader,
     _In_ UINT32                          DxcPart,
     _Outptr_result_nullonfailure_ void** ppPartData,
-    _Out_ UINT32*                        pPartSizeInBytes) = 0;
+    _Out_ UINT32* pPartSizeInBytes) = 0;
 
   // Create reflection interface from serialized Dxil container, or DXC_PART_REFLECTION_DATA.
   // TBD: Require part header for RDAT?  (leaning towards yes)
@@ -494,14 +494,14 @@ struct IDxcUtils : public IUnknown
     _In_opt_count_(argCount) LPCWSTR* pArguments,     // Array of pointers to arguments
     _In_ UINT32                       argCount,       // Number of arguments
     _In_count_(defineCount)
-      const DxcDefine*              pDefines,    // Array of defines
-    _In_ UINT32                     defineCount, // Number of defines
-    _COM_Outptr_ IDxcCompilerArgs** ppArgs       // Arguments you can use with Compile() method
+      const DxcDefine* pDefines,           // Array of defines
+    _In_ UINT32        defineCount,        // Number of defines
+    _COM_Outptr_ IDxcCompilerArgs** ppArgs // Arguments you can use with Compile() method
     ) = 0;
 
   // Takes the shader PDB and returns the hash and the container inside it
   virtual HRESULT STDMETHODCALLTYPE GetPDBContents(
-    _In_ IDxcBlob*          pPDBBlob,
+    _In_ IDxcBlob* pPDBBlob,
     _COM_Outptr_ IDxcBlob** ppHash,
     _COM_Outptr_ IDxcBlob** ppContainer) = 0;
 };
@@ -541,7 +541,7 @@ struct IDxcResult : public IDxcOperationResult
   virtual HRESULT STDMETHODCALLTYPE GetOutput(_In_ DXC_OUT_KIND                        dxcOutKind,
                                               _In_ REFIID                              iid,
                                               _COM_Outptr_opt_result_maybenull_ void** ppvObject,
-                                              _COM_Outptr_ IDxcBlobWide**              ppOutputName) = 0;
+                                              _COM_Outptr_ IDxcBlobWide** ppOutputName) = 0;
 
   virtual UINT32       GetNumOutputs()                = 0;
   virtual DXC_OUT_KIND GetOutputByIndex(UINT32 Index) = 0;
@@ -557,9 +557,9 @@ struct IDxcExtraOutputs : public IUnknown
 {
 
   virtual UINT32 STDMETHODCALLTYPE  GetOutputCount()                                                         = 0;
-  virtual HRESULT STDMETHODCALLTYPE GetOutput(_In_ UINT32                                      uIndex,
-                                              _In_ REFIID                                      iid,
-                                              _COM_Outptr_opt_result_maybenull_ void**         ppvObject,
+  virtual HRESULT STDMETHODCALLTYPE GetOutput(_In_ UINT32                              uIndex,
+                                              _In_ REFIID                              iid,
+                                              _COM_Outptr_opt_result_maybenull_ void** ppvObject,
                                               _COM_Outptr_opt_result_maybenull_ IDxcBlobWide** ppOutputType,
                                               _COM_Outptr_opt_result_maybenull_ IDxcBlobWide** ppOutputName) = 0;
 };
@@ -572,19 +572,19 @@ struct IDxcCompiler3 : public IUnknown
   // Compile a root signature (-T rootsig_*), or
   // Preprocess HLSL source (-P)
   virtual HRESULT STDMETHODCALLTYPE Compile(
-    _In_ const DxcBuffer*             pSource,         // Source text to compile
-    _In_opt_count_(argCount) LPCWSTR* pArguments,      // Array of pointers to arguments
-    _In_ UINT32                       argCount,        // Number of arguments
-    _In_opt_ IDxcIncludeHandler*      pIncludeHandler, // user-provided interface to handle #include directives (optional)
-    _In_ REFIID                       riid,
-    _Out_ LPVOID*                     ppResult // IDxcResult: status, buffer, and errors
+    _In_ const DxcBuffer*             pSource,    // Source text to compile
+    _In_opt_count_(argCount) LPCWSTR* pArguments, // Array of pointers to arguments
+    _In_ UINT32                       argCount,   // Number of arguments
+    _In_opt_ IDxcIncludeHandler* pIncludeHandler, // user-provided interface to handle #include directives (optional)
+    _In_ REFIID                  riid,
+    _Out_ LPVOID* ppResult // IDxcResult: status, buffer, and errors
     ) = 0;
 
   // Disassemble a program.
   virtual HRESULT STDMETHODCALLTYPE Disassemble(
     _In_ const DxcBuffer* pObject, // Program to disassemble: dxil container or bitcode.
     _In_ REFIID           riid,
-    _Out_ LPVOID*         ppResult // IDxcResult: status, disassembly text, and errors
+    _Out_ LPVOID* ppResult // IDxcResult: status, disassembly text, and errors
     ) = 0;
 };
 
@@ -599,8 +599,8 @@ struct IDxcValidator : public IUnknown
 {
   // Validate a shader.
   virtual HRESULT STDMETHODCALLTYPE Validate(
-    _In_ IDxcBlob*                     pShader, // Shader to validate.
-    _In_ UINT32                        Flags,   // Validation flags.
+    _In_ IDxcBlob* pShader,                     // Shader to validate.
+    _In_ UINT32    Flags,                       // Validation flags.
     _COM_Outptr_ IDxcOperationResult** ppResult // Validation output status, buffer, and errors
     ) = 0;
 };
@@ -610,10 +610,10 @@ struct IDxcValidator2 : public IDxcValidator
 {
   // Validate a shader.
   virtual HRESULT STDMETHODCALLTYPE ValidateWithDebug(
-    _In_ IDxcBlob*                     pShader,          // Shader to validate.
-    _In_ UINT32                        Flags,            // Validation flags.
-    _In_opt_ DxcBuffer*                pOptDebugBitcode, // Optional debug module bitcode to provide line numbers
-    _COM_Outptr_ IDxcOperationResult** ppResult          // Validation output status, buffer, and errors
+    _In_ IDxcBlob* pShader,                     // Shader to validate.
+    _In_ UINT32    Flags,                       // Validation flags.
+    _In_opt_ DxcBuffer* pOptDebugBitcode,       // Optional debug module bitcode to provide line numbers
+    _COM_Outptr_ IDxcOperationResult** ppResult // Validation output status, buffer, and errors
     ) = 0;
 };
 
@@ -631,7 +631,7 @@ struct IDxcAssembler : public IUnknown
 {
   // Assemble dxil in ll or llvm bitcode to DXIL container.
   virtual HRESULT STDMETHODCALLTYPE AssembleToContainer(
-    _In_ IDxcBlob*                     pShader, // Shader to assemble.
+    _In_ IDxcBlob* pShader,                     // Shader to assemble.
     _COM_Outptr_ IDxcOperationResult** ppResult // Assembly output status, buffer, and errors
     ) = 0;
 };
@@ -662,10 +662,10 @@ struct IDxcOptimizer : public IUnknown
 {
   virtual HRESULT STDMETHODCALLTYPE GetAvailablePassCount(_Out_ UINT32* pCount)                               = 0;
   virtual HRESULT STDMETHODCALLTYPE GetAvailablePass(UINT32 index, _COM_Outptr_ IDxcOptimizerPass** ppResult) = 0;
-  virtual HRESULT STDMETHODCALLTYPE RunOptimizer(IDxcBlob*                           pBlob,
-                                                 _In_count_(optionCount) LPCWSTR*    ppOptions,
-                                                 UINT32                              optionCount,
-                                                 _COM_Outptr_ IDxcBlob**             pOutputModule,
+  virtual HRESULT STDMETHODCALLTYPE RunOptimizer(IDxcBlob*                        pBlob,
+                                                 _In_count_(optionCount) LPCWSTR* ppOptions,
+                                                 UINT32                           optionCount,
+                                                 _COM_Outptr_ IDxcBlob** pOutputModule,
                                                  _COM_Outptr_opt_ IDxcBlobEncoding** ppOutputText)            = 0;
 };
 
