e98b8dc15ccbaca466b08b852b473c2114afa428
Theophilus Eriata <theoeriata@gmail.com>
2023-03-25 22:04:25 -0600

    Foundation: Expand Argument Helper.

--- Source/Engine/Foundation/Basics/Platform/BlackMagic.h
+++ Source/Engine/Foundation/Basics/Platform/BlackMagic.h
@@ -6,9 +6,9 @@
 /// If an empty __VA_ARGS__ is passed in, this will still return 1.
 /// There is no perfect way to detect parameter lists with zero elements.
 #ifndef XII_VA_NUM_ARGS
-#  define XII_VA_NUM_ARGS(...) XII_VA_NUM_ARGS_HELPER(__VA_ARGS__, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+#  define XII_VA_NUM_ARGS(...) XII_VA_NUM_ARGS_HELPER(__VA_ARGS__, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 
-#  define XII_VA_NUM_ARGS_HELPER(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, N, ...) N
+#  define XII_VA_NUM_ARGS_HELPER(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, N, ...) N
 #endif
 
 
