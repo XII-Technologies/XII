3707613131425b7a699836ec9c06a8e874091a1b
Theophilus Eriata <theoeriata@gmail.com>
2023-03-27 07:26:50 -0600

    Fix Texture Sample.

--- Source/Samples/TextureSample/TextureSample.cpp
+++ Source/Samples/TextureSample/TextureSample.cpp
@@ -181,7 +181,8 @@ public:
     // Create a device
     {
       xiiGALDeviceCreationDescription DeviceInit;
-      DeviceInit.m_bDebugDevice = true;
+      DeviceInit.m_bDebugDevice   = true;
+      DeviceInit.m_GraphicsDevice = xiiGALDeviceFactory::GetGraphicsDevice(szRendererName);
 
       m_pDevice = xiiGALDeviceFactory::CreateDevice(szRendererName, xiiFoundation::GetDefaultAllocator(), DeviceInit);
       XII_ASSERT_DEV(m_pDevice != nullptr, "Device implemention for '{}' not found", szRendererName);
