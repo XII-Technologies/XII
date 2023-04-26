af01d5a63efa932534066e8d5a528f8500946dce
Theophilus Eriata <theoeriata@gmail.com>
2023-04-26 11:00:36 -0600

    ThirdParty: Add Tracy Profiler To Source Tree.

---
+++ Source/ThirdParty/Tracy/init.cmake
@@ -0,0 +1,6 @@
+
+######################################
+### Tracy Profiler support
+######################################
+
+set (XII_BUILD_TRACY ON CACHE BOOL "Whether support for the Tracy profiler should be added.")
