e0fba76db3f4f8f1e775ceaf1895722315ad0b06
Theophilus Eriata <theoeriata@gmail.com>
2023-05-03 07:54:14 -0600

    RendererDX11: Add Support For Texture Array Usage.

--- Source/Engine/RendererDX11/Resources/Implementation/UnorderedAccessViewDX11.cpp
+++ Source/Engine/RendererDX11/Resources/Implementation/UnorderedAccessViewDX11.cpp
@@ -82,7 +82,9 @@ xiiResult xiiGALUnorderedAccessViewDX11::InitPlatform(xiiGALDevice* pDevice)
     switch (texDesc.m_Type)
     {
       case xiiGALTextureType::Texture2D:
+      case xiiGALTextureType::Texture2DArray:
       case xiiGALTextureType::Texture2DProxy:
+      case xiiGALTextureType::Texture2DProxyArray:
 
         if (!bIsArrayView)
         {
@@ -99,6 +101,7 @@ xiiResult xiiGALUnorderedAccessViewDX11::InitPlatform(xiiGALDevice* pDevice)
         break;
 
       case xiiGALTextureType::TextureCube:
+      case xiiGALTextureType::TextureCubeArray:
         DXUAVDesc.ViewDimension                  = D3D11_UAV_DIMENSION_TEXTURE2DARRAY;
         DXUAVDesc.Texture2DArray.MipSlice        = m_Description.m_uiMipLevelToUse;
         DXUAVDesc.Texture2DArray.ArraySize       = m_Description.m_uiArraySize;
