5e3c4e254f71839b9107bad2ed10cd8e342640c8
Theophilus Eriata <theoeriata@gmail.com>
2023-05-15 07:03:16 -0600

    Foundation: Fix Function Property Alias. (#57)

--- Source/Engine/Foundation/Reflection/Implementation/FunctionProperty.h
+++ Source/Engine/Foundation/Reflection/Implementation/FunctionProperty.h
@@ -58,7 +58,7 @@ class xiiFunctionProperty
   class xiiFunctionProperty<R (CLASS::*)(Args...) CONSTNESS> : public xiiTypedFunctionProperty<R, Args...>                                                   \
   {                                                                                                                                                          \
   public:                                                                                                                                                    \
-    using TargetFunction = R (CLASS::*)(Args...);                                                                                                            \
+    using TargetFunction = R (CLASS::*)(Args...) CONSTNESS;                                                                                                  \
                                                                                                                                                              \
     xiiFunctionProperty(const char* szPropertyName, TargetFunction func) : xiiTypedFunctionProperty<R, Args...>(szPropertyName)                              \
     {                                                                                                                                                        \
