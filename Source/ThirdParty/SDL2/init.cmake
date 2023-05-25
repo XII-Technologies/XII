0a759ca4dfa6b733f4a8a24d1caec0239893c276
Theophilus Eriata <theoeriata@gmail.com>
2023-05-25 15:03:47 -0600

    Core: Use SDL as the default window and input handler.

--- Source/ThirdParty/SDL2/init.cmake
+++ Source/ThirdParty/SDL2/init.cmake
@@ -7,9 +7,10 @@ if(XII_CMAKE_PLATFORM_LINUX)
   # On Linux we want to use SDL by default
   set (XII_3RDPARTY_SDL_SUPPORT ON CACHE BOOL "Use SDL to manage windows and input")
   message(STATUS "Using SDL Window and Input Handling by Default")
-  else()
+else()
   # On all other platforms SDL support is optional.
-  set (XII_3RDPARTY_SDL_SUPPORT OFF CACHE BOOL "Use SDL to manage windows and input")
+  set (XII_3RDPARTY_SDL_SUPPORT ON CACHE BOOL "Use SDL to manage windows and input")
+
   if (NOT XII_CMAKE_PLATFORM_WINDOWS_DESKTOP)
     mark_as_advanced(FORCE XII_3RDPARTY_SDL_SUPPORT)
   endif()
