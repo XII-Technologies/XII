f15e7160a284fb5155ef61cf1a3a3577715916cd
Theophilus Eriata <theoeriata@gmail.com>
2023-06-02 23:47:46 -0600

    Core: Disable SDL2 On Windows.

--- Source/ThirdParty/SDL2/init.cmake
+++ Source/ThirdParty/SDL2/init.cmake
@@ -9,7 +9,7 @@ if(XII_CMAKE_PLATFORM_LINUX)
   message(STATUS "Using SDL Window and Input Handling by Default")
 else()
   # On all other platforms SDL support is optional.
-  set (XII_3RDPARTY_SDL_SUPPORT ON CACHE BOOL "Use SDL to manage windows and input")
+  set (XII_3RDPARTY_SDL_SUPPORT OFF CACHE BOOL "Use SDL to manage windows and input")
 
   if (NOT XII_CMAKE_PLATFORM_WINDOWS_DESKTOP)
     mark_as_advanced(FORCE XII_3RDPARTY_SDL_SUPPORT)
