b1c2046e141f94334d9173cbdb6b194f1964f964
Theophilus Eriata <theoeriata@gmail.com>
2023-06-05 07:52:56 -0600

    RendererCore: Revert Reflection Probe Changes.

--- Source/Engine/RendererCore/Lights/Implementation/ReflectionProbeUpdater.cpp
+++ Source/Engine/RendererCore/Lights/Implementation/ReflectionProbeUpdater.cpp
@@ -30,8 +30,7 @@ xiiReflectionProbeUpdater::ProbeUpdateInfo::ProbeUpdateInfo()
     desc.m_uiHeight                    = s_uiReflectionCubeMapSize;
     desc.m_uiMipLevelCount             = GetMipLevels();
     desc.m_Format                      = xiiGALResourceFormat::RGBAHalf;
-    desc.m_Type                        = xiiGALTextureType::Texture2DArray;
-    desc.m_uiArraySize                 = 6u;
+    desc.m_Type                        = xiiGALTextureType::TextureCube;
     desc.m_bCreateRenderTarget         = true;
     desc.m_bAllowDynamicMipGeneration  = true;
     desc.m_ResourceAccess.m_bReadBack  = true;
