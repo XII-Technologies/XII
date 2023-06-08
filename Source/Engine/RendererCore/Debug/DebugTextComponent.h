114dab08a2a831501778e20cf2159be02df68190
XII Automation <automation@xiitechnologies.com>
2023-06-08 13:57:27 +0000

    Applied code formatting rules

--- Source/Engine/RendererCore/Debug/DebugTextComponent.h
+++ Source/Engine/RendererCore/Debug/DebugTextComponent.h
@@ -5,7 +5,7 @@
 
 struct xiiMsgExtractRenderData;
 
-using xiiDebugTextComponentManager =  xiiComponentManager<class xiiDebugTextComponent, xiiBlockStorageType::Compact>;
+using xiiDebugTextComponentManager = xiiComponentManager<class xiiDebugTextComponent, xiiBlockStorageType::Compact>;
 
 /// \brief This component prints debug text at the owner object's position.
 class XII_RENDERERCORE_DLL xiiDebugTextComponent : public xiiComponent
