cd15b572f8b98059cca30f2c856d2303d6ac3b65
Theophilus Eriata <theoeriata@gmail.com>
2023-06-16 00:00:05 -0600

    TextureSample: Add RendererDX11 Dependency.

--- Source/Samples/TextureSample/finalize.cmake
+++ Source/Samples/TextureSample/finalize.cmake
@@ -2,3 +2,7 @@
 if (TARGET TextureSample AND TARGET RendererDiligent)
   add_dependencies(TextureSample RendererDiligent)
 endif()
+
+if (TARGET TextureSample AND TARGET RendererDX11)
+  add_dependencies(TextureSample RendererDX11)
+endif()
