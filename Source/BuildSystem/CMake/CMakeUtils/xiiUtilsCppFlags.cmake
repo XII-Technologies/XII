cbfa1270a4ae4551ca03174e67a290e28051a8c3
Theophilus Eriata <theoeriata@gmail.com>
2023-07-27 21:53:31 -0600

    BuildSystem: Disable Implicit Conversion Between Enum Types Warning.

--- Source/BuildSystem/CMake/CMakeUtils/xiiUtilsCppFlags.cmake
+++ Source/BuildSystem/CMake/CMakeUtils/xiiUtilsCppFlags.cmake
@@ -124,19 +124,20 @@ function(xii_set_build_flags_msvc TARGET_NAME)
 
 	# Ignore various warnings we are not interested in
 
-	# 4100 = unreferenced formal parameter *
-	# 4127 = conditional expression is constant *
-	# 4189 = local variable is initialized but not referenced *
-	# 4201 = nonstandard extension used: nameless struct/union *
-	# 4245 = signed/unsigned mismatch *
-	# 4251 = class 'type' needs to have dll-interface to be used by clients of class 'type2' -> dll export / import issues (mostly with templates) *
-	# 4310 = cast truncates constant value *
-	# 4324 = structure was padded due to alignment specifier *
-	# 4345 = behavior change: an object of POD type constructed with an initializer of the form () will be default-initialized
-	# 4389 = signed/unsigned mismatch *
-	# 4714 = function 'function' marked as __forceinline not inlined
+	# 4100 = Unreferenced formal parameter *
+	# 4127 = Conditional expression is constant *
+	# 4189 = Local variable is initialized but not referenced *
+	# 4201 = Nonstandard extension used: nameless struct/union *
+	# 4245 = Signed/unsigned mismatch *
+	# 4251 = Class 'type' needs to have dll-interface to be used by clients of class 'type2' -> dll export / import issues (mostly with templates) *
+	# 4310 = Cast truncates constant value *
+	# 4324 = Structure was padded due to alignment specifier *
+	# 4345 = Behavior change: an object of POD type constructed with an initializer of the form () will be default-initialized
+	# 4389 = Signed/unsigned mismatch *
+	# 4714 = Function 'function' marked as __forceinline not inlined
 	# 6326 = Potential comparison of a constant with another constant
-	target_compile_options(${TARGET_NAME} PUBLIC /wd4201 /wd4251 /wd4324 /wd4345)
+	# 5054 = Automatic conversion on operands. An implicit conversion between enumeration types produces a level 4 warning.
+	target_compile_options(${TARGET_NAME} PUBLIC /wd4201 /wd4251 /wd4324 /wd4345 /wd5054)
 	target_compile_options(${TARGET_NAME} PRIVATE /wd4100 /wd4189 /wd4127 /wd4245 /wd4389 /wd4310 /wd4714 /wd6326)
 
 	# Set Warnings as Errors: Too few/many parameters given for Macro
@@ -145,7 +146,7 @@ function(xii_set_build_flags_msvc TARGET_NAME)
 	# 4099 = Linker warning "PDB was not found with lib"
 	target_link_options(${TARGET_NAME} PRIVATE /ignore:4099)
 
-	# 'nodiscard': attribute is ignored in this syntactic position
+	# 'nodiscard': Attribute is ignored in this syntactic position
 	target_compile_options(${TARGET_NAME} PRIVATE /wd5240)
 
 endfunction()
