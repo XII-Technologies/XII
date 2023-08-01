e4496327548b33d7f7fca1c6c48a3b609318c390
Theophilus Eriata <theoeriata@gmail.com>
2023-08-01 07:43:50 -0600

    Foundation: Improve Linux Debug Break.
    This allows the XII_DEBUG_BREAK to be continued in the debugger when compiling for Linux GCC or Clang.

--- Source/Engine/Foundation/Basics/Compiler/Clang/Clang.h
+++ Source/Engine/Foundation/Basics/Compiler/Clang/Clang.h
@@ -15,10 +15,30 @@
 
 #  define XII_ALIGNMENT_OF(type) XII_COMPILE_TIME_MAX(__alignof(type), XII_ALIGNMENT_MINIMUM)
 
-#  define XII_DEBUG_BREAK \
-    {                     \
-      __builtin_trap();   \
-    }
+#  if __has_builtin(__builtin_debugtrap)
+#    define XII_DEBUG_BREAK    \
+      {                        \
+        __builtin_debugtrap(); \
+      }
+#  elif __has_builtin(__debugbreak)
+#    define XII_DEBUG_BREAK \
+      {                     \
+        __debugbreak();     \
+      }
+#  else
+#    include <signal.h>
+#    if defined(SIGTRAP)
+#      define XII_DEBUG_BREAK \
+        {                     \
+          raise(SIGTRAP);     \
+        }
+#    else
+#      define XII_DEBUG_BREAK \
+        {                     \
+          raise(SIGABRT);     \
+        }
+#    endif
+#  endif
 
 #  define XII_SOURCE_FUNCTION __PRETTY_FUNCTION__
 #  define XII_SOURCE_LINE     __LINE__
