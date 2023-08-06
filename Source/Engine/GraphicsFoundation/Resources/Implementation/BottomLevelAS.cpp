19450dd023fd8acbf5fd37432885515dd8818bed
Theophilus Eriata <theoeriata@gmail.com>
2023-08-05 19:46:33 -0600

    GraphicsFoundation: Restructure Bottom Level AS Descriptions.

---
+++ Source/Engine/GraphicsFoundation/Resources/Implementation/BottomLevelAS.cpp
@@ -0,0 +1,14 @@
+#include <GraphicsFoundation/Resources/BottomLevelAS.h>
+
+// clang-format off
+
+XII_BEGIN_STATIC_REFLECTED_ENUM(xiiGALRaytTracingBuildASFlags, 1)
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::None),
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::AllowUpdate),
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::AllowCompaction),
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::PreferFastTrace),
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::PreferFastBuild),
+  XII_ENUM_CONSTANT(xiiGALRaytTracingBuildASFlags::LowMemory),
+XII_END_STATIC_REFLECTED_ENUM;
+
+// clang-format on
