d4610c7e0e10db359fbe8450f54bef09986c73d9
Theophilus Eriata <theoeriata@gmail.com>
2023-08-07 15:17:53 -0600

    GraphicsFoundation: Add Blend State Creation Description.

--- Source/Engine/GraphicsFoundation/States/Implementation/DepthStencilState.cpp
+++ Source/Engine/GraphicsFoundation/States/Implementation/DepthStencilState.cpp
@@ -1,3 +1,19 @@
 #include <GraphicsFoundation/GraphicsFoundationPCH.h>
 
 #include <GraphicsFoundation/States/DepthStencilState.h>
+
+// clang-format off
+
+XII_BEGIN_STATIC_REFLECTED_ENUM(xiiGALStencilOperation, 1)
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::Undefined),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::Keep),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::Zero),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::Replace),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::IncrementSaturate),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::DecrementSaturate),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::Invert),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::IncrementWrap),
+  XII_ENUM_CONSTANT(xiiGALStencilOperation::DecrementWrap),
+XII_END_STATIC_REFLECTED_ENUM;
+
+// clang-format on
