e003ad4425da9aa0089999a1b3ad6d51b8bf1917
Theophilus Eriata <theoeriata@gmail.com>
2023-08-15 14:15:29 -0600

    GraphicsFoundation: Add Pipeline State Descriptions.

--- Source/Engine/GraphicsFoundation/States/Implementation/PipelineState.cpp
+++ Source/Engine/GraphicsFoundation/States/Implementation/PipelineState.cpp
@@ -2,4 +2,20 @@
 
 #include <GraphicsFoundation/States/PipelineState.h>
 
+// clang-format off
+
+XII_BEGIN_STATIC_REFLECTED_ENUM(xiiGALShaderVariableFlags, 1)
+  XII_ENUM_CONSTANT(xiiGALShaderVariableFlags::None),
+  XII_ENUM_CONSTANT(xiiGALShaderVariableFlags::NoDynamicBuffers),
+  XII_ENUM_CONSTANT(xiiGALShaderVariableFlags::InputAttachment),
+XII_END_STATIC_REFLECTED_ENUM;
+
+XII_BEGIN_STATIC_REFLECTED_ENUM(xiiGALPipelineShadingRateFlags, 1)
+  XII_ENUM_CONSTANT(xiiGALPipelineShadingRateFlags::None),
+  XII_ENUM_CONSTANT(xiiGALPipelineShadingRateFlags::PerPrimitive),
+  XII_ENUM_CONSTANT(xiiGALPipelineShadingRateFlags::TextureBased),
+XII_END_STATIC_REFLECTED_ENUM;
+
+// clang-format on
+
 XII_STATICLINK_FILE(GraphicsFoundation, GraphicsFoundation_States_Implementation_PipelineState);
