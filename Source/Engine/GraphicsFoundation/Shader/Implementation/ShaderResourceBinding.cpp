036e82b8bb54a6823449529b6ea8c4eb43d56c27
Theophilus Eriata <theoeriata@gmail.com>
2023-08-17 17:26:55 -0600

    GraphicsFoundation: Add Shader Resource Binding File.

---
+++ Source/Engine/GraphicsFoundation/Shader/Implementation/ShaderResourceBinding.cpp
@@ -0,0 +1,5 @@
+#include <GraphicsFoundation/GraphicsFoundationPCH.h>
+
+#include <GraphicsFoundation/Shader/ShaderResourceBinding.h>
+
+XII_STATICLINK_FILE(GraphicsFoundation, GraphicsFoundation_Shader_Implementation_ShaderResourceBinding);
