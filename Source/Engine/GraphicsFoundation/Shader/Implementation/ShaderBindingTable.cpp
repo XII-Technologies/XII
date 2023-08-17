1d630eb32ecec73cce246659383d286925e65cbc
Theophilus Eriata <theoeriata@gmail.com>
2023-08-17 17:27:13 -0600

    GraphicsFoundation: Add Shader Binding Table File.

---
+++ Source/Engine/GraphicsFoundation/Shader/Implementation/ShaderBindingTable.cpp
@@ -0,0 +1,5 @@
+#include <GraphicsFoundation/GraphicsFoundationPCH.h>
+
+#include <GraphicsFoundation/Shader/ShaderBindingTable.h>
+
+XII_STATICLINK_FILE(GraphicsFoundation, GraphicsFoundation_Shader_Implementation_ShaderBindingTable);
---
+++ Source/Engine/GraphicsFoundation/Shader/ShaderBindingTable.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <GraphicsFoundation/GraphicsFoundationDLL.h>
+
+#include <GraphicsFoundation/Shader/Implementation/ShaderBindingTable_inl.h>
