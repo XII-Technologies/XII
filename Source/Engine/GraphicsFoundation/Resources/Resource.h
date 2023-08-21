bd6cc4fa5bea9804c5b6a1b668d318e11f08eb12
Theophilus Eriata <theoeriata@gmail.com>
2023-08-21 17:31:10 -0600

    GraphicsFoundation: Add Texture Dimension Helper Functions.

--- Source/Engine/GraphicsFoundation/Resources/Resource.h
+++ Source/Engine/GraphicsFoundation/Resources/Resource.h
@@ -17,16 +17,18 @@ protected:
   {
     XII_ASSERT_DEV(m_hDefaultBufferView.IsInvalidated(), "");
     XII_ASSERT_DEV(m_hDefaultTextureView.IsInvalidated(), "");
+    XII_ASSERT_DEV(m_hDefaultRenderTargetView.IsInvalidated(), "");
 
     XII_ASSERT_DEV(m_BufferViews.IsEmpty(), "There are resident buffer views remaining.");
     XII_ASSERT_DEV(m_TextureViews.IsEmpty(), "There are resident texture views remaining.");
   }
 
-  xiiHashTable<xiiUInt32, xiiGALBufferViewHandle>     m_BufferViews;
+  xiiHashTable<xiiUInt32, xiiGALBufferViewHandle>  m_BufferViews;
   xiiHashTable<xiiUInt32, xiiGALTextureViewHandle> m_TextureViews;
 
-  xiiGALBufferViewHandle m_hDefaultBufferView;
+  xiiGALBufferViewHandle  m_hDefaultBufferView;
   xiiGALTextureViewHandle m_hDefaultTextureView;
+  xiiGALTextureViewHandle m_hDefaultRenderTargetView;
 
 #if XII_ENABLED(XII_COMPILE_FOR_DEVELOPMENT)
   mutable xiiHashedString m_sDebugName;
--- Source/Engine/GraphicsFoundation/Resources/Texture.h
+++ Source/Engine/GraphicsFoundation/Resources/Texture.h
@@ -63,6 +63,12 @@ struct XII_GRAPHICSFOUNDATION_DLL xiiGALTextureCreationDescription : public xiiH
   xiiGALOptimizedClearValue           m_ClearValue;                                              ///< Optimized clear value.
   xiiUInt64                           m_uiImmediateContextMask = XII_BIT(0);                     ///< Defines which immediate contexts are allowed to execute commands that use this texture. The default is the main immediate context.
                                                                                                  ///< Only specify the bits that indicate those immediate contexts where the resource will be used, setting unnecessary bits will result in extra overhead.
+
+  bool IsArray() const;
+  bool Is1D() const;
+  bool Is2D() const;
+  bool Is3D() const;
+  bool IsCube() const;
 };
 
 /// \brief This describes the data for one texture sub-resource.
