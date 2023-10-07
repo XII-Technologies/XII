6abc68fa9717e30927ddc04952b0d32a74669f30
Theophilus Eriata <theoeriata@gmail.com>
2023-10-06 23:48:56 -0600

    GraphicsD3D12: Begin xiiGALDevice Implementation.

---
+++ Source/Engine/GraphicsD3D12/Device/Implementation/DeviceD3D12.cpp
@@ -0,0 +1,231 @@
+#include <GraphicsD3D12/GraphicsD3D12PCH.h>
+
+#include <Foundation/Configuration/Startup.h>
+#include <GraphicsFoundation/Device/DeviceFactory.h>
+
+#include <GraphicsD3D12/Device/DeviceD3D12.h>
+
+xiiInternal::NewInstance<xiiGALDevice> CreateD3D12Device(xiiAllocatorBase* pAllocator, const xiiGALDeviceCreationDescription& description)
+{
+  return XII_NEW(pAllocator, xiiGALDeviceD3D12, description);
+}
+
+// clang-format off
+XII_BEGIN_SUBSYSTEM_DECLARATION(GraphicsD3D12, DeviceFactory)
+
+ON_CORESYSTEMS_STARTUP
+{
+  xiiGALDeviceFactory::RegisterCreatorFunc("D3D12", &CreateD3D12Device, "D3D12_SM60", "xiiShaderCompiler");
+}
+
+ON_CORESYSTEMS_SHUTDOWN
+{
+  xiiGALDeviceFactory::UnregisterCreatorFunc("D3D12");
+}
+
+XII_END_SUBSYSTEM_DECLARATION;
+// clang-format on
+
+xiiGALDeviceD3D12::xiiGALDeviceD3D12(const xiiGALDeviceCreationDescription& description) :
+  xiiGALDevice(description)
+{
+}
+
+xiiGALDeviceD3D12::~xiiGALDeviceD3D12() = default;
+
+xiiResult xiiGALDeviceD3D12::InitializePlatform()
+{
+  return XII_SUCCESS;
+}
+
+void xiiGALDeviceD3D12::ReportLiveGPUObjects()
+{
+}
+
+void xiiGALDeviceD3D12::FlushPendingObjects()
+{
+}
+
+xiiResult xiiGALDeviceD3D12::ShutdownPlatform()
+{
+  return XII_SUCCESS;
+}
+
+void xiiGALDeviceD3D12::BeginPipelinePlatform(xiiStringView Name, xiiGALSwapChain* pSwapChain)
+{
+}
+
+void xiiGALDeviceD3D12::EndPipelinePlatform(xiiGALSwapChain* pSwapChain)
+{
+}
+
+xiiGALPass* xiiGALDeviceD3D12::BeginPassPlatform(xiiStringView Name)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::EndPassPlatform(xiiGALPass* pPass)
+{
+}
+
+void xiiGALDeviceD3D12::BeginFramePlatform(const xiiUInt64 uiRenderFrame)
+{
+}
+
+void xiiGALDeviceD3D12::EndFramePlatform()
+{
+}
+
+xiiGALBlendState* xiiGALDeviceD3D12::CreateBlendStatePlatform(const xiiGALBlendStateCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyBlendStatePlatform(xiiGALBlendState* pBlendState)
+{
+}
+
+xiiGALDepthStencilState* xiiGALDeviceD3D12::CreateDepthStencilStatePlatform(const xiiGALDepthStencilStateCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyDepthStencilStatePlatform(xiiGALDepthStencilState* pDepthStencilState)
+{
+}
+
+xiiGALRasterizerState* xiiGALDeviceD3D12::CreateRasterizerStatePlatform(const xiiGALRasterizerStateCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyRasterizerStatePlatform(xiiGALRasterizerState* pRasterizerState)
+{
+}
+
+xiiGALShader* xiiGALDeviceD3D12::CreateShaderPlatform(const xiiGALShaderCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyShaderPlatform(xiiGALShader* pShader)
+{
+}
+
+xiiGALBuffer* xiiGALDeviceD3D12::CreateBufferPlatform(const xiiGALBufferCreationDescription& description, const xiiGALBufferData* pInitialData)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyBufferPlatform(xiiGALBuffer* pBuffer)
+{
+}
+
+xiiGALBufferView* xiiGALDeviceD3D12::CreateBufferViewPlatform(const xiiGALBuffer* pBuffer, const xiiGALBufferViewCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyBufferViewPlatform(xiiGALBufferView* pBufferView)
+{
+}
+
+xiiGALTexture* xiiGALDeviceD3D12::CreateTexturePlatform(const xiiGALTextureCreationDescription& description, const xiiGALTextureData* pInitialData)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyTexturePlatform(xiiGALTexture* pTexture)
+{
+}
+
+xiiGALTextureView* xiiGALDeviceD3D12::CreateTextureViewPlatform(const xiiGALTexture* pTexture, const xiiGALTextureViewCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyTextureViewPlatform(xiiGALTextureView* pTextureView)
+{
+}
+
+xiiGALSampler* xiiGALDeviceD3D12::CreateSamplerPlatform(const xiiGALSamplerCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroySamplerPlatform(xiiGALSampler* pSamplerState)
+{
+}
+
+xiiGALInputLayout* xiiGALDeviceD3D12::CreateInputLayoutPlatform(const xiiGALInputLayoutCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyInputLayoutPlatform(xiiGALInputLayout* pInputLayout)
+{
+}
+
+xiiGALQuery* xiiGALDeviceD3D12::CreateQueryPlatform(const xiiGALQueryCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyQueryPlatform(xiiGALQuery* pQuery)
+{
+}
+
+xiiGALFence* xiiGALDeviceD3D12::CreateFencePlatform(const xiiGALFenceCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyFencePlatform(xiiGALFence* pFence)
+{
+}
+
+xiiGALRenderPass* xiiGALDeviceD3D12::CreateRenderPassPlatform(const xiiGALRenderPassCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyRenderPassPlatform(xiiGALRenderPass* pRenderPass)
+{
+}
+
+xiiGALFramebuffer* xiiGALDeviceD3D12::CreateFramebufferPlatform(const xiiGALFramebufferCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyFramebufferPlatform(xiiGALFramebuffer* pFramebuffer)
+{
+}
+
+xiiGALBottomLevelAS* xiiGALDeviceD3D12::CreateBottomLevelASPlatform(const xiiGALBottomLevelASCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyBottomLevelASPlatform(xiiGALBottomLevelAS* pBottomLevelAS)
+{
+}
+
+xiiGALTopLevelAS* xiiGALDeviceD3D12::CreateTopLevelASPlatform(const xiiGALTopLevelASCreationDescription& description)
+{
+  return nullptr;
+}
+
+void xiiGALDeviceD3D12::DestroyTopLevelASPlatform(xiiGALTopLevelAS* pTopLevelAS)
+{
+}
+
+void xiiGALDeviceD3D12::WaitIdlePlatform()
+{
+}
+
+void xiiGALDeviceD3D12::FillCapabilitiesPlatform()
+{
+}
+
+XII_STATICLINK_FILE(GraphicsD3D12, GraphicsD3D12_Device_Implementation_DeviceD3D12);
