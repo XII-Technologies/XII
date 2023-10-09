5a6e6bb8682c3f1d44ce14eb6457b6eab460b5ad
Theophilus Eriata <theoeriata@gmail.com>
2023-10-09 12:46:25 -0600

    GraphicsD3D12: Implement xiiGALFramebufferD3D12.

---
+++ Source/Engine/GraphicsD3D12/Resources/Implementation/FramebufferD3D12.cpp
@@ -0,0 +1,51 @@
+#include <GraphicsD3D12/GraphicsD3D12PCH.h>
+
+#include <GraphicsD3D12/Device/DeviceD3D12.h>
+#include <GraphicsD3D12/Resources/FramebufferD3D12.h>
+#include <GraphicsD3D12/Resources/RenderPassD3D12.h>
+#include <GraphicsD3D12/Resources/TextureViewD3D12.h>
+
+xiiGALFramebufferD3D12::xiiGALFramebufferD3D12(const xiiGALFramebufferCreationDescription& creationDescription) :
+  xiiGALFramebuffer(creationDescription)
+{
+}
+
+xiiGALFramebufferD3D12::~xiiGALFramebufferD3D12() = default;
+
+xiiResult xiiGALFramebufferD3D12::InitPlatform(xiiGALDevice* pDevice)
+{
+  xiiGALDeviceD3D12* pDeviceD3D12 = static_cast<xiiGALDeviceD3D12*>(pDevice);
+
+  Diligent::FramebufferDesc framebufferDescription;
+  framebufferDescription.Name           = m_Description.m_sName.GetStartPointer();
+  framebufferDescription.pRenderPass    = const_cast<Diligent::IRenderPass*>(static_cast<const xiiGALRenderPassD3D12*>(pDeviceD3D12->GetRenderPass(m_Description.m_hRenderPass))->GetRenderPass());
+  framebufferDescription.Width          = m_Description.m_FramebufferSize.width;
+  framebufferDescription.Height         = m_Description.m_FramebufferSize.height;
+  framebufferDescription.NumArraySlices = m_Description.m_uiArraySliceCount;
+
+  const xiiUInt32 uiAttachmentViewCount = m_Description.m_Attachments.GetCount();
+
+  xiiHybridArray<Diligent::ITextureView*, 16U> attachmentViews;
+  attachmentViews.SetCount(uiAttachmentViewCount);
+
+  for (xiiUInt32 i = 0; i < uiAttachmentViewCount; ++i)
+  {
+    const auto& xiiAttachmentView = m_Description.m_Attachments[i];
+    auto        attachmentView    = attachmentViews[i];
+
+    attachmentView = const_cast<Diligent::ITextureView*>(static_cast<const xiiGALTextureViewD3D12*>(pDeviceD3D12->GetTextureView(xiiAttachmentView))->GetTextureView());
+  }
+  framebufferDescription.AttachmentCount = uiAttachmentViewCount;
+  framebufferDescription.ppAttachments   = attachmentViews.GetData();
+
+  pDeviceD3D12->GetDevice()->CreateFramebuffer(framebufferDescription, &m_pFramebuffer);
+
+  return (m_pFramebuffer != nullptr) ? XII_SUCCESS : XII_FAILURE;
+}
+
+xiiResult xiiGALFramebufferD3D12::DeInitPlatform(xiiGALDevice* pDevice)
+{
+  XII_GAL_DILIGENT_REF_RELEASE(m_pFramebuffer);
+
+  return XII_SUCCESS;
+}
---
+++ Source/Engine/GraphicsD3D12/Resources/Implementation/FramebufferD3D12_inl.h
@@ -0,0 +1,5 @@
+
+XII_ALWAYS_INLINE const Diligent::IFramebuffer* xiiGALFramebufferD3D12::GetFramebuffer() const
+{
+  return m_pFramebuffer;
+}
