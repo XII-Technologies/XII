77e0e212ff896a754914adda0823437e2dcd80af
Theophilus Eriata <theoeriata@gmail.com>
2023-10-22 00:08:30 -0600

    GraphicsFoundation: Add Graphics Command Encoder Source Files.

---
+++ Source/Engine/GraphicsFoundation/CommandEncoder/Implementation/GraphicsCommandEncoder.cpp
@@ -0,0 +1,8 @@
+#include <GraphicsFoundation/GraphicsFoundationPCH.h>
+
+#include <GraphicsFoundation/CommandEncoder/GraphicsCommandEncoder.h>
+#include <GraphicsFoundation/Device/Device.h>
+#include <GraphicsFoundation/Resources/Buffer.h>
+#include <GraphicsFoundation/Resources/Texture.h>
+
+XII_STATICLINK_FILE(GraphicsFoundation, GraphicsFoundation_CommandEncoder_Implementation_GraphicsCommandEncoder);
