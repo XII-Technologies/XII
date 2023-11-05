09b2fbbc1a7c58f4d69dc2a356f3441934a68b61
Theophilus Eriata <theoeriata@gmail.com>
2023-11-05 14:29:19 -0700

    ThirdParty: Add OZZ Animation Library.
    Version: 0.14.2.

---
+++ Source/ThirdParty/ozz/init.cmake
@@ -0,0 +1,6 @@
+
+# #####################################
+# ## Ozz Animation Library support
+# #####################################
+set (XII_3RDPARTY_OZZ_SUPPORT ON CACHE BOOL "Whether to add support for the Ozz animation system.")
+mark_as_advanced(FORCE XII_3RDPARTY_OZZ_SUPPORT)
