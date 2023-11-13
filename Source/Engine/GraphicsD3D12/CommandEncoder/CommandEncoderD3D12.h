11572fc4e4809c2de469b4c19f2274e79b9f6363
Theophilus Eriata <theoeriata@gmail.com>
2023-11-13 08:23:53 -0700

    GraphicsD3D12: Add Command Encoder Implementation Functions.

--- Source/Engine/GraphicsD3D12/CommandEncoder/CommandEncoderD3D12.h
+++ Source/Engine/GraphicsD3D12/CommandEncoder/CommandEncoderD3D12.h
@@ -10,6 +10,94 @@ public:
   xiiGALCommandEncoderD3D12(xiiGALDeviceD3D12& deviceD3D12);
   ~xiiGALCommandEncoderD3D12();
 
+  //  xiiGALCommandEncoderCommonPlatformInterface
+
+  // State setting functions
+
+  virtual void SetShaderPlatform(const xiiGALShader* pShader) override;
+  virtual void SetConstantBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pBuffer) override;
+  virtual void SetSamplerPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALSampler* pSampler) override;
+  virtual void SetBufferViewPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALBufferView* pBufferView) override;
+  virtual void SetTextureViewPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALTextureView* pRTextureView) override;
+  virtual void SetUnorderedAccessBufferViewPlatform(xiiUInt32 uiSlot, const xiiGALBufferView* pUnorderedAccessBufferView) override;
+  virtual void SetUnorderedAccessTextureViewPlatform(xiiUInt32 uiSlot, const xiiGALTextureView* pUnorderedAccessTextureView) override;
+
+  // Query functions
+
+  virtual void      BeginQueryPlatform(const xiiGALQuery* pQuery) override;
+  virtual void      EndQueryPlatform(const xiiGALQuery* pQuery) override;
+  virtual xiiResult GetQueryResultPlatform(const xiiGALQuery* pQuery, void* pData) override;
+
+  // Fence functions
+
+  // Resource update functions
+
+  virtual void ClearUnorderedAccessViewPlatform(const xiiGALBufferView* pBufferView, xiiVec4 vClearValues) override;
+  virtual void ClearUnorderedAccessViewPlatform(const xiiGALTextureView* pTextureView, xiiVec4 vClearValues) override;
+  virtual void ClearUnorderedAccessViewPlatform(const xiiGALBufferView* pBufferView, xiiVec4U32 vClearValues) override;
+  virtual void ClearUnorderedAccessViewPlatform(const xiiGALTextureView* pTextureView, xiiVec4U32 vClearValues) override;
+
+  virtual void CopyBufferPlatform(const xiiGALBuffer* pDestination, const xiiGALBuffer* pSource) override;
+  virtual void CopyBufferRegionPlatform(const xiiGALBuffer* pDestination, xiiUInt32 uiDestOffset, const xiiGALBuffer* pSource, xiiUInt32 uiSourceOffset, xiiUInt32 uiByteCount) override;
+  virtual void UpdateBufferPlatform(const xiiGALBuffer* pDestination, xiiUInt32 uiDestOffset, xiiArrayPtr<const xiiUInt8> sourceData, xiiBitflags<xiiGALMapFlags> mapFlags) override;
+
+  virtual void CopyTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTexture* pSource) override;
+  virtual void CopyTextureRegionPlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiVec3U32& vDestinationPoint, const xiiGALTexture* pSource, const xiiGALTextureSubResourceData& sourceSubResource, const xiiBoundingBoxu32& box) override;
+  virtual void UpdateTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiBoundingBoxu32& destinationBox, const xiiGALTextureData& sourceData) override;
+  virtual void ResolveTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiGALTexture* pSource, const xiiGALTextureSubResourceData& sourceSubResource) override;
+  virtual void ReadbackTexturePlatform(const xiiGALTexture* pTexture, const xiiGALTexture* pStagingTexture) override;
+  virtual void CopyTextureReadbackResultPlatform(const xiiGALTexture* pTexture, const xiiGALTexture* pStagingTexture, xiiArrayPtr<xiiGALTextureSubResourceData> sourceSubResource, xiiArrayPtr<xiiGALTextureData> targetData) override;
+  virtual void GenerateMipMapsPlatform(const xiiGALTextureView* pTextureView) override;
+
+  // Miscellaneous
+
+  virtual void FlushPlatform() override;
+
+  // Debug helper functions
+
+  virtual void PushMarkerPlatform(xiiStringView sMarker) override;
+  virtual void PopMarkerPlatform() override;
+  virtual void InsertEventMarkerPlatform(xiiStringView sMarker) override;
+
+  // xiiGALCommandEncoderGraphicsPlatformInterface
+
+  // Draw functions
+
+  virtual void ClearPlatform(const xiiColor& clearColor, xiiUInt32 uiRenderTargetClearMask, bool bClearDepth, bool bClearStencil, float fDepthClear, xiiUInt8 uiStencilClear) override;
+
+  virtual void DrawPlatform(xiiUInt32 uiVertexCount, xiiUInt32 uiStartVertex) override;
+  virtual void DrawIndexedPlatform(xiiUInt32 uiIndexCount, xiiUInt32 uiStartIndex) override;
+  virtual void DrawIndexedInstancedPlatform(xiiUInt32 uiIndexCountPerInstance, xiiUInt32 uiInstanceCount, xiiUInt32 uiStartIndex) override;
+  virtual void DrawIndexedInstancedIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes) override;
+  virtual void DrawInstancedPlatform(xiiUInt32 uiVertexCountPerInstance, xiiUInt32 uiInstanceCount, xiiUInt32 uiStartVertex) override;
+  virtual void DrawInstancedIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes) override;
+
+  virtual void BeginStreamOutPlatform() override;
+  virtual void EndStreamOutPlatform() override;
+
+  // State functions
+
+  virtual void SetIndexBufferPlatform(const xiiGALBuffer* pIndexBuffer) override;
+  virtual void SetVertexBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pVertexBuffer) override;
+  virtual void SetInputLayoutPlatform(const xiiGALInputLayout* pInputLayout) override;
+  virtual void SetPrimitiveTopologyPlatform(xiiEnum<xiiGALPrimitiveTopology> topology) override;
+
+  virtual void SetBlendStatePlatform(const xiiGALBlendState* pBlendState, const xiiColor& blendFactor, xiiUInt32 uiSampleMask) override;
+  virtual void SetDepthStencilStatePlatform(const xiiGALDepthStencilState* pDepthStencilState, xiiUInt8 uiStencilRefValue) override;
+  virtual void SetRasterizerStatePlatform(const xiiGALRasterizerState* pRasterizerState) override;
+
+  virtual void SetViewportPlatform(const xiiRectFloat& rect, float fMinDepth, float fMaxDepth) override;
+  virtual void SetScissorRectPlatform(const xiiRectU32& rect) override;
+
+  virtual void SetStreamOutBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pBuffer, xiiUInt32 uiOffset) override;
+
+  // xiiGALCommandEncoderComputePlatformInterface
+
+  // Dispatch
+
+  virtual void DispatchPlatform(xiiUInt32 uiThreadGroupCountX, xiiUInt32 uiThreadGroupCountY, xiiUInt32 uiThreadGroupCountZ) override;
+  virtual void DispatchIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes) override;
+
 private:
   friend class xiiGALPassD3D12;
 
--- Source/Engine/GraphicsD3D12/CommandEncoder/Implementation/CommandEncoderD3D12.cpp
+++ Source/Engine/GraphicsD3D12/CommandEncoder/Implementation/CommandEncoderD3D12.cpp
@@ -12,4 +12,201 @@ xiiGALCommandEncoderD3D12::~xiiGALCommandEncoderD3D12()
 {
 }
 
+void xiiGALCommandEncoderD3D12::SetShaderPlatform(const xiiGALShader* pShader)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetConstantBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pBuffer)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetSamplerPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALSampler* pSampler)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetBufferViewPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALBufferView* pBufferView)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetTextureViewPlatform(xiiBitflags<xiiGALShaderStage> stage, xiiUInt32 uiSlot, const xiiGALTextureView* pRTextureView)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetUnorderedAccessBufferViewPlatform(xiiUInt32 uiSlot, const xiiGALBufferView* pUnorderedAccessBufferView)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetUnorderedAccessTextureViewPlatform(xiiUInt32 uiSlot, const xiiGALTextureView* pUnorderedAccessTextureView)
+{
+}
+
+void xiiGALCommandEncoderD3D12::BeginQueryPlatform(const xiiGALQuery* pQuery)
+{
+}
+
+void xiiGALCommandEncoderD3D12::EndQueryPlatform(const xiiGALQuery* pQuery)
+{
+}
+
+xiiResult xiiGALCommandEncoderD3D12::GetQueryResultPlatform(const xiiGALQuery* pQuery, void* pData)
+{
+  return XII_FAILURE;
+}
+
+void xiiGALCommandEncoderD3D12::ClearUnorderedAccessViewPlatform(const xiiGALBufferView* pBufferView, xiiVec4 vClearValues)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ClearUnorderedAccessViewPlatform(const xiiGALTextureView* pTextureView, xiiVec4 vClearValues)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ClearUnorderedAccessViewPlatform(const xiiGALBufferView* pBufferView, xiiVec4U32 vClearValues)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ClearUnorderedAccessViewPlatform(const xiiGALTextureView* pTextureView, xiiVec4U32 vClearValues)
+{
+}
+
+void xiiGALCommandEncoderD3D12::CopyBufferPlatform(const xiiGALBuffer* pDestination, const xiiGALBuffer* pSource)
+{
+}
+
+void xiiGALCommandEncoderD3D12::CopyBufferRegionPlatform(const xiiGALBuffer* pDestination, xiiUInt32 uiDestOffset, const xiiGALBuffer* pSource, xiiUInt32 uiSourceOffset, xiiUInt32 uiByteCount)
+{
+}
+
+void xiiGALCommandEncoderD3D12::UpdateBufferPlatform(const xiiGALBuffer* pDestination, xiiUInt32 uiDestOffset, xiiArrayPtr<const xiiUInt8> sourceData, xiiBitflags<xiiGALMapFlags> mapFlags)
+{
+}
+
+void xiiGALCommandEncoderD3D12::CopyTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTexture* pSource)
+{
+}
+
+void xiiGALCommandEncoderD3D12::CopyTextureRegionPlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiVec3U32& vDestinationPoint, const xiiGALTexture* pSource, const xiiGALTextureSubResourceData& sourceSubResource, const xiiBoundingBoxu32& box)
+{
+}
+
+void xiiGALCommandEncoderD3D12::UpdateTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiBoundingBoxu32& destinationBox, const xiiGALTextureData& sourceData)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ResolveTexturePlatform(const xiiGALTexture* pDestination, const xiiGALTextureSubResourceData& destinationSubResource, const xiiGALTexture* pSource, const xiiGALTextureSubResourceData& sourceSubResource)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ReadbackTexturePlatform(const xiiGALTexture* pTexture, const xiiGALTexture* pStagingTexture)
+{
+}
+
+void xiiGALCommandEncoderD3D12::CopyTextureReadbackResultPlatform(const xiiGALTexture* pTexture, const xiiGALTexture* pStagingTexture, xiiArrayPtr<xiiGALTextureSubResourceData> sourceSubResource, xiiArrayPtr<xiiGALTextureData> targetData)
+{
+}
+
+void xiiGALCommandEncoderD3D12::GenerateMipMapsPlatform(const xiiGALTextureView* pTextureView)
+{
+}
+
+void xiiGALCommandEncoderD3D12::FlushPlatform()
+{
+}
+
+void xiiGALCommandEncoderD3D12::PushMarkerPlatform(xiiStringView sMarker)
+{
+}
+
+void xiiGALCommandEncoderD3D12::PopMarkerPlatform()
+{
+}
+
+void xiiGALCommandEncoderD3D12::InsertEventMarkerPlatform(xiiStringView sMarker)
+{
+}
+
+void xiiGALCommandEncoderD3D12::ClearPlatform(const xiiColor& clearColor, xiiUInt32 uiRenderTargetClearMask, bool bClearDepth, bool bClearStencil, float fDepthClear, xiiUInt8 uiStencilClear)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawPlatform(xiiUInt32 uiVertexCount, xiiUInt32 uiStartVertex)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawIndexedPlatform(xiiUInt32 uiIndexCount, xiiUInt32 uiStartIndex)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawIndexedInstancedPlatform(xiiUInt32 uiIndexCountPerInstance, xiiUInt32 uiInstanceCount, xiiUInt32 uiStartIndex)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawIndexedInstancedIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawInstancedPlatform(xiiUInt32 uiVertexCountPerInstance, xiiUInt32 uiInstanceCount, xiiUInt32 uiStartVertex)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DrawInstancedIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes)
+{
+}
+
+void xiiGALCommandEncoderD3D12::BeginStreamOutPlatform()
+{
+}
+
+void xiiGALCommandEncoderD3D12::EndStreamOutPlatform()
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetIndexBufferPlatform(const xiiGALBuffer* pIndexBuffer)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetVertexBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pVertexBuffer)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetInputLayoutPlatform(const xiiGALInputLayout* pInputLayout)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetPrimitiveTopologyPlatform(xiiEnum<xiiGALPrimitiveTopology> topology)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetBlendStatePlatform(const xiiGALBlendState* pBlendState, const xiiColor& blendFactor, xiiUInt32 uiSampleMask)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetDepthStencilStatePlatform(const xiiGALDepthStencilState* pDepthStencilState, xiiUInt8 uiStencilRefValue)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetRasterizerStatePlatform(const xiiGALRasterizerState* pRasterizerState)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetViewportPlatform(const xiiRectFloat& rect, float fMinDepth, float fMaxDepth)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetScissorRectPlatform(const xiiRectU32& rect)
+{
+}
+
+void xiiGALCommandEncoderD3D12::SetStreamOutBufferPlatform(xiiUInt32 uiSlot, const xiiGALBuffer* pBuffer, xiiUInt32 uiOffset)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DispatchPlatform(xiiUInt32 uiThreadGroupCountX, xiiUInt32 uiThreadGroupCountY, xiiUInt32 uiThreadGroupCountZ)
+{
+}
+
+void xiiGALCommandEncoderD3D12::DispatchIndirectPlatform(const xiiGALBuffer* pIndirectArgumentBuffer, xiiUInt32 uiArgumentOffsetInBytes)
+{
+}
+
 XII_STATICLINK_FILE(GraphicsD3D12, GraphicsD3D12_CommandEncoder_Implementation_CommandEncoderD3D12);
