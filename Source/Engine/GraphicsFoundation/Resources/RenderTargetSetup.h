cb7aae59a50e061aa8eef1acf82a3f11921acd7a
Theophilus Eriata <theoeriata@gmail.com>
2023-11-14 09:01:56 -0700

    GraphicsFoundation: Deprecate Render Target Setup.

--- Source/Engine/GraphicsFoundation/Resources/RenderTargetSetup.h
+++ Source/Engine/GraphicsFoundation/Resources/RenderTargetSetup.h
@@ -14,46 +14,4 @@ struct XII_GRAPHICSFOUNDATION_DLL xiiGALRenderTargets
   xiiGALTextureHandle m_hDSTarget;
 };
 
-// \brief This class can be used to construct render target setups on the stack.
-class XII_GRAPHICSFOUNDATION_DLL xiiGALRenderTargetSetup
-{
-public:
-  xiiGALRenderTargetSetup();
-
-  xiiGALRenderTargetSetup& SetRenderTarget(xiiUInt8 uiIndex, xiiGALTextureViewHandle hRenderTarget);
-  xiiGALRenderTargetSetup& SetDepthStencilTarget(xiiGALTextureViewHandle hDSTarget);
-
-  bool operator==(const xiiGALRenderTargetSetup& other) const;
-  bool operator!=(const xiiGALRenderTargetSetup& other) const;
-
-  inline xiiUInt8 GetRenderTargetCount() const;
-
-  inline xiiGALTextureViewHandle GetRenderTarget(xiiUInt8 uiIndex) const;
-  inline xiiGALTextureViewHandle GetDepthStencilTarget() const;
-
-  void DestroyAllAttachedViews();
-
-protected:
-  xiiGALTextureViewHandle m_hRTs[XII_GAL_MAX_RENDERTARGET_COUNT];
-  xiiGALTextureViewHandle m_hDSTarget;
-
-  xiiUInt8 m_uiRTCount = 0;
-};
-
-struct XII_GRAPHICSFOUNDATION_DLL xiiGALRenderingSetup
-{
-  bool operator==(const xiiGALRenderingSetup& other) const;
-  bool operator!=(const xiiGALRenderingSetup& other) const;
-
-  xiiGALRenderTargetSetup m_RenderTargetSetup;
-  xiiColor                m_ClearColor              = xiiColor(0, 0, 0, 0);
-  xiiUInt32               m_uiRenderTargetClearMask = 0x0U;
-  float                   m_fDepthClear             = 1.0f;
-  xiiUInt8                m_uiStencilClear          = 0;
-  bool                    m_bClearDepth             = false;
-  bool                    m_bClearStencil           = false;
-  bool                    m_bDiscardColor           = false;
-  bool                    m_bDiscardDepth           = false;
-};
-
 #include <GraphicsFoundation/Resources/Implementation/RenderTargetSetup_inl.h>
