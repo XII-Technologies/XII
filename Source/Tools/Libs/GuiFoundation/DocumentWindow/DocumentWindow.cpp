ccd08d0c4b1462362cd338ba1975ad4793b099c0
Theophilus Eriata <theoeriata@gmail.com>
2023-11-23 12:04:27 -0700

    GUIFoundation: Fix Node Editor Shortcuts Not Working.
    Some central widgets consume the shortcut (or any key press for that matter) instead of passing it up the parent hierarchy. For example a QGraphicsView will forward any key-press to the QGraphicsScene which will consume every event. As a workaround, we overrule the central widget by default when it comes to shortcuts.

--- Source/Tools/Libs/GuiFoundation/DocumentWindow/DocumentWindow.cpp
+++ Source/Tools/Libs/GuiFoundation/DocumentWindow/DocumentWindow.cpp
@@ -307,6 +307,15 @@ bool xiiQtDocumentWindow::eventFilter(QObject* obj, QEvent* e)
       if (xiiQtProxy::TriggerDocumentAction(m_pDocument, keyEvent, e->type() == QEvent::ShortcutOverride))
         return true;
     }
+
+    // Some central widgets consume the shortcut (or any key press for that matter) instead of passing it up the parent hierarchy. For example a QGraphicsView will forward
+    // any key-press to the QGraphicsScene which will consume every event. As a workaround, we overrule the central widget by default when it comes to shortcuts.
+    if (obj == centralWidget())
+    {
+      QKeyEvent* keyEvent = static_cast<QKeyEvent*>(e);
+      if (xiiQtProxy::TriggerDocumentAction(m_pDocument, keyEvent, e->type() == QEvent::ShortcutOverride))
+        return true;
+    }
   }
   return false;
 }
@@ -324,6 +333,9 @@ bool xiiQtDocumentWindow::event(QEvent* event)
 
 void xiiQtDocumentWindow::FinishWindowCreation()
 {
+  if (centralWidget())
+    centralWidget()->installEventFilter(this);
+
   ScheduleRestoreWindowLayout();
 }
 
