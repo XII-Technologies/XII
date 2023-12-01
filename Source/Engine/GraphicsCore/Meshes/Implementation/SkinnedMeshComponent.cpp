99485b00073a3491a8df0a4c3c309dcbf8bde000
Theophilus Eriata <theoeriata@gmail.com>
2023-12-01 13:51:04 -0700

    GraphicsD3D12: Apply Compilation Fix.

--- Source/Engine/GraphicsCore/Meshes/Implementation/SkinnedMeshComponent.cpp
+++ Source/Engine/GraphicsCore/Meshes/Implementation/SkinnedMeshComponent.cpp
@@ -6,6 +6,7 @@
 #include <GraphicsCore/RenderWorld/RenderWorld.h>
 #include <GraphicsCore/Shader/Types.h>
 #include <GraphicsFoundation/Device/Device.h>
+#include <GraphicsFoundation/Resources/Buffer.h>
 
 // clang-format off
 XII_BEGIN_DYNAMIC_REFLECTED_TYPE(xiiSkinnedMeshRenderData, 1, xiiRTTIDefaultAllocator<xiiSkinnedMeshRenderData>)
@@ -44,16 +45,16 @@ void xiiSkinningState::TransformsChanged()
     if (m_Transforms.GetCount() == 0)
       return;
 
-    xiiGALBufferCreationDescription BufferDesc;
-    BufferDesc.m_uiElementByteStride = sizeof(xiiShaderTransform);
-    BufferDesc.m_uiSize              = BufferDesc.m_uiElementByteStride * m_Transforms.GetCount();
-    BufferDesc.m_Mode                = xiiGALBufferMode::Structured;
-    BufferDesc.m_BindFlags.Add(xiiGALBindFlags::ShaderResource);
+    xiiGALBufferCreationDescription bufferDescription;
+    bufferDescription.m_uiElementByteStride = sizeof(xiiShaderTransform);
+    bufferDescription.m_uiSize              = bufferDescription.m_uiElementByteStride * m_Transforms.GetCount();
+    bufferDescription.m_Mode                = xiiGALBufferMode::Structured;
+    bufferDescription.m_BindFlags.Add(xiiGALBindFlags::ShaderResource);
 
     xiiGALBufferData initData;
     initData.m_pData      = m_Transforms.GetData();
     initData.m_uiDataSize = m_Transforms.GetArrayPtr().ToByteArray().GetCount();
-    m_hGpuBuffer          = xiiGALDevice::GetDefaultDevice()->CreateBuffer(BufferDesc, &initData);
+    m_hGpuBuffer          = xiiGALDevice::GetDefaultDevice()->CreateBuffer(bufferDescription, &initData);
 
     m_bTransformsUpdated[0] = std::make_shared<bool>(true);
     m_bTransformsUpdated[1] = std::make_shared<bool>(true);
