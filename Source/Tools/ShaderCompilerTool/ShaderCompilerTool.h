56e11ac4286103d8d4999d05ee1d3ea429057276
XII Automation <automation@xiitechnologies.com>
2023-12-10 03:35:13 +0000

    Apply Code Format Rules.

--- Source/Tools/ShaderCompilerTool/ShaderCompilerTool.h
+++ Source/Tools/ShaderCompilerTool/ShaderCompilerTool.h
@@ -13,20 +13,20 @@ public:
   virtual xiiApplication::Execution Run() override;
 
 private:
-  void PrintConfig();
+  void      PrintConfig();
   xiiResult CompileShader(xiiStringView sShaderFile);
   xiiResult ExtractPermutationVarValues(xiiStringView sShaderFile);
 
   virtual xiiResult BeforeCoreSystemsStartup() override;
-  virtual void AfterCoreSystemsStartup() override;
-  virtual void Init_LoadProjectPlugins() override {}
-  virtual void Init_SetupDefaultResources() override {}
-  virtual void Init_ConfigureInput() override {}
-  virtual void Init_ConfigureTags() override {}
-  virtual bool Run_ProcessApplicationInput() override { return true; }
+  virtual void      AfterCoreSystemsStartup() override;
+  virtual void      Init_LoadProjectPlugins() override {}
+  virtual void      Init_SetupDefaultResources() override {}
+  virtual void      Init_ConfigureInput() override {}
+  virtual void      Init_ConfigureTags() override {}
+  virtual bool      Run_ProcessApplicationInput() override { return true; }
 
-  xiiPermutationGenerator m_PermutationGenerator;
-  xiiString m_sPlatforms;
-  xiiString m_sShaderFiles;
+  xiiPermutationGenerator                         m_PermutationGenerator;
+  xiiString                                       m_sPlatforms;
+  xiiString                                       m_sShaderFiles;
   xiiMap<xiiString, xiiHybridArray<xiiString, 4>> m_FixedPermVars;
 };
