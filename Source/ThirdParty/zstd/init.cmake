f7fb070620cf1b61fec6c1ad35e21f062ac9b15c
Theophilus Eriata <theoeriata@gmail.com>
2024-03-21 16:16:56 -0600

    ThirdParty: Add ZLIB Version 1.3.1.

--- Source/ThirdParty/zstd/init.cmake
+++ Source/ThirdParty/zstd/init.cmake
@@ -1,3 +1,6 @@
-### zstd
+
+# #####################################
+# ## ZSTD support
+# #####################################
 set (XII_3RDPARTY_ZSTD_SUPPORT ON CACHE BOOL "Whether to add support for zstd.")
 mark_as_advanced(FORCE XII_3RDPARTY_ZSTD_SUPPORT)
