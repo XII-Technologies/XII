44f2fc547cc588f3d581ca194b6583bc22d3e4f3
Theophilus Eriata <theoeriata@gmail.com>
2024-03-23 15:40:44 -0600

    Foundation: Add Color RGBA Conversion.

--- Source/Engine/Foundation/Math/Color8UNorm.h
+++ Source/Engine/Foundation/Math/Color8UNorm.h
@@ -29,6 +29,20 @@ public:
 
   /// \brief Conversion to xiiUInt8*
   xiiUInt8* GetData() { return &r; }
+
+  /// \brief Packs the 4 color values into a single uint32 with A in the least significant bits and R in the most significant ones.
+  [[nodiscard]] xiiUInt32 ToRGBA8() const
+  {
+    // RGBA (A at lowest address, R at highest).
+    return (static_cast<xiiUInt32>(r) << 24) + (static_cast<xiiUInt32>(g) << 16) + (static_cast<xiiUInt32>(b) << 8) + (static_cast<xiiUInt32>(a) << 0);
+  }
+
+  /// \brief Packs the 4 color values into a single uint32 with R in the least significant bits and A in the most significant ones.
+  [[nodiscard]] xiiUInt32 ToABGR8() const
+  {
+    // RGBA (A at highest address, R at lowest).
+    return (static_cast<xiiUInt32>(a) << 24) + (static_cast<xiiUInt32>(b) << 16) + (static_cast<xiiUInt32>(g) << 8) + (static_cast<xiiUInt32>(r) << 0);
+  }
 };
 
 XII_CHECK_AT_COMPILETIME(sizeof(xiiColorBaseUB) == 4);
