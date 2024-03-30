e195c902968190784cd4afdf43a6842642957036
Theophilus Eriata <theoeriata@gmail.com>
2024-03-29 22:58:02 -0600

    GraphicsD3D11: Fix Buffer View Retreival.

--- Source/Engine/GraphicsD3D11/Resources/Implementation/BufferViewD3D11_inl.h
+++ Source/Engine/GraphicsD3D11/Resources/Implementation/BufferViewD3D11_inl.h
@@ -1,5 +1,5 @@
 
-XII_ALWAYS_INLINE Diligent::IBufferView* xiiGALBufferViewD3D11::GetBufferView() const
+XII_ALWAYS_INLINE ID3D11View* xiiGALBufferViewD3D11::GetBufferView() const
 {
   return m_pBufferView;
 }
