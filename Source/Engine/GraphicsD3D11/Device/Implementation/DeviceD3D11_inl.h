88f77d73e9a6a95852cbc3f898e0c23177f8caf5
Theophilus Eriata <theoeriata@gmail.com>
2024-04-02 14:56:16 -0600

    GraphicsD3D11: Device Cleanup.
    * Disables D3D12 and Vulkan Experimental Libraries.

--- Source/Engine/GraphicsD3D11/Device/Implementation/DeviceD3D11_inl.h
+++ Source/Engine/GraphicsD3D11/Device/Implementation/DeviceD3D11_inl.h
@@ -1,5 +1,5 @@
 
-XII_ALWAYS_INLINE ID3D11Device* xiiGALDeviceD3D11::GetDeviceD3D11() const
+XII_ALWAYS_INLINE ID3D11Device* xiiGALDeviceD3D11::GetD3D11Device() const
 {
   return m_pDeviceD3D11;
 }
@@ -14,53 +14,10 @@ XII_ALWAYS_INLINE IDXGIFactory4* xiiGALDeviceD3D11::GetDXGIFactory() const
   return m_pDXGIFactory;
 }
 
-XII_ALWAYS_INLINE xiiMemoryAllocatorD3D11* xiiGALDeviceD3D11::GetD3D11Allocator() const
+XII_ALWAYS_INLINE ID3D11DeviceContext* xiiGALDeviceD3D11::GetImmediateContext()
 {
-  return m_pAllocatorD3D11.Borrow();
+  return m_pDeviceContext;
 }
-
-#if 0
-XII_ALWAYS_INLINE Diligent::IDeviceContext* xiiGALDeviceD3D11::GetImmediateContext()
-{
-  return m_pDeviceContexts[0];
-}
-
-XII_ALWAYS_INLINE Diligent::IDeviceContext* xiiGALDeviceD3D11::GetComputeContext()
-{
-  for (xiiUInt32 i = 1; i < m_pDeviceContexts.GetCount(); ++i)
-  {
-    auto pContext = m_pDeviceContexts[i];
-
-    if (pContext->GetDesc().QueueType == Diligent::COMMAND_QUEUE_TYPE_COMPUTE)
-      return pContext;
-  }
-  return nullptr;
-}
-
-XII_ALWAYS_INLINE Diligent::IDeviceContext* xiiGALDeviceD3D11::GetTransferContext()
-{
-  for (xiiUInt32 i = 1; i < m_pDeviceContexts.GetCount(); ++i)
-  {
-    auto pContext = m_pDeviceContexts[i];
-
-    if (pContext->GetDesc().QueueType == Diligent::COMMAND_QUEUE_TYPE_TRANSFER)
-      return pContext;
-  }
-  return nullptr;
-}
-
-XII_ALWAYS_INLINE Diligent::IDeviceContext* xiiGALDeviceD3D11::GetSparseBindingContext()
-{
-  for (xiiUInt32 i = 1; i < m_pDeviceContexts.GetCount(); ++i)
-  {
-    auto pContext = m_pDeviceContexts[i];
-
-    if (pContext->GetDesc().QueueType == Diligent::COMMAND_QUEUE_TYPE_SPARSE_BINDING)
-      return pContext;
-  }
-  return nullptr;
-}
-#endif
 
 XII_ALWAYS_INLINE const xiiGALFormatLookupTableD3D11& xiiGALDeviceD3D11::GetFormatLookupTable() const
 {
