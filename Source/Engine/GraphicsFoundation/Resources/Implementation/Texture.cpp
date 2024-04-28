e4d62e20d94216657384b676f3c3cfdd794f19cb
Theophilus Eriata <theoeriata@gmail.com>
2024-04-28 00:14:08 -0600

    GraphicsFoundation: Fix Texture Cube Default Views Creation.

--- Source/Engine/GraphicsFoundation/Resources/Implementation/Texture.cpp
+++ Source/Engine/GraphicsFoundation/Resources/Implementation/Texture.cpp
@@ -30,43 +30,14 @@ void xiiGALTexture::CreateDefaultResourceViews(xiiGALTextureHandle hTexture)
   xiiGALTextureViewCreationDescription viewDescription;
   viewDescription.m_hTexture                  = hTexture;
   viewDescription.m_Format                    = m_Description.m_Format;
+  viewDescription.m_ResourceDimension         = m_Description.m_Type;
   viewDescription.m_uiMostDetailedMip         = 0U;
   viewDescription.m_uiFirstArrayOrDepthSlice  = 0U;
   viewDescription.m_uiMipLevelCount           = 0U;
   viewDescription.m_uiArrayOrDepthSlicesCount = 0U;
 
-  switch (m_Description.m_Type)
-  {
-    case xiiGALResourceDimension::Texture1D:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture1D;
-      break;
-
-    case xiiGALResourceDimension::Texture1DArray:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture1DArray;
-      break;
-
-    case xiiGALResourceDimension::Texture2D:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture2D;
-      break;
-
-    case xiiGALResourceDimension::Texture2DArray:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture2DArray;
-      break;
-
-    case xiiGALResourceDimension::Texture3D:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture3D;
-      break;
-
-    case xiiGALResourceDimension::TextureCube:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::TextureCube;
-      break;
-
-    case xiiGALResourceDimension::TextureCubeArray:
-      viewDescription.m_ResourceDimension = xiiGALResourceDimension::TextureCubeArray;
-      break;
-
-      XII_DEFAULT_CASE_NOT_IMPLEMENTED;
-  }
+  if (m_Description.m_Type == xiiGALResourceDimension::TextureCube || m_Description.m_Type == xiiGALResourceDimension::TextureCubeArray)
+    viewDescription.m_ResourceDimension = xiiGALResourceDimension::Texture2DArray;
 
   if (m_Description.m_BindFlags.IsSet(xiiGALBindFlags::ShaderResource))
   {
@@ -83,7 +54,6 @@ void xiiGALTexture::CreateDefaultResourceViews(xiiGALTextureHandle hTexture)
   {
     auto rtViewDescription                                     = viewDescription;
     rtViewDescription.m_ViewType                               = xiiGALTextureViewType::RenderTarget;
-    rtViewDescription.m_ResourceDimension                      = xiiGALResourceDimension::Texture2DArray;
     m_DefaultTextureViews[xiiGALTextureViewType::RenderTarget] = m_pDevice->CreateTextureView(rtViewDescription);
   }
   if (m_Description.m_BindFlags.IsSet(xiiGALBindFlags::DepthStencil))
@@ -98,7 +68,6 @@ void xiiGALTexture::CreateDefaultResourceViews(xiiGALTextureHandle hTexture)
   {
     auto uavViewDescription                                       = viewDescription;
     uavViewDescription.m_ViewType                                 = xiiGALTextureViewType::UnorderedAccess;
-    uavViewDescription.m_ResourceDimension                        = xiiGALResourceDimension::Texture2DArray;
     m_DefaultTextureViews[xiiGALTextureViewType::UnorderedAccess] = m_pDevice->CreateTextureView(uavViewDescription);
   }
   if (m_Description.m_BindFlags.IsSet(xiiGALBindFlags::ShadingRate))
