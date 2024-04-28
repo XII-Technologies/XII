b8a05e6f6ce4052b262df1afbd6a8fa6e2d4d568
Theophilus Eriata <theoeriata@gmail.com>
2024-04-27 23:41:11 -0600

    Shaders: Fix Compile Errors With Permutation Generation.
    Compiling all shaders succeeds.
    * ShaderCompilerTool.exe -renderer D3D11 -platform D3D11_SM50 -project C:\XII\Data\Base -shader Shaders

--- Data/Base/Shaders/Materials/MaterialPixelShaderSimplified.h
+++ Data/Base/Shaders/Materials/MaterialPixelShaderSimplified.h
@@ -1,16 +1,21 @@
 #pragma once
 
-#if RENDER_PASS != RENDER_PASS_FORWARD
-  #error "MaterialPixelShaderSimplified supports only the forward render pass"
-#endif
-
 #if BLEND_MODE == BLEND_MODE_MASKED && RENDER_PASS != RENDER_PASS_WIREFRAME
 
-  #if defined(MSAA)
-    #if MSAA == TRUE
-      #define USE_ALPHA_TEST_SUPER_SAMPLING
-    #endif
-  #endif
+// No need to do alpha test again if we have a depth prepass
+#  if defined(FORWARD_PASS_WRITE_DEPTH) && (RENDER_PASS == RENDER_PASS_FORWARD || RENDER_PASS == RENDER_PASS_EDITOR)
+#    if FORWARD_PASS_WRITE_DEPTH == TRUE
+#      define USE_ALPHA_TEST
+#    endif
+#  else
+#    define USE_ALPHA_TEST
+#  endif
+
+#  if defined(USE_ALPHA_TEST) && defined(MSAA)
+#    if MSAA == TRUE
+#      define USE_ALPHA_TEST_SUPER_SAMPLING
+#    endif
+#  endif
 
 #endif
 
@@ -19,8 +24,10 @@
 
 struct PS_OUT
 {
-  float4 Color : SV_Target;
-  
+  #if RENDER_PASS != RENDER_PASS_DEPTH_ONLY
+    float4 Color : SV_Target;
+  #endif
+
   #if defined(USE_ALPHA_TEST_SUPER_SAMPLING)
     uint Coverage : SV_Coverage;
   #endif
@@ -33,10 +40,13 @@ PS_OUT main(PS_IN Input)
 #endif
 
   G.Input = Input;
+  #if defined(CUSTOM_GLOBALS)
+    FillCustomGlobals();
+  #endif
 
   PS_OUT Output;
 
-  #if BLEND_MODE == BLEND_MODE_MASKED
+  #if defined(USE_ALPHA_TEST)
     uint coverage = CalculateCoverage();
     if (coverage == 0)
     {
@@ -48,7 +58,8 @@ PS_OUT main(PS_IN Input)
   #endif
 
   xiiMaterialData matData = FillMaterialData();
-  
+  uint gameObjectId = GetInstanceData().GameObjectID;
+
   #if SHADING_MODE == SHADING_MODE_LIT
     AccumulatedLight light = CalculateLightingSimplified(matData);
   #else
@@ -61,6 +72,14 @@ PS_OUT main(PS_IN Input)
   #if RENDER_PASS == RENDER_PASS_FORWARD
 
     Output.Color = float4(litColor, matData.opacity);
+
+  #elif RENDER_PASS == RENDER_PASS_EDITOR
+    Output.Color = float4(litColor, matData.opacity);
+  #elif RENDER_PASS == RENDER_PASS_WIREFRAME
+    Output.Color = float4(litColor, matData.opacity);
+  #elif (RENDER_PASS == RENDER_PASS_PICKING || RENDER_PASS == RENDER_PASS_PICKING_WIREFRAME)
+    Output.Color = RGBA8ToFloat4(gameObjectId);
+  #elif RENDER_PASS == RENDER_PASS_DEPTH_ONLY
 
   #else
     Output.Color = float4(litColor, matData.opacity);
