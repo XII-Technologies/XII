4b3ae6de4d13e1c0bb163ac2892c28429d3a7060
Theophilus Eriata <theoeriata@gmail.com>
2024-05-01 00:07:47 -0600

    GraphicsCore: Add Default Sampler Configuration To AO Pass.

--- Source/Engine/GraphicsCore/Pipeline/Implementation/Passes/AOPass.cpp
+++ Source/Engine/GraphicsCore/Pipeline/Implementation/Passes/AOPass.cpp
@@ -373,13 +373,18 @@ void xiiAOPass::CreateSampler()
   if (m_hSSAOSampler.IsInvalidated())
   {
     xiiGALSamplerCreationDescription desc;
-    desc.m_MinFilter   = xiiGALFilterType::Point;
-    desc.m_MagFilter   = xiiGALFilterType::Point;
-    desc.m_MipFilter   = xiiGALFilterType::Point;
-    desc.m_AddressU    = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
-    desc.m_AddressV    = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
-    desc.m_AddressW    = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
-    desc.m_BorderColor = xiiColor::White * m_fFadeOutEnd;
+    desc.m_MinFilter          = xiiGALFilterType::Point;
+    desc.m_MagFilter          = xiiGALFilterType::Point;
+    desc.m_MipFilter          = xiiGALFilterType::Point;
+    desc.m_AddressU           = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
+    desc.m_AddressV           = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
+    desc.m_AddressW           = xiiTextureUtils::GALTextureAddressMode(xiiImageAddressMode::ClampBorder);
+    desc.m_BorderColor        = xiiColor::White * m_fFadeOutEnd;
+    desc.m_ComparisonFunction = xiiGALComparisonFunction::Never;
+    desc.m_fMipLODBias        = 0.0f;
+    desc.m_fMinLOD            = -1.0f;
+    desc.m_fMaxLOD            = 42000.0f;
+    desc.m_uiMaxAnisotropy    = 4U;
 
     m_hSSAOSampler = xiiGALDevice::GetDefaultDevice()->CreateSampler(desc);
   }
