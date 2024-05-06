9502b537145afeb96689e08cd919e7a81b893890
Theophilus Eriata <theoeriata@gmail.com>
2024-05-05 23:12:05 -0600

    GraphicsD3D12: Apply GAL Fixes To Fence.

--- Source/Engine/GraphicsD3D12/GraphicsD3D12PCH.h
+++ Source/Engine/GraphicsD3D12/GraphicsD3D12PCH.h
@@ -5,8 +5,10 @@
 
 #include <Foundation/Basics/Platform/Win/IncludeWindows.h>
 
-#include <GraphicsD3D12/MemoryAllocator/MemoryAllocator.h>
+#include <GraphicsD3D12/MemoryAllocator/MemoryAllocatorD3D12.h>
 
 #include <GraphicsD3D12/Utilities/D3D12TypeConversions.h>
 
 #include <D3D12MemoryAllocator/D3D12MemAlloc.h>
+
+#include <Foundation/Basics/Platform/Win/HResultUtils.h>
--- Source/Engine/GraphicsD3D12/Resources/Implementation/FenceD3D12_inl.h
+++ Source/Engine/GraphicsD3D12/Resources/Implementation/FenceD3D12_inl.h
@@ -1,40 +1,5 @@
 
-XII_ALWAYS_INLINE xiiUInt64 xiiGALFenceD3D12::GetCompletedValue()
+XII_ALWAYS_INLINE ID3D12Fence* xiiGALFenceD3D12::GetD3D12Fence() const
 {
-  xiiUInt64 uiValue = m_pFence->GetCompletedValue();
-  XII_ASSERT_DEV(uiValue != xiiMath::MaxValue<xiiUInt64>(), "If the device has been removed, the return value will be UINT64_MAX.");
-  return uiValue;
-}
-
-XII_ALWAYS_INLINE void xiiGALFenceD3D12::Signal(xiiUInt64 uiValue)
-{
-  XII_ASSERT_DEV(m_Description.m_Type == xiiGALFenceType::General, "The fence must be created with xiiGALFenceType::General.");
-  XII_ASSERT_DEV(m_pDevice->GetDescription().m_DeviceFeatures.m_NativeFence == xiiGALDeviceFeatureState::Enabled, "CPU fence signal requires the device Native Fence feature.");
-
-  m_pFence->Signal(uiValue);
-}
-
-XII_ALWAYS_INLINE void xiiGALFenceD3D12::Wait(xiiUInt64 uiValue)
-{
-  if (GetCompletedValue() >= uiValue)
-    return;
-
-  if (m_pFenceCompleteEvent != nullptr)
-  {
-    m_pFence->SetEventOnCompletion(uiValue, m_pFenceCompleteEvent);
-
-    WaitForSingleObject(m_pFenceCompleteEvent, INFINITE);
-  }
-  else
-  {
-    while (GetCompletedValue() < uiValue)
-    {
-      xiiThreadUtils::Sleep(xiiTime::Microseconds(1));
-    }
-  }
-}
-
-XII_ALWAYS_INLINE ID3D12Fence* xiiGALFenceD3D12::GetFence() const
-{
-  return m_pFence;
+  return m_pD3D12Fence;
 }
