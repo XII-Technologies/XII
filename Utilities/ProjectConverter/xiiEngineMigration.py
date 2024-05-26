d182dcc3dd7b131b0855ef3d0aacbc5378fc1ee6
Theophilus Eriata <theoeriata@gmail.com>
2024-05-26 10:52:08 -0600

    Utilities: Add More Code Format Rules.

--- Utilities/ProjectConverter/xiiEngineMigration.py
+++ Utilities/ProjectConverter/xiiEngineMigration.py
@@ -281,7 +281,7 @@ def ResolveNamespace() -> None:
                     uiIndexAdvancement += 1
 
             # Cleanup multiline function arguments, clang-format does not clean them up.
-                # We do not apply code format rules on comments. (Block comments are unhandled)
+            # We do not apply code format rules on comments. (Block comments are unhandled)
             if (rSearch := re.search(r"[a-zA-Z0-9]+\s+[a-zA-Z0-9_]+\(+([a-zA-Z0-9\s<>&*:,.\(\)])+$", sLineContent)) and rSearch != None and not sLineContent.strip().startswith("//") and not sLineContent.strip().startswith("/*") and not sLineContent.strip().startswith("*") and not sLineContent.strip().startswith("#"):
                 sLineParse: str = sLineContent
 
@@ -300,6 +300,72 @@ def ResolveNamespace() -> None:
                     sLineParse += sProcessedLine
 
                     if sProcessedLine.endswith('(') or sProcessedLine.endswith('{') or sProcessedLine.endswith(';') or sProcessedLine.endswith('[tested]'):
+                        sLineContent       = sLineParse
+                        uiCurrentLine     += uiIndexAdvancement
+                        uiNextLinesToSkip += uiIndexAdvancement
+                        logger.info(f"Applied code format rules: {sLineContent}")
+                        break
+
+                    # Max depth
+                    if uiIndexAdvancement > 50:
+                        logger.info(f"Failed to apply code format rules: {sLineContent}")
+                        break
+
+                    uiIndexAdvancement += 1
+
+            # Cleanup multiline function arguments, clang-format does not clean them up.
+            # We do not apply code format rules on comments. (Block comments are unhandled)
+            if (rSearch := re.search(r"[a-zA-Z0-9]+::+[a-zA-Z0-9_]+\($", sLineContent)) and rSearch != None and not sLineContent.strip().startswith("//") and not sLineContent.strip().startswith("/*") and not sLineContent.strip().startswith("*") and not sLineContent.strip().startswith("#"):
+                sLineParse: str = sLineContent
+
+                uiIndexAdvancement: int = 1
+                while (uiCurrentLine + uiIndexAdvancement) < len(fileContent):
+                    sProcessedLine: str = GetResolvedLineContent(fileContent[uiCurrentLine + uiIndexAdvancement].strip())
+
+                    # Empty lines are ambiguous at the moment.
+                    if len(sProcessedLine) == 0:
+                        break
+
+                    # We do not apply code format rules on comments. (Block comments are unhandled)
+                    if sProcessedLine.strip().startswith("//"):
+                        break
+
+                    sLineParse += sProcessedLine
+
+                    if sProcessedLine.endswith('(') or sProcessedLine.endswith('{') or sProcessedLine.endswith(';') or sProcessedLine.endswith('[tested]'):
+                        sLineContent       = sLineParse
+                        uiCurrentLine     += uiIndexAdvancement
+                        uiNextLinesToSkip += uiIndexAdvancement
+                        logger.info(f"Applied code format rules: {sLineContent}")
+                        break
+
+                    # Max depth
+                    if uiIndexAdvancement > 50:
+                        logger.info(f"Failed to apply code format rules: {sLineContent}")
+                        break
+
+                    uiIndexAdvancement += 1
+
+            # Cleanup multiline function arguments, clang-format does not clean them up.
+            # We do not apply code format rules on comments. (Block comments are unhandled)
+            if (rSearch := re.search(r"XII_ASSERT_(DEBUG|DEV|RELEASE|ALWAYS)\($", sLineContent)) and rSearch != None and not sLineContent.strip().startswith("//") and not sLineContent.strip().startswith("/*") and not sLineContent.strip().startswith("*") and not sLineContent.strip().startswith("#"):
+                sLineParse: str = sLineContent
+
+                uiIndexAdvancement: int = 1
+                while (uiCurrentLine + uiIndexAdvancement) < len(fileContent):
+                    sProcessedLine: str = GetResolvedLineContent(fileContent[uiCurrentLine + uiIndexAdvancement].strip())
+
+                    # Empty lines are ambiguous at the moment.
+                    if len(sProcessedLine) == 0:
+                        break
+
+                    # We do not apply code format rules on comments. (Block comments are unhandled)
+                    if sProcessedLine.strip().startswith("//"):
+                        break
+
+                    sLineParse += sProcessedLine
+
+                    if sProcessedLine.endswith(';'):
                         sLineContent       = sLineParse
                         uiCurrentLine     += uiIndexAdvancement
                         uiNextLinesToSkip += uiIndexAdvancement
