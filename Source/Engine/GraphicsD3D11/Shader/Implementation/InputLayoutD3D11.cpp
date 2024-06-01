cb3cbd5c09b9e6355967e9c61c6461cbd5f78827
Theophilus Eriata <theoeriata@gmail.com>
2024-06-01 10:01:03 -0600

    GraphicsD3D11: Migrate To xiiGALShaderType.

--- Source/Engine/GraphicsD3D11/Shader/Implementation/InputLayoutD3D11.cpp
+++ Source/Engine/GraphicsD3D11/Shader/Implementation/InputLayoutD3D11.cpp
@@ -31,9 +31,9 @@ xiiGALInputLayoutD3D11::~xiiGALInputLayoutD3D11() = default;
 xiiResult xiiGALInputLayoutD3D11::InitPlatform()
 {
   xiiGALDeviceD3D11* pDeviceD3D11 = static_cast<xiiGALDeviceD3D11*>(m_pDevice);
-  xiiGALShaderD3D11* pShaderD3D11 = static_cast<xiiGALShaderD3D11*>(pDeviceD3D11->GetShader(m_Description.m_hShader));
+  xiiGALShaderD3D11* pShaderD3D11 = static_cast<xiiGALShaderD3D11*>(pDeviceD3D11->GetShader(m_Description.m_hVertexShader));
 
-  if (pShaderD3D11 == nullptr || !pShaderD3D11->GetDescription().HasByteCodeForStage(xiiGALShaderType::Vertex))
+  if (pShaderD3D11 == nullptr || !pShaderD3D11->GetDescription().HasValidByteCode() || pShaderD3D11->GetDescription().m_ShaderType != xiiGALShaderType::Vertex)
   {
     xiiLog::Error("Shader is invalid, or does not have Vertex shader bytecode.");
     return XII_FAILURE;
@@ -63,7 +63,7 @@ xiiResult xiiGALInputLayoutD3D11::InitPlatform()
     }
   }
 
-  auto& byteCode = pShaderD3D11->GetDescription().m_ByteCodes[xiiGALShaderType::GetStageIndex(xiiGALShaderType::Vertex)];
+  const auto& byteCode = pShaderD3D11->GetDescription().m_ByteCode;
   if (FAILED(pDeviceD3D11->GetD3D11Device()->CreateInputLayout(inputElementDescriptions.GetData(), inputElementDescriptions.GetCount(), byteCode->GetByteCode(), byteCode->GetSize(), &m_pInputLayout)))
   {
     xiiLog::Error("Failed to create the Direct3D11 input layout.");
