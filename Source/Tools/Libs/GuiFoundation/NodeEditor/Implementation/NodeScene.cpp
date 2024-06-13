af5616483ad89aa7db728cc90c6ba10ff1076b27
XII Automation <automation@xiitechnologies.com>
2024-06-13 17:47:51 +0000

    Apply Code Format Rules.

--- Source/Tools/Libs/GuiFoundation/NodeEditor/Implementation/NodeScene.cpp
+++ Source/Tools/Libs/GuiFoundation/NodeEditor/Implementation/NodeScene.cpp
@@ -355,7 +355,7 @@ void xiiQtNodeScene::keyPressEvent(QKeyEvent* event)
   }
 
   // Pass Shortcuts/KeyPresses up the chain again, so e.g. Ctrl+S work even if inside a window.
-  if(event->type() == QEvent::ShortcutOverride || event->type() == QEvent::KeyPress)
+  if (event->type() == QEvent::ShortcutOverride || event->type() == QEvent::KeyPress)
   {
     event->ignore();
   }
