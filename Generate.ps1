e6fd847500c9155f6a2987b097415665ed4af901
Theophilus Eriata <theoeriata@gmail.com>
2024-07-06 07:45:35 -0600

    BuildSystem: Enable Vulkan And D3D12.

--- Generate.ps1
+++ Generate.ps1
@@ -1,59 +1,59 @@
 param
 (
-    [Parameter(Mandatory = $True)] [ValidateSet('Win64vs2022', 'Uwp64vs2022')][string] $Target,
-    [switch]$NoUnityBuild,
-    [switch]$NoSubmoduleUpdate,
-    [string]$SolutionName
+  [Parameter(Mandatory = $True)] [ValidateSet('Win64vs2022', 'Uwp64vs2022')][string] $Target,
+  [switch]$NoUnityBuild,
+  [switch]$NoSubmoduleUpdate,
+  [string]$SolutionName
 )
 
 Set-Location $PSScriptRoot
 
 if ($NoSubmoduleUpdate -eq $False) {
-    $CURRENT_COMMIT = git log -n 1 --format=%H
+  $CURRENT_COMMIT = git log -n 1 --format=%H
 
-    Write-Host "Current commit: $CURRENT_COMMIT"
+  Write-Host "Current commit: $CURRENT_COMMIT"
 
-    $UPDATE_SUBMODULES = $True
-    $LAST_UPDATE_FILE = "$PSScriptRoot\Data\Content\AssetCache\LastSubmoduleUpdate.txt"
+  $UPDATE_SUBMODULES = $True
+  $LAST_UPDATE_FILE = "$PSScriptRoot\Data\Content\AssetCache\LastSubmoduleUpdate.txt"
 
-    if (Test-Path $LAST_UPDATE_FILE -PathType Leaf -ErrorAction SilentlyContinue) {
-        $LAST_COMMIT = Get-Content -Path $LAST_UPDATE_FILE
+  if (Test-Path $LAST_UPDATE_FILE -PathType Leaf -ErrorAction SilentlyContinue) {
+    $LAST_COMMIT = Get-Content -Path $LAST_UPDATE_FILE
 
-        if ($CURRENT_COMMIT -eq $LAST_COMMIT) {
-            Write-Host "Submodules already up-to-date."
-            $UPDATE_SUBMODULES = $False
-        }
-        else {
-            Write-Host "Submodules were last updated at commit: $LAST_COMMIT"
-        }
+    if ($CURRENT_COMMIT -eq $LAST_COMMIT) {
+      Write-Host "Submodules already up-to-date."
+      $UPDATE_SUBMODULES = $False
     }
-
-    if ($UPDATE_SUBMODULES) {
-        Write-Host "Updating submodules"
-
-        git submodule init
-        git submodule update
-
-        Out-File ( New-Item -Path $LAST_UPDATE_FILE -Force) -InputObject $CURRENT_COMMIT
+    else {
+      Write-Host "Submodules were last updated at commit: $LAST_COMMIT"
     }
+  }
+
+  if ($UPDATE_SUBMODULES) {
+    Write-Host "Updating submodules"
+
+    git submodule init
+    git submodule update
+
+    Out-File ( New-Item -Path $LAST_UPDATE_FILE -Force) -InputObject $CURRENT_COMMIT
+  }
 }
 
 $CMAKE_ARGS = @("-S", "$PSScriptRoot")
 
 if ($NoUnityBuild) {
-    $CMAKE_ARGS += "-DXII_ENABLE_FOLDER_UNITY_FILES:BOOL=OFF"
+  $CMAKE_ARGS += "-DXII_ENABLE_FOLDER_UNITY_FILES:BOOL=OFF"
 }
 else {
-    $CMAKE_ARGS += "-DXII_ENABLE_FOLDER_UNITY_FILES:BOOL=ON"
+  $CMAKE_ARGS += "-DXII_ENABLE_FOLDER_UNITY_FILES:BOOL=ON"
 }
 
 if ($SolutionName -ne "") {
-    $CMAKE_ARGS += "-XII_SOLUTION_NAME:STRING='$SolutionName'"
+  $CMAKE_ARGS += "-XII_SOLUTION_NAME:STRING='$SolutionName'"
 }
 
 $CMAKE_ARGS += "-DXII_BUILD_D3D11:BOOL=ON"
-$CMAKE_ARGS += "-DXII_BUILD_D3D12:BOOL=OFF"
-$CMAKE_ARGS += "-DXII_BUILD_VULKAN:BOOL=OFF"
+$CMAKE_ARGS += "-DXII_BUILD_D3D12:BOOL=ON"
+$CMAKE_ARGS += "-DXII_BUILD_VULKAN:BOOL=ON"
 
 $CMAKE_ARGS += "-G"
 
@@ -61,30 +61,30 @@ Write-Host ""
 
 if ($Target -eq "Win64vs2022") {
 
-    Write-Host "=== Generating Solution for Visual Studio 2022 x64 ==="
+  Write-Host "=== Generating Solution for Visual Studio 2022 x64 ==="
 
-    $CMAKE_ARGS += "Visual Studio 17 2022"
-    $CMAKE_ARGS += "-A"
-    $CMAKE_ARGS += "x64"
-    $CMAKE_ARGS += "-B"
-    $CMAKE_ARGS += "$PSScriptRoot\Workspace\vs2022x64"
+  $CMAKE_ARGS += "Visual Studio 17 2022"
+  $CMAKE_ARGS += "-A"
+  $CMAKE_ARGS += "x64"
+  $CMAKE_ARGS += "-B"
+  $CMAKE_ARGS += "$PSScriptRoot\Workspace\vs2022x64"
 }
 elseif ($Target -eq "Uwp64vs2022") {
 
-    Write-Host "=== Generating Solution for Visual Studio 2022 x64 UWP ==="
+  Write-Host "=== Generating Solution for Visual Studio 2022 x64 UWP ==="
 
-    $CMAKE_ARGS += "Visual Studio 17 2022"
-    $CMAKE_ARGS += "-A"
-    $CMAKE_ARGS += "x64"
-    $CMAKE_ARGS += "-B"
-    $CMAKE_ARGS += "$PSScriptRoot\Workspace\vs2022x64uwp"
-    $CMAKE_ARGS += "-DCMAKE_TOOLCHAIN_FILE=$PSScriptRoot\Source\BuildSystem\CMake\toolchain-winstore.cmake"
+  $CMAKE_ARGS += "Visual Studio 17 2022"
+  $CMAKE_ARGS += "-A"
+  $CMAKE_ARGS += "x64"
+  $CMAKE_ARGS += "-B"
+  $CMAKE_ARGS += "$PSScriptRoot\Workspace\vs2022x64uwp"
+  $CMAKE_ARGS += "-DCMAKE_TOOLCHAIN_FILE=$PSScriptRoot\Source\BuildSystem\CMake\toolchain-winstore.cmake"
 
-    $CMAKE_ARGS += "-DXII_ENABLE_QT_SUPPORT:BOOL=OFF"
-    $CMAKE_ARGS += "-DXII_BUILD_FILTER='UwpProjects'"
+  $CMAKE_ARGS += "-DXII_ENABLE_QT_SUPPORT:BOOL=OFF"
+  $CMAKE_ARGS += "-DXII_BUILD_FILTER='UwpProjects'"
 }
 else {
-    throw "Unknown target '$Target'."
+  throw "Unknown target '$Target'."
 }
 
 Write-Host ""
@@ -93,5 +93,5 @@ Write-Host ""
 &Data\Tools\Precompiled\cmake\bin\cmake.exe $CMAKE_ARGS
 
 if (!$?) {
-    throw "CMake failed with exit code '$LASTEXITCODE'."
+  throw "CMake failed with exit code '$LASTEXITCODE'."
 }
