195c9e677e207c81ff5c7a19e9f9cf66ace7b1d5
Theophilus Eriata <theoeriata@gmail.com>
2024-12-24 19:15:49 -0700

    TexConv: Apply Updates And Improvements.

--- Source/Tools/TexConv/ParseUtils.cpp
+++ Source/Tools/TexConv/ParseUtils.cpp
@@ -2,16 +2,16 @@
 
 #include <TexConv/TexConv.h>
 
-xiiResult xiiTexConv::ParseUIntOption(const char* szOption, xiiInt32 iMinValue, xiiInt32 iMaxValue, xiiUInt32& ref_uiResult) const
+xiiResult xiiTexConv::ParseUIntOption(xiiStringView sOption, xiiInt32 iMinValue, xiiInt32 iMaxValue, xiiUInt32& ref_uiResult) const
 {
   const auto      pCmd      = xiiCommandLineUtils::GetGlobalInstance();
   const xiiUInt32 uiDefault = ref_uiResult;
 
-  const xiiInt32 val = pCmd->GetIntOption(szOption, ref_uiResult);
+  const xiiInt32 val = pCmd->GetIntOption(sOption, ref_uiResult);
 
   if (!xiiMath::IsInRange(val, iMinValue, iMaxValue))
   {
-    xiiLog::Error("'{}' value {} is out of valid range [{}; {}]", szOption, val, iMinValue, iMaxValue);
+    xiiLog::Error("'{}' value {} is out of valid range [{}; {}]", sOption, val, iMinValue, iMaxValue);
     return XII_FAILURE;
   }
 
@@ -19,84 +19,84 @@ xiiResult xiiTexConv::ParseUIntOption(const char* szOption, xiiInt32 iMinValue, 
 
   if (ref_uiResult == uiDefault)
   {
-    xiiLog::Info("Using default '{}': '{}'.", szOption, ref_uiResult);
+    xiiLog::Info("Using default '{}': '{}'.", sOption, ref_uiResult);
     return XII_SUCCESS;
   }
 
-  xiiLog::Info("Selected '{}': '{}'.", szOption, ref_uiResult);
+  xiiLog::Info("Selected '{}': '{}'.", sOption, ref_uiResult);
 
   return XII_SUCCESS;
 }
 
-xiiResult xiiTexConv::ParseStringOption(const char* szOption, const xiiDynamicArray<KeyEnumValuePair>& allowed, xiiInt32& ref_iResult) const
+xiiResult xiiTexConv::ParseStringOption(xiiStringView sOption, const xiiDynamicArray<KeyEnumValuePair>& allowed, xiiInt32& ref_iResult) const
 {
   const auto             pCmd   = xiiCommandLineUtils::GetGlobalInstance();
-  const xiiStringBuilder sValue = pCmd->GetStringOption(szOption, 0);
+  const xiiStringBuilder sValue = pCmd->GetStringOption(sOption, 0);
 
   if (sValue.IsEmpty())
   {
     ref_iResult = allowed[0].m_iEnumValue;
 
-    xiiLog::Info("Using default '{}': '{}'", szOption, allowed[0].m_szKey);
+    xiiLog::Info("Using default '{}': '{}'", sOption, allowed[0].m_sKey);
     return XII_SUCCESS;
   }
 
   for (xiiUInt32 i = 0; i < allowed.GetCount(); ++i)
   {
-    if (sValue.IsEqual_NoCase(allowed[i].m_szKey))
+    if (sValue.IsEqual_NoCase(allowed[i].m_sKey))
     {
       ref_iResult = allowed[i].m_iEnumValue;
 
-      xiiLog::Info("Selected '{}': '{}'", szOption, allowed[i].m_szKey);
+      xiiLog::Info("Selected '{}': '{}'", sOption, allowed[i].m_sKey);
       return XII_SUCCESS;
     }
   }
 
-  xiiLog::Error("Unknown value for option '{}': '{}'.", szOption, sValue);
+  xiiLog::Error("Unknown value for option '{}': '{}'.", sOption, sValue);
 
-  PrintOptionValues(szOption, allowed);
+  PrintOptionValues(sOption, allowed);
 
   return XII_FAILURE;
 }
 
-void xiiTexConv::PrintOptionValues(const char* szOption, const xiiDynamicArray<KeyEnumValuePair>& allowed) const
+void xiiTexConv::PrintOptionValues(xiiStringView sOption, const xiiDynamicArray<KeyEnumValuePair>& allowed) const
 {
-  xiiLog::Info("Valid values for option '{}' are:", szOption);
+  xiiLog::Info("Valid values for option '{}' are:", sOption);
 
   for (xiiUInt32 i = 0; i < allowed.GetCount(); ++i)
   {
-    xiiLog::Info("  {}", allowed[i].m_szKey);
+    xiiLog::Info("  {}", allowed[i].m_sKey);
   }
 }
 
-void xiiTexConv::PrintOptionValuesHelp(const char* szOption, const xiiDynamicArray<KeyEnumValuePair>& allowed) const
+void xiiTexConv::PrintOptionValuesHelp(xiiStringView sOption, const xiiDynamicArray<KeyEnumValuePair>& allowed) const
 {
-  xiiStringBuilder out(szOption, " ");
+  xiiStringBuilder out(sOption, " ");
 
   for (xiiUInt32 i = 0; i < allowed.GetCount(); ++i)
   {
     if (i > 0)
       out.Append(" | ");
 
-    out.Append(allowed[i].m_szKey);
+    out.Append(allowed[i].m_sKey);
   }
 
   xiiLog::Info(out);
 }
 
-bool xiiTexConv::ParseFile(const char* szOption, xiiString& ref_sResult) const
+bool xiiTexConv::ParseFile(xiiStringView sOption, xiiString& ref_sResult) const
 {
   const auto pCmd = xiiCommandLineUtils::GetGlobalInstance();
-  ref_sResult     = pCmd->GetAbsolutePathOption(szOption);
+  ref_sResult     = pCmd->GetAbsolutePathOption(sOption);
 
   if (!ref_sResult.IsEmpty())
   {
-    xiiLog::Info("'{}' file: '{}'", szOption, ref_sResult);
+    xiiLog::Info("'{}' file: '{}'", sOption, ref_sResult);
     return true;
   }
   else
   {
-    xiiLog::Info("No '{}' file specified.", szOption);
+    xiiLog::Info("No '{}' file specified.", sOption);
     return false;
   }
 }
