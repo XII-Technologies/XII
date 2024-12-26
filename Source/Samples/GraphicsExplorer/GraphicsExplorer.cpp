3f06b107372640fcfcc3f66c832b3e3a3240c0c6
Theophilus Eriata <theoeriata@gmail.com>
2024-12-26 13:21:51 -0700

    GraphicsExplorer: Fix Incorrect Clear Value And Assert Framebuffer.

--- Source/Samples/GraphicsExplorer/GraphicsExplorer.cpp
+++ Source/Samples/GraphicsExplorer/GraphicsExplorer.cpp
@@ -167,7 +167,7 @@ public:
         clearValue2.m_ClearColor.r = tanf(fGlobalTime);
         clearValue2.m_ClearColor.g = sinf(fGlobalTime);
         clearValue2.m_ClearColor.b = cosf(fGlobalTime);
-        clearValue2.m_ClearColor.r = 1.0f;
+        clearValue2.m_ClearColor.a = 1.0f;
 
         pCommandList->BeginRenderPass(beginRenderPass);
         pCommandList->EndRenderPass();
@@ -564,6 +564,7 @@ public:
       framebufferDesc.m_Attachments.PushBack(hBackBufferView);
 
       m_hFrameBuffer = m_pDevice->CreateFramebuffer(framebufferDesc);
+      XII_ASSERT_DEV(!m_hFrameBuffer.IsInvalidated(), "Failed to create frame buffer.");
     }
   }
 
