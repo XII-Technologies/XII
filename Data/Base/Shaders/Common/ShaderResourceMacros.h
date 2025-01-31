5c7a3405ef0cc640bd3de0d65efdd5577aa3d1f9
Theophilus Eriata <theoeriata@gmail.com>
2025-01-30 23:46:08 -0700

    Shader: Refactor And Refine Shader Resource Macros.

--- Data/Base/Shaders/Common/ShaderResourceMacros.h
+++ Data/Base/Shaders/Common/ShaderResourceMacros.h
@@ -30,15 +30,16 @@ float3x3 TransformToRotation(Transform t)
 
 #  define XII_SHADER_STRUCT
 
-#  define DECLARE_CONSTANT_BUFFER(Name, Slot, Set)         cbuffer Name : register(b##Slot, space##Set)
-#  define DECLARE_TEXTURE(Name, Slot, Set)                 Texture2D Name : register(t##Slot, space##Set)
-#  define DECLARE_SAMPLER(Name, Slot, Set)                 SamplerState Name : register(s##Slot, space##Set)
-#  define DECLARE_STRUCTURED_BUFFER(Name, Type, Slot, Set) StructuredBuffer<Type> Name : register(u##Slot, space##Set)
-#  define DECLARE_COMBINED_IMAGE_SAMPLER(Name, Slot, Set) \
-    Texture2D    Name : register(t##Slot, space##Set);    \
-    SamplerState Name##Sampler : register(s##Slot, space##Set)
-
 #  if XII_ENABLED(PLATFORM_VULKAN)
+#    define DECLARE_CONSTANT_BUFFER(Name, Slot, Set)         cbuffer Name : register(b##Slot, space##Set)
+#    define DECLARE_TEXTURE(Name, Slot, Set)                 Texture2D Name : register(t##Slot, space##Set)
+#    define DECLARE_SAMPLER(Name, Slot, Set)                 SamplerState Name : register(s##Slot, space##Set)
+#    define DECLARE_STRUCTURED_BUFFER(Name, Type, Slot, Set) StructuredBuffer<Type> Name : register(u##Slot, space##Set)
+
+#    define DECLARE_COMBINED_IMAGE_SAMPLER(Name, Slot, Set)                           \
+      [[vk::combinedImageSampler]] Texture2D    Name : register(t##Slot, space##Set); \
+      [[vk::combinedImageSampler]] SamplerState Name##_AutoSampler : register(s##Slot, space##Set)
+
 #    define BEGIN_PUSH_CONSTANTS(Name) struct XII_SHADER_STRUCT XII_PP_CONCAT(Name, _PushConstants)
 #    define END_PUSH_CONSTANTS(Name) \
       ;                              \
@@ -48,9 +49,17 @@ float3x3 TransformToRotation(Transform t)
 #  elif XII_ENABLED(PLATFORM_D3D11)
 // D3D11 does not support push constants, so we just emulate them via a normal constant buffer.
 
-#    define BEGIN_PUSH_CONSTANTS(Name)        cbuffer Name
-#    define END_PUSH_CONSTANTS(Name)          ;
-#    define GET_PUSH_CONSTANT(Name, Constant) Constant
+#    define DECLARE_CONSTANT_BUFFER(Name, Slot, Set)         cbuffer Name : register(b##Slot)
+#    define DECLARE_TEXTURE(Name, Slot, Set)                 Texture2D Name : register(t##Slot)
+#    define DECLARE_SAMPLER(Name, Slot, Set)                 SamplerState Name : register(s##Slot)
+#    define DECLARE_STRUCTURED_BUFFER(Name, Type, Slot, Set) StructuredBuffer<Type> Name : register(u##Slot)
+#    define BEGIN_PUSH_CONSTANTS(Name)                       cbuffer Name
+#    define END_PUSH_CONSTANTS(Name)                         ;
+#    define GET_PUSH_CONSTANT(Name, Constant)                Constant
+
+#    define DECLARE_COMBINED_IMAGE_SAMPLER(Name, Slot, Set) \
+      Texture2D    Name : register(t##Slot);                \
+      SamplerState Name##_AutoSampler : register(s##Slot)
 #  endif
 
 #  define FLOAT1(Name)                            float Name
