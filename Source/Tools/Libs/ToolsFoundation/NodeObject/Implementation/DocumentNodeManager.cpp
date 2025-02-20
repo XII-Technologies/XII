bf391a3b9377e4904b3de27882432f6b80473249
Theophilus Eriata <theoeriata@gmail.com>
2025-02-20 16:17:55 -0700

    Apply Code Format Rules.

--- Source/Tools/Libs/ToolsFoundation/NodeObject/Implementation/DocumentNodeManager.cpp
+++ Source/Tools/Libs/ToolsFoundation/NodeObject/Implementation/DocumentNodeManager.cpp
@@ -903,7 +903,7 @@ void xiiDocumentNodeManager::ObjectHandler(const xiiDocumentObjectEvent& e)
     }
     break;
 
-    XII_DEFAULT_CASE_NOT_IMPLEMENTED;
+      XII_DEFAULT_CASE_NOT_IMPLEMENTED;
   }
 }
 
