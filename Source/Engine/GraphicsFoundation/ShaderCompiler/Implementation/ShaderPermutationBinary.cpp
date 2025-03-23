092c2b2514a8951219eedabc3e8b436d3b750d84
Theophilus Eriata <theoeriata@gmail.com>
2025-03-23 14:49:18 -0600

    GraphicsFoundation: Add Permutation Binary And State Descriptor.

---
+++ Source/Engine/GraphicsFoundation/ShaderCompiler/Implementation/ShaderPermutationBinary.cpp
@@ -0,0 +1,119 @@
+#include <GraphicsFoundation/GraphicsFoundationPCH.h>
+
+#include <GraphicsFoundation/ShaderCompiler/ShaderPermutationBinary.h>
+
+struct xiiGALShaderPermutationBinaryVersion
+{
+  using StorageType = xiiUInt8;
+
+  enum Enum : StorageType
+  {
+    Version1 = 1,
+
+    // Increase this version number to trigger shader recompilation
+
+    ENUM_COUNT,
+    Current = ENUM_COUNT - 1
+  };
+};
+
+xiiGALShaderPermutationBinary::xiiGALShaderPermutationBinary() = default;
+
+xiiResult xiiGALShaderPermutationBinary::Write(xiiStreamWriter& inout_stream)
+{
+  // write this at the beginning so that the file can be read as a xiiDependencyFile.
+  m_DependencyFile.StoreCurrentTimeStamp();
+  XII_SUCCEED_OR_RETURN(m_DependencyFile.WriteDependencyFile(inout_stream));
+
+  const xiiUInt8 uiVersion = xiiGALShaderPermutationBinaryVersion::Current;
+  if (inout_stream.WriteBytes(&uiVersion, sizeof(xiiUInt8)).Failed())
+    return XII_FAILURE;
+
+  const xiiUInt32 uiShaderStageHashCount = m_ShaderStageHashes.GetCount();
+  if (inout_stream.WriteDWordValue(&uiShaderStageHashCount).Failed())
+    return XII_FAILURE;
+
+  for (auto it : m_ShaderStageHashes)
+  {
+    if (inout_stream.WriteDWordValue(&it.Key()).Failed())
+      return XII_FAILURE;
+
+    if (inout_stream.WriteDWordValue(&it.Value()).Failed())
+      return XII_FAILURE;
+  }
+
+  m_StateDescriptor.Save(inout_stream);
+
+  const xiiUInt32 uiPermutationVariableCount = m_ShaderStageHashes.GetCount();
+  if (inout_stream.WriteDWordValue(&uiPermutationVariableCount).Failed())
+    return XII_FAILURE;
+
+  for (const auto& var : m_PermutationVariables)
+  {
+    if (inout_stream.WriteString(var.m_sName).Failed())
+      return XII_FAILURE;
+
+    if (inout_stream.WriteString(var.m_sValue).Failed())
+      return XII_FAILURE;
+  }
+
+  return XII_SUCCESS;
+}
+
+xiiResult xiiGALShaderPermutationBinary::Read(xiiStreamReader& inout_stream, bool& out_bOldVersion)
+{
+  XII_SUCCEED_OR_RETURN(m_DependencyFile.ReadDependencyFile(inout_stream));
+
+  xiiUInt8 uiVersion = 0;
+  if (inout_stream.ReadBytes(&uiVersion, sizeof(xiiUInt8)) != sizeof(xiiUInt8))
+    return XII_FAILURE;
+
+  XII_ASSERT_DEV(uiVersion <= xiiGALShaderPermutationBinaryVersion::Current, "Invalid Version {0}.", uiVersion);
+
+  out_bOldVersion = uiVersion != xiiGALShaderPermutationBinaryVersion::Current;
+
+  const xiiUInt32 uiShaderStageHashCount = 0;
+  if (inout_stream.ReadDWordValue(&uiShaderStageHashCount).Failed())
+    return XII_FAILURE;
+
+  for (xiiUInt32 i = 0; i < uiShaderStageHashCount; ++i)
+  {
+    xiiUInt32 uiStage = 0;
+    if (inout_stream.ReadDWordValue(&uiStage).Failed())
+      return XII_FAILURE;
+
+    xiiUInt32 uiHash = 0;
+    if (inout_stream.ReadDWordValue(&uiHash).Failed())
+      return XII_FAILURE;
+
+    m_ShaderStageHashes.Insert((xiiGALShaderType::Enum)uiStage, uiHash);
+  }
+
+  m_StateDescriptor.Load(inout_stream);
+
+  {
+    xiiUInt32 uiPermutationCount = 0U;
+    if (inout_stream.ReadDWordValue(&uiPermutationCount).Failed())
+      return XII_FAILURE;
+
+    m_PermutationVariables.SetCount(uiPermutationCount);
+
+    xiiStringBuilder tmp;
+    for (xiiUInt32 i = 0; i < uiPermutationCount; ++i)
+    {
+      auto& var = m_PermutationVariables[i];
+
+      if (inout_stream.ReadString(tmp).Failed())
+        return XII_FAILURE;
+
+      var.m_sName.Assign(tmp.GetView());
+
+      if (inout_stream.ReadString(tmp).Failed())
+        return XII_FAILURE;
+
+      var.m_sValue.Assign(tmp.GetView());
+    }
+  }
+
+  return XII_SUCCESS;
+}
---
+++ Source/Engine/GraphicsFoundation/ShaderCompiler/ShaderPermutationBinary.h
@@ -0,0 +1,40 @@
+#include <GraphicsFoundation/GraphicsFoundationDLL.h>
+
+#include <Foundation/IO/DependencyFile.h>
+
+#include <GraphicsFoundation/ShaderCompiler/Descriptors.h>
+#include <GraphicsFoundation/States/BlendState.h>
+#include <GraphicsFoundation/States/DepthStencilState.h>
+#include <GraphicsFoundation/States/RasterizerState.h>
+
+struct XII_GRAPHICSFOUNDATION_DLL xiiGALShaderStateResourceDescriptor
+{
+  xiiGALBlendStateCreationDescription        m_BlendDescription;
+  xiiGALDepthStencilStateCreationDescription m_DepthStencilDescription;
+  xiiGALRasterizerStateCreationDescription   m_RasterizerDescription;
+
+  xiiResult Parse(xiiStringView sSource);
+  void      Load(xiiStreamReader& inout_stream);
+  void      Save(xiiStreamWriter& inout_stream) const;
+
+  xiiUInt32 CalculateHash() const;
+};
+
+/// \brief Serialized state of a shader permutation.
+class XII_GRAPHICSFOUNDATION_DLL xiiGALShaderPermutationBinary
+{
+public:
+  xiiGALShaderPermutationBinary();
+
+  xiiResult Write(xiiStreamWriter& inout_stream);
+  xiiResult Read(xiiStreamReader& inout_stream, bool& out_bOldVersion);
+
+  // Actual binary will be loaded from the hash.
+  xiiMap<xiiGALShaderType::Enum, xiiUInt32> m_ShaderStageHashes;
+
+  xiiDependencyFile m_DependencyFile;
+
+  xiiGALShaderStateResourceDescriptor m_StateDescriptor;
+
+  xiiHybridArray<xiiGALPermutationVariable, 16> m_PermutationVariables;
+};
