fe8f4683af7472c188c2510cf8b723619c0d97bd
Theophilus Eriata <theoeriata@gmail.com>
2025-03-22 23:10:54 -0600

    GraphicsFoundation: Add Shader Compiler Descriptors.

---
+++ Source/Engine/GraphicsFoundation/ShaderCompiler/Descriptors.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <GraphicsFoundation/GraphicsFoundationDLL.h>
+
+#include <Foundation/Types/SharedPtr.h>
+#include <GraphicsFoundation/Shader/ShaderByteCode.h>
+
+/// \brief A shader resource definition found inside the shader source code.
+struct XII_GRAPHICSFOUNDATION_DLL xiiGALShaderResourceDefinition
+{
+  /// \brief Just the declaration inside the shader source, e.g. "Texture1D Texture".
+  xiiStringView m_sDeclaration;
+
+  /// \brief The declaration with any optional register mappings, e.g. "Texture1D Texture : register(12t, space3)"
+  xiiStringView m_sDeclarationAndRegister;
+
+  /// \brief The extracted reflection of the resource containing type, binding index, sets, etc.
+  xiiGALShaderResourceDescription m_ResourceDescription;
+};
+
+/// \brief Flags that affect the compilation process of a shader.
+struct xiiGALShaderCompilerFlags
+{
+  using StorageType = xiiUInt8;
+
+  enum Enum : StorageType
+  {
+    Debug   = XII_BIT(0),
+    Default = 0U,
+  };
+
+  struct Bits
+  {
+    StorageType Debug : 1;
+  };
+};
+
+XII_DECLARE_FLAGS_OPERATORS(xiiGALShaderCompilerFlags);
+
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSFOUNDATION_DLL, xiiGALShaderCompilerFlags);
+
+/// \brief Storage used during the shader compilation process.
+struct XII_GRAPHICSFOUNDATION_DLL xiiGALShaderProgramData
+{
+  struct StageData
+  {
+    xiiString                                          m_sShaderSource;
+    xiiHybridArray<xiiGALShaderResourceDefinition, 4U> m_Resources;
+    xiiUInt32                                          m_uiSourceHash = 0U;
+    xiiSharedPtr<xiiGALShaderByteCode>                 m_pByteCode;
+    bool                                               m_bWriteToDisk = true;
+  };
+
+  xiiBitflags<xiiGALShaderCompilerFlags>    m_Flags;
+  xiiStringView                             m_sPlatform;
+  xiiStringView                             m_sSourceFile;
+  xiiMap<xiiGALShaderType::Enum, StageData> m_StageData;
+};
+
+struct XII_GRAPHICSFOUNDATION_DLL xiiGALPermutationVariable
+{
+  XII_DECLARE_MEM_RELOCATABLE_TYPE();
+
+  xiiHashedString m_sName;
+  xiiHashedString m_sValue;
+
+  XII_ALWAYS_INLINE bool operator==(const xiiGALPermutationVariable& other) const
+  {
+    return m_sName == other.m_sName && m_sValue == other.m_sValue;
+  }
+};
---
+++ Source/Engine/GraphicsFoundation/ShaderCompiler/Implementation/Descriptors.cpp
@@ -0,0 +1,11 @@
+#include <GraphicsFoundation/GraphicsFoundationPCH.h>
+
+#include <GraphicsFoundation/ShaderCompiler/Descriptors.h>
+
+// clang-format off
+
+XII_BEGIN_STATIC_REFLECTED_BITFLAGS(xiiGALShaderCompilerFlags, 1)
+  XII_BITFLAGS_CONSTANT(xiiGALShaderCompilerFlags::Debug),
+XII_END_STATIC_REFLECTED_BITFLAGS;
+
+// clang-format on
