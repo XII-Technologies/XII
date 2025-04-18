f1a1a0500f5174bb3911d411ef7693a195aa8ae8
XII Automation <automation@xiitechnologies.com>
2025-04-18 23:11:03 +0000

    Apply Code Format Rules.

--- Source/Engine/GraphicsCore/Meshes/Implementation/MeshBufferUtils.cpp
+++ Source/Engine/GraphicsCore/Meshes/Implementation/MeshBufferUtils.cpp
@@ -30,9 +30,9 @@ XII_BEGIN_STATIC_REFLECTED_ENUM(xiiMeshVertexColorConversion, 1)
   XII_ENUM_CONSTANT(xiiMeshVertexColorConversion::LinearToSrgb),
   XII_ENUM_CONSTANT(xiiMeshVertexColorConversion::SrgbToLinear),
 XII_END_STATIC_REFLECTED_ENUM;
-// clang-format on
+  // clang-format on
 
-// clang-format off
+  // clang-format off
 namespace
 {
   template <xiiUInt32 Bits>
@@ -61,24 +61,24 @@ namespace
     return (value & uiMaxValue) * (1.0f / fMaxValue);
   }
 } // namespace
-// clang-format on
+  // clang-format on
 
-// static
-xiiResult xiiMeshBufferUtils::EncodeFromFloat(const float fSource, xiiArrayPtr<xiiUInt8> dest, xiiEnum<xiiGALResourceFormat> destFormat)
-{
-  XII_ASSERT_DEBUG(dest.GetCount() >= xiiGALTextureUtilities::GetResourceFormatProperties(destFormat).GetElementSize(), "Destination buffer is too small");
-
-  switch (destFormat)
+  // static
+  xiiResult xiiMeshBufferUtils::EncodeFromFloat(const float fSource, xiiArrayPtr<xiiUInt8> dest, xiiEnum<xiiGALResourceFormat> destFormat)
   {
-    case xiiGALResourceFormat::R32Float:
-      *reinterpret_cast<float*>(dest.GetPtr()) = fSource;
-      return XII_SUCCESS;
-    case xiiGALResourceFormat::R16Float:
-      *reinterpret_cast<xiiFloat16*>(dest.GetPtr()) = fSource;
-      return XII_SUCCESS;
-    default:
-      return XII_FAILURE;
-  }
+    XII_ASSERT_DEBUG(dest.GetCount() >= xiiGALTextureUtilities::GetResourceFormatProperties(destFormat).GetElementSize(), "Destination buffer is too small");
+
+    switch (destFormat)
+    {
+      case xiiGALResourceFormat::R32Float:
+        *reinterpret_cast<float*>(dest.GetPtr()) = fSource;
+        return XII_SUCCESS;
+      case xiiGALResourceFormat::R16Float:
+        *reinterpret_cast<xiiFloat16*>(dest.GetPtr()) = fSource;
+        return XII_SUCCESS;
+      default:
+        return XII_FAILURE;
+    }
 }
 
 // static
