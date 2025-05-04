482525bfdadf69a9996de939a834691ae6962361
Theophilus Eriata <theoeriata@gmail.com>
2025-05-04 07:41:35 -0600

    GraphicsFoundation: Update Descriptor Hash For Pipeline Types.

--- Source/Engine/GraphicsFoundation/Utilities/DescriptorHash.h
+++ Source/Engine/GraphicsFoundation/Utilities/DescriptorHash.h
@@ -11,6 +11,18 @@ public:
   static xiiUInt32 Hash(const xiiGALPipelineStateCreationDescription& description);
   static bool      Equal(const xiiGALPipelineStateCreationDescription& a, const xiiGALPipelineStateCreationDescription& b);
 
+  static xiiUInt32 Hash(const xiiGALGraphicsPipelineStateCreationDescription& description);
+  static bool      Equal(const xiiGALGraphicsPipelineStateCreationDescription& a, const xiiGALGraphicsPipelineStateCreationDescription& b);
+
+  static xiiUInt32 Hash(const xiiGALComputePipelineStateCreationDescription& description);
+  static bool      Equal(const xiiGALComputePipelineStateCreationDescription& a, const xiiGALComputePipelineStateCreationDescription& b);
+
+  static xiiUInt32 Hash(const xiiGALRayTracingPipelineStateCreationDescription& description);
+  static bool      Equal(const xiiGALRayTracingPipelineStateCreationDescription& a, const xiiGALRayTracingPipelineStateCreationDescription& b);
+
+  static xiiUInt32 Hash(const xiiGALTilePipelineStateCreationDescription& description);
+  static bool      Equal(const xiiGALTilePipelineStateCreationDescription& a, const xiiGALTilePipelineStateCreationDescription& b);
+
   static xiiUInt32 Hash(const xiiGALPipelineResourceSignatureCreationDescription& description);
   static bool      Equal(const xiiGALPipelineResourceSignatureCreationDescription& a, const xiiGALPipelineResourceSignatureCreationDescription& b);
 };
--- Source/Engine/GraphicsFoundation/Utilities/Implementation/DescriptorHash.cpp
+++ Source/Engine/GraphicsFoundation/Utilities/Implementation/DescriptorHash.cpp
@@ -5,111 +5,183 @@
 #include <GraphicsFoundation/States/PipelineState.h>
 #include <GraphicsFoundation/Utilities/DescriptorHash.h>
 
+template <typename T>
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, xiiSharedPtr<T>& pPtr)
+{
+  ref_stream << reinterpret_cast<const xiiUInt64&>(pPtr.Borrow());
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALSampleDescription& value)
+{
+  ref_stream << value.m_uiCount;
+  ref_stream << value.m_uiQuality;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALGraphicsPipelineDescription& value)
+{
+  ref_stream << value.m_pBlendState;
+  ref_stream << value.m_pRasterizerState;
+  ref_stream << value.m_pDepthStencilState;
+  ref_stream << value.m_pInputLayout;
+  ref_stream << value.m_pRenderPass;
+  ref_stream << value.m_uiSampleMask;
+  ref_stream << value.m_PrimitiveTopology;
+  ref_stream << value.m_uiViewportCount;
+  ref_stream << value.m_uiSubpassIndex;
+  ref_stream << value.m_ShadingRateFlags;
+  ref_stream << value.m_SampleDescription;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALRayTracingPipelineDescription& value)
+{
+  ref_stream << value.m_uiShaderRecordSize;
+  ref_stream << value.m_uiMaxRecursionDepth;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALRayTracingGeneralShaderGroupDescription& value)
+{
+  ref_stream << value.m_sName;
+  ref_stream << value.m_pShader;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALRayTracingTriangleHitShaderGroupDescription& value)
+{
+  ref_stream << value.m_sName;
+  ref_stream << value.m_pClosestHitShader;
+  ref_stream << value.m_pAnyHitShader;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALRayTracingProceduralHitShaderGroupDescription& value)
+{
+  ref_stream << value.m_sName;
+  ref_stream << value.m_pIntersectionShader;
+  ref_stream << value.m_pClosestHitShader;
+  ref_stream << value.m_pAnyHitShader;
+  return ref_stream;
+}
+
+xiiStreamWriter& operator<<(xiiStreamWriter& ref_stream, const xiiGALTilePipelineDescription& value)
+{
+  ref_stream << value.m_SampleCount;
+  ref_stream << value.m_RenderTargetFormats.GetCount();
+
+  for (xiiUInt32 i = 0; i < value.m_RenderTargetFormats.GetCount(); ++i)
+  {
+    ref_stream << value.m_RenderTargetFormats[i];
+  }
+  return ref_stream;
+}
+
 xiiUInt32 xiiGALDescriptorHash::Hash(const xiiGALPipelineStateCreationDescription& description)
 {
   xiiHashStreamWriter32 writer;
 
   writer << description.m_PipelineType;
-  writer << description.m_uiNodeMask;
-
-  // Graphics pipeline state.
-  switch (description.m_PipelineType)
-  {
-    case xiiGALPipelineType::Graphics:
-    case xiiGALPipelineType::Mesh:
-    {
-      const auto& graphicsPipeline = description.m_GraphicsPipeline;
-
-      writer << graphicsPipeline.m_pVertexShader;
-      writer << graphicsPipeline.m_pPixelShader;
-      writer << graphicsPipeline.m_pDomainShader;
-      writer << graphicsPipeline.m_pHullShader;
-      writer << graphicsPipeline.m_pGeometryShader;
-      writer << graphicsPipeline.m_pAmplificationShader;
-      writer << graphicsPipeline.m_pMeshShader;
-      writer << graphicsPipeline.m_pBlendState;
-      writer << graphicsPipeline.m_pRasterizerState;
-      writer << graphicsPipeline.m_pDepthStencilState;
-      writer << graphicsPipeline.m_pInputLayout;
-      writer << graphicsPipeline.m_pRenderPass;
-      writer << graphicsPipeline.m_uiSampleMask;
-      writer << graphicsPipeline.m_PrimitiveTopology;
-      writer << graphicsPipeline.m_uiViewportCount;
-      writer << graphicsPipeline.m_uiSubpassIndex;
-      writer << graphicsPipeline.m_ShadingRateFlags;
-      writer << graphicsPipeline.m_SampleDescription.m_uiCount;
-      writer << graphicsPipeline.m_SampleDescription.m_uiQuality;
-    }
-    break;
-    case xiiGALPipelineType::Compute:
-    {
-      const auto& computePipeline = description.m_ComputePipeline;
-
-      writer << computePipeline.m_pComputeShader;
-    }
-    break;
-    case xiiGALPipelineType::RayTracing:
-    {
-      const auto& rayTracingPipeline = description.m_RayTracingPipeline;
-
-      writer << rayTracingPipeline.m_sShaderRecordName;
-      writer << rayTracingPipeline.m_uiMaximumAttributeSize;
-      writer << rayTracingPipeline.m_uiMaximumPayloadSize;
-      writer << rayTracingPipeline.m_uiShaderRecordSize;
-      writer << rayTracingPipeline.m_uiMaxRecursionDepth;
-
-      writer << rayTracingPipeline.m_GeneralShaders.GetCount();
-      for (xiiUInt32 i = 0; i < rayTracingPipeline.m_GeneralShaders.GetCount(); ++i)
-      {
-        const auto& generalShaderDescription = rayTracingPipeline.m_GeneralShaders[i];
-
-        writer << generalShaderDescription.m_sName;
-        writer << generalShaderDescription.m_pShader;
-      }
-
-      writer << rayTracingPipeline.m_TriangleHitShaders.GetCount();
-      for (xiiUInt32 i = 0; i < rayTracingPipeline.m_TriangleHitShaders.GetCount(); ++i)
-      {
-        const auto& triangleHitShaderDescription = rayTracingPipeline.m_TriangleHitShaders[i];
-
-        writer << triangleHitShaderDescription.m_sName;
-        writer << triangleHitShaderDescription.m_pClosestHitShader;
-        writer << triangleHitShaderDescription.m_pAnyHitShader;
-      }
-
-      writer << rayTracingPipeline.m_ProceduralHitShaders.GetCount();
-      for (xiiUInt32 i = 0; i < rayTracingPipeline.m_ProceduralHitShaders.GetCount(); ++i)
-      {
-        const auto& proceduralHitShaderDescription = rayTracingPipeline.m_ProceduralHitShaders[i];
-
-        writer << proceduralHitShaderDescription.m_sName;
-        writer << proceduralHitShaderDescription.m_pIntersectionShader;
-        writer << proceduralHitShaderDescription.m_pClosestHitShader;
-        writer << proceduralHitShaderDescription.m_pAnyHitShader;
-      }
-    }
-    break;
-    case xiiGALPipelineType::Tile:
-    {
-      const auto& tilePipeline = description.m_TilePipeline;
-
-      writer << tilePipeline.m_SampleCount;
-      writer << tilePipeline.m_RenderTargetFormats.GetCount();
-
-      for (xiiUInt32 i = 0; i < tilePipeline.m_RenderTargetFormats.GetCount(); ++i)
-      {
-        writer << tilePipeline.m_RenderTargetFormats[i];
-      }
-    }
-    break;
-
-      XII_DEFAULT_CASE_NOT_IMPLEMENTED;
-  }
-
+  writer << description.m_pPipelineResourceSignature;
   return writer.GetHashValue();
 }
 
 bool xiiGALDescriptorHash::Equal(const xiiGALPipelineStateCreationDescription& a, const xiiGALPipelineStateCreationDescription& b)
+{
+  return a == b;
+}
+
+xiiUInt32 xiiGALDescriptorHash::Hash(const xiiGALGraphicsPipelineStateCreationDescription& description)
+{
+  xiiHashStreamWriter32 writer;
+
+  writer << description.m_PipelineType;
+  writer << description.m_pPipelineResourceSignature;
+  writer << description.m_GraphicsPipeline;
+  writer << description.m_pVertexShader;
+  writer << description.m_pPixelShader;
+  writer << description.m_pDomainShader;
+  writer << description.m_pHullShader;
+  writer << description.m_pGeometryShader;
+  writer << description.m_pAmplificationShader;
+  writer << description.m_pMeshShader;
+
+  return writer.GetHashValue();
+}
+
+bool xiiGALDescriptorHash::Equal(const xiiGALGraphicsPipelineStateCreationDescription& a, const xiiGALGraphicsPipelineStateCreationDescription& b)
+{
+  return a == b;
+}
+
+xiiUInt32 xiiGALDescriptorHash::Hash(const xiiGALComputePipelineStateCreationDescription& description)
+{
+  xiiHashStreamWriter32 writer;
+
+  writer << description.m_PipelineType;
+  writer << description.m_pPipelineResourceSignature;
+  writer << description.m_pComputeShader;
+
+  return writer.GetHashValue();
+}
+
+bool xiiGALDescriptorHash::Equal(const xiiGALComputePipelineStateCreationDescription& a, const xiiGALComputePipelineStateCreationDescription& b)
+{
+  return a == b;
+}
+
+xiiUInt32 xiiGALDescriptorHash::Hash(const xiiGALRayTracingPipelineStateCreationDescription& description)
+{
+  xiiHashStreamWriter32 writer;
+
+  writer << description.m_PipelineType;
+  writer << description.m_pPipelineResourceSignature;
+  writer << description.m_RayTracingPipeline;
+  writer << description.m_sShaderRecordName;
+
+  writer << description.m_GeneralShaders.GetCount();
+  for (xiiUInt32 i = 0; i < description.m_GeneralShaders.GetCount(); ++i)
+  {
+    writer << description.m_GeneralShaders[i];
+  }
+
+  writer << description.m_TriangleHitShaders.GetCount();
+  for (xiiUInt32 i = 0; i < description.m_TriangleHitShaders.GetCount(); ++i)
+  {
+    writer << description.m_TriangleHitShaders[i];
+  }
+
+  writer << description.m_ProceduralHitShaders.GetCount();
+  for (xiiUInt32 i = 0; i < description.m_ProceduralHitShaders.GetCount(); ++i)
+  {
+    writer << description.m_ProceduralHitShaders[i];
+  }
+
+  writer << description.m_uiMaximumAttributeSize;
+  writer << description.m_uiMaximumPayloadSize;
+
+  return writer.GetHashValue();
+}
+
+bool xiiGALDescriptorHash::Equal(const xiiGALRayTracingPipelineStateCreationDescription& a, const xiiGALRayTracingPipelineStateCreationDescription& b)
+{
+  return a == b;
+}
+
+xiiUInt32 xiiGALDescriptorHash::Hash(const xiiGALTilePipelineStateCreationDescription& description)
+{
+  xiiHashStreamWriter32 writer;
+
+  writer << description.m_PipelineType;
+  writer << description.m_pPipelineResourceSignature;
+  writer << description.m_TilePipeline;
+  writer << description.m_pTileShader;
+
+  return writer.GetHashValue();
+}
+
+bool xiiGALDescriptorHash::Equal(const xiiGALTilePipelineStateCreationDescription& a, const xiiGALTilePipelineStateCreationDescription& b)
 {
   return a == b;
 }
