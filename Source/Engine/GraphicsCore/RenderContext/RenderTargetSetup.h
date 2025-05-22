1c38adc0ba874faa865830d3361392d28481562b
Theophilus Eriata <theoeriata@gmail.com>
2025-05-22 03:01:42 -0600

    GraphicsCore: Deprecate Render Target And Rendering Setup.

--- Source/Engine/GraphicsCore/RenderContext/RenderTargetSetup.h
+++ Source/Engine/GraphicsCore/RenderContext/RenderTargetSetup.h
@@ -4,51 +4,13 @@
 
 #include <GraphicsFoundation/Resources/Texture.h>
 
-// \brief This class can be used to define the render targets to be used by a xiiView.
+/// \brief This class can be used to define the render targets to be used by a xiiView.
 struct XII_GRAPHICSCORE_DLL xiiGALRenderTargets
 {
   bool operator==(const xiiGALRenderTargets& other) const;
 
   xiiSharedPtr<xiiGALTextureView> m_pRTs[XII_GAL_MAX_RENDERTARGET_COUNT];
   xiiSharedPtr<xiiGALTextureView> m_pDSTarget;
-};
-
-// \brief This class can be used to construct render target setups on the stack.
-class XII_GRAPHICSCORE_DLL xiiGALRenderTargetSetup
-{
-public:
-  xiiGALRenderTargetSetup();
-
-  xiiGALRenderTargetSetup& SetRenderTarget(xiiUInt8 uiIndex, xiiSharedPtr<xiiGALTextureView> pRenderTarget);
-  xiiGALRenderTargetSetup& SetDepthStencilTarget(xiiSharedPtr<xiiGALTextureView> pDSTarget);
-
-  bool operator==(const xiiGALRenderTargetSetup& other) const;
-
-  inline xiiUInt8 GetRenderTargetCount() const;
-
-  inline xiiSharedPtr<xiiGALTextureView> GetRenderTarget(xiiUInt8 uiIndex) const;
-  inline xiiSharedPtr<xiiGALTextureView> GetDepthStencilTarget() const;
-
-  void DestroyAllAttachedViews();
-
-protected:
-  xiiSharedPtr<xiiGALTextureView> m_pRTs[XII_GAL_MAX_RENDERTARGET_COUNT];
-  xiiSharedPtr<xiiGALTextureView> m_pDSTarget;
-
-  xiiUInt8 m_uiRTCount = 0;
-};
-
-struct XII_GRAPHICSCORE_DLL xiiGALRenderingSetup
-{
-  bool operator==(const xiiGALRenderingSetup& other) const;
-
-  xiiGALRenderTargetSetup m_RenderTargetSetup;
-  xiiUInt32               m_uiRenderTargetClearMask = 0x0U;
-  bool                    m_bClearDepth             = false;
-  bool                    m_bClearStencil           = false;
-  bool                    m_bDiscardColor           = false;
-  bool                    m_bDiscardDepth           = false;
-  xiiColor                m_ClearColor              = xiiColor::Black;
 };
 
 #include <GraphicsCore/RenderContext/Implementation/RenderTargetSetup_inl.h>
