8483d70a1269854c359dda5e52f2314f0d775c20
Theophilus Eriata <theoeriata@gmail.com>
2025-06-02 10:50:43 -0600

    GraphicsFoundation: Fix Command List Submission Returning UINT64_MAX.

--- Source/Engine/GraphicsFoundation/Device/SwapChain.h
+++ Source/Engine/GraphicsFoundation/Device/SwapChain.h
@@ -44,7 +44,7 @@ public:
 
   /// \brief This sets the maximum number of frames that the swap chain is allowed to queue for rendering.
   ///
-  /// This value is only relevant for DirectX11 and DirectX12 backends and ignored for others. By default it matches the number of buffers in the swap chain. For example, for a 2-buffer
+  /// This value is only relevant for Direct3D11 and Direct3D12 backends and ignored for others. By default it matches the number of buffers in the swap chain. For example, for a 2-buffer
   /// swap chain, the CPU can enqueue frames 0 and 1, but Present command of frame 2 will block until frame 0 is presented. If in the example above the maximum frame latency is set
   /// to 1, then Present command of frame 1 will block until Present of frame 0 is complete.
   virtual void SetMaximumFrameLatency(xiiUInt32 uiMaxLatency) { XII_IGNORE_UNUSED(uiMaxLatency); };
