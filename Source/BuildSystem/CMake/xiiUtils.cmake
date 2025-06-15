4fb9c2561d0659865ac01f2da7f8a969c50f5738
Theophilus Eriata <theoeriata@gmail.com>
2025-06-14 22:08:46 -0600

    GraphicsCore: Update Git Attributes.

--- Source/BuildSystem/CMake/xiiUtils.cmake
+++ Source/BuildSystem/CMake/xiiUtils.cmake
@@ -323,6 +323,8 @@ function(xii_glob_source_files ROOT_DIR RESULT_ALL_SOURCES)
     "${ROOT_DIR}/*.xiiShaderTemplate"
     "${ROOT_DIR}/*.rml"
     "${ROOT_DIR}/*.rcss"
+    "${ROOT_DIR}/*.json"
+    "${ROOT_DIR}/*.yml"
   )
 
   set(${RESULT_ALL_SOURCES} ${RELEVANT_FILES} PARENT_SCOPE)
