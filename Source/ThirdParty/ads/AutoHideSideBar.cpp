7bb48a1891bc2dd3ee0581d342eedf9226c08978
Theophilus Eriata <theoeriata@gmail.com>
2025-07-03 09:13:16 -0600

    ThirdParty: Update QT Advanced Docking System.

--- Source/ThirdParty/ads/AutoHideSideBar.cpp
+++ Source/ThirdParty/ads/AutoHideSideBar.cpp
@@ -52,10 +52,10 @@ class CTabsWidget;
  */
 struct AutoHideSideBarPrivate
 {
-  /**
-   * Private data constructor
-   */
-  AutoHideSideBarPrivate(CAutoHideSideBar* _public);
+	/**
+	 * Private data constructor
+	 */
+	AutoHideSideBarPrivate(CAutoHideSideBar* _public);
 
     CAutoHideSideBar* _this;
     CDockContainerWidget* ContainerWidget;
@@ -69,7 +69,7 @@ struct AutoHideSideBarPrivate
      */
     bool isHorizontal() const
     {
-      return Qt::Horizontal == Orientation;
+    	return Qt::Horizontal == Orientation;
     }
 
     /**
@@ -85,26 +85,26 @@ struct AutoHideSideBarPrivate
 class CTabsWidget : public QWidget
 {
 public:
-  using QWidget::QWidget;
-  using Super = QWidget;
-  AutoHideSideBarPrivate* EventHandler;
+	using QWidget::QWidget;
+	using Super = QWidget;
+	AutoHideSideBarPrivate* EventHandler;
 
-  /**
-   * Returns the size hint as minimum size hint
-   */
-  virtual QSize minimumSizeHint() const override
-  {
-    return Super::sizeHint();
-  }
+	/**
+	 * Returns the size hint as minimum size hint
+	 */
+	virtual QSize minimumSizeHint() const override
+	{
+		return Super::sizeHint();
+	}
 
-  /**
-   * Forward event handling to EventHandler
-   */
-  virtual bool event(QEvent* e) override
-  {
-    EventHandler->handleViewportEvent(e);
-    return Super::event(e);
-  }
+	/**
+	 * Forward event handling to EventHandler
+	 */
+	virtual bool event(QEvent* e) override
+	{
+		EventHandler->handleViewportEvent(e);
+		return Super::event(e);
+	}
 };
 
 
@@ -118,18 +118,18 @@ AutoHideSideBarPrivate::AutoHideSideBarPrivate(CAutoHideSideBar* _public) :
 //============================================================================
 void AutoHideSideBarPrivate::handleViewportEvent(QEvent* e)
 {
-  switch (e->type())
-  {
-  case QEvent::ChildRemoved:
-    if (TabsLayout->isEmpty())
-    {
-      _this->hide();
-    }
-    break;
+	switch (e->type())
+	{
+	case QEvent::ChildRemoved:
+		if (TabsLayout->isEmpty())
+		{
+			_this->hide();
+		}
+		break;
 
-  default:
-    break;
-  }
+	default:
+		break;
+	}
 }
 
 
@@ -138,21 +138,21 @@ CAutoHideSideBar::CAutoHideSideBar(CDockContainerWidget* parent, SideBarLocation
     Super(parent),
     d(new AutoHideSideBarPrivate(this))
 {
-  d->SideTabArea = area;
+	d->SideTabArea = area;
     d->ContainerWidget = parent;
     d->Orientation = (area == SideBarLocation::SideBarBottom || area == SideBarLocation::SideBarTop)
-      ? Qt::Horizontal : Qt::Vertical;
+    	? Qt::Horizontal : Qt::Vertical;
 
-  setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-  setFrameStyle(QFrame::NoFrame);
-  setWidgetResizable(true);
-  setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+	setFrameStyle(QFrame::NoFrame);
+	setWidgetResizable(true);
+	setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-  d->TabsContainerWidget = new CTabsWidget();
-  d->TabsContainerWidget->EventHandler = d;
-  d->TabsContainerWidget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-  d->TabsContainerWidget->setObjectName("sideTabsContainerWidget");
+	d->TabsContainerWidget = new CTabsWidget();
+	d->TabsContainerWidget->EventHandler = d;
+	d->TabsContainerWidget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+	d->TabsContainerWidget->setObjectName("sideTabsContainerWidget");
 
 
     d->TabsLayout = new QBoxLayout(d->Orientation == Qt::Vertical ? QBoxLayout::TopToBottom : QBoxLayout::LeftToRight);
@@ -160,34 +160,34 @@ CAutoHideSideBar::CAutoHideSideBar(CDockContainerWidget* parent, SideBarLocation
     d->TabsLayout->setSpacing(12);
     d->TabsLayout->addStretch(1);
     d->TabsContainerWidget->setLayout(d->TabsLayout);
-  setWidget(d->TabsContainerWidget);
+	setWidget(d->TabsContainerWidget);
 
     setFocusPolicy(Qt::NoFocus);
-  if (d->isHorizontal())
-  {
-    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  }
-  else
-  {
-    setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
-  }
+	if (d->isHorizontal())
+	{
+		setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+	}
+	else
+	{
+		setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+	}
 
-  hide();
+	hide();
 }
 
 
 //============================================================================
-CAutoHideSideBar::~CAutoHideSideBar()
+CAutoHideSideBar::~CAutoHideSideBar() 
 {
-  ADS_PRINT("~CSideTabBar()");
-  // The SideTabeBar is not the owner of the tabs and to prevent deletion
-  // we set the parent here to nullptr to remove it from the children
-  auto Tabs = findChildren<CAutoHideTab*>(QString(), Qt::FindDirectChildrenOnly);
-  for (auto Tab : Tabs)
-  {
-    Tab->setParent(nullptr);
-  }
-  delete d;
+	ADS_PRINT("~CSideTabBar()");
+	// The SideTabeBar is not the owner of the tabs and to prevent deletion
+	// we set the parent here to nullptr to remove it from the children
+	auto Tabs = findChildren<CAutoHideTab*>(QString(), Qt::FindDirectChildrenOnly);
+	for (auto Tab : Tabs)
+	{
+		Tab->setParent(nullptr);
+	}
+	delete d;
 }
 
 
@@ -195,15 +195,15 @@ CAutoHideSideBar::~CAutoHideSideBar()
 void CAutoHideSideBar::insertTab(int Index, CAutoHideTab* SideTab)
 {
     SideTab->setSideBar(this);
-  SideTab->installEventFilter(this);
-  // Default insertion is append
+	SideTab->installEventFilter(this);
+	// Default insertion is append
     if (Index < 0)
     {
-      d->TabsLayout->insertWidget(d->TabsLayout->count() - 1, SideTab);
+    	d->TabsLayout->insertWidget(d->TabsLayout->count() - 1, SideTab);
     }
     else
     {
-      d->TabsLayout->insertWidget(Index, SideTab);
+    	d->TabsLayout->insertWidget(Index, SideTab);
     }
     show();
 }
@@ -212,68 +212,68 @@ void CAutoHideSideBar::insertTab(int Index, CAutoHideTab* SideTab)
 //============================================================================
 CAutoHideDockContainer* CAutoHideSideBar::insertDockWidget(int Index, CDockWidget* DockWidget)
 {
-  auto AutoHideContainer = new CAutoHideDockContainer(DockWidget, d->SideTabArea, d->ContainerWidget);
-  DockWidget->dockManager()->dockFocusController()->clearDockWidgetFocus(DockWidget);
-  auto Tab = AutoHideContainer->autoHideTab();
-  DockWidget->setSideTabWidget(Tab);
-  insertTab(Index, Tab);
-  return AutoHideContainer;
+	auto AutoHideContainer = new CAutoHideDockContainer(DockWidget, d->SideTabArea, d->ContainerWidget);
+	DockWidget->dockManager()->dockFocusController()->clearDockWidgetFocus(DockWidget);
+	auto Tab = AutoHideContainer->autoHideTab();
+	DockWidget->setSideTabWidget(Tab);
+	insertTab(Index, Tab);
+	return AutoHideContainer;
 }
 
 
 //============================================================================
 void CAutoHideSideBar::removeAutoHideWidget(CAutoHideDockContainer* AutoHideWidget)
 {
-  AutoHideWidget->autoHideTab()->removeFromSideBar();
-  auto DockContainer = AutoHideWidget->dockContainer();
-  if (DockContainer)
-  {
-    DockContainer->removeAutoHideWidget(AutoHideWidget);
-  }
-  AutoHideWidget->setParent(nullptr);
+	AutoHideWidget->autoHideTab()->removeFromSideBar();
+	auto DockContainer = AutoHideWidget->dockContainer();
+	if (DockContainer)
+	{
+		DockContainer->removeAutoHideWidget(AutoHideWidget);
+	}
+	AutoHideWidget->setParent(nullptr);
 }
 
 //============================================================================
 void CAutoHideSideBar::addAutoHideWidget(CAutoHideDockContainer* AutoHideWidget,
-  int TabIndex)
+	int TabIndex)
 {
-  auto SideBar = AutoHideWidget->autoHideTab()->sideBar();
-  if (SideBar == this)
-  {
-    // If we move to the same tab index or if we insert before the next
-    // tab index, then we will end at the same tab position and can leave
-    if (AutoHideWidget->tabIndex() == TabIndex || (AutoHideWidget->tabIndex() + 1) == TabIndex)
-    {
-      return;
-    }
+	auto SideBar = AutoHideWidget->autoHideTab()->sideBar();
+	if (SideBar == this)
+	{
+		// If we move to the same tab index or if we insert before the next
+		// tab index, then we will end at the same tab position and can leave
+		if (AutoHideWidget->tabIndex() == TabIndex || (AutoHideWidget->tabIndex() + 1) == TabIndex)
+		{
+			return;
+		}
 
-    // We remove this auto hide widget from the sidebar in the code below
-    // and therefore need to correct the TabIndex here
-    if (AutoHideWidget->tabIndex() < TabIndex)
-    {
-      --TabIndex;
-    }
-  }
+		// We remove this auto hide widget from the sidebar in the code below
+		// and therefore need to correct the TabIndex here
+		if (AutoHideWidget->tabIndex() < TabIndex)
+		{
+			--TabIndex;
+		}
+	}
 
-  if (SideBar)
-  {
-    SideBar->removeAutoHideWidget(AutoHideWidget);
-  }
-  AutoHideWidget->setParent(d->ContainerWidget);
-  AutoHideWidget->setSideBarLocation(d->SideTabArea);
-  d->ContainerWidget->registerAutoHideWidget(AutoHideWidget);
-  insertTab(TabIndex, AutoHideWidget->autoHideTab());
+	if (SideBar)
+	{
+		SideBar->removeAutoHideWidget(AutoHideWidget);
+	}
+	AutoHideWidget->setParent(d->ContainerWidget);
+	AutoHideWidget->setSideBarLocation(d->SideTabArea);
+	d->ContainerWidget->registerAutoHideWidget(AutoHideWidget);
+	insertTab(TabIndex, AutoHideWidget->autoHideTab());
 }
 
 
 //============================================================================
 void CAutoHideSideBar::removeTab(CAutoHideTab* SideTab)
 {
-  SideTab->removeEventFilter(this);
+	SideTab->removeEventFilter(this);
     d->TabsLayout->removeWidget(SideTab);
     if (d->TabsLayout->isEmpty())
     {
-      hide();
+    	hide();
     }
 }
 
@@ -281,30 +281,30 @@ void CAutoHideSideBar::removeTab(CAutoHideTab* SideTab)
 //============================================================================
 bool CAutoHideSideBar::eventFilter(QObject *watched, QEvent *event)
 {
-  auto Tab = qobject_cast<CAutoHideTab*>(watched);
-  if (!Tab)
-  {
-    return false;
-  }
+	auto Tab = qobject_cast<CAutoHideTab*>(watched);
+	if (!Tab)
+	{
+		return false;
+	}
 
-  switch (event->type())
-  {
-  case QEvent::ShowToParent:
-     show();
-       break;
+	switch (event->type())
+	{
+	case QEvent::ShowToParent:
+		 show();
+	     break;
 
-  case QEvent::HideToParent:
-     if (!hasVisibleTabs())
-     {
-       hide();
-     }
-     break;
+	case QEvent::HideToParent:
+		 if (!hasVisibleTabs())
+		 {
+			 hide();
+		 }
+		 break;
 
-  default:
-    break;
-  }
+	default:
+		break;
+	}
 
-  return false;
+	return false;
 }
 
 
@@ -332,176 +332,176 @@ int CAutoHideSideBar::count() const
 //============================================================================
 int CAutoHideSideBar::visibleTabCount() const
 {
-  int VisibleTabCount = 0;
-  auto ParentWidget = parentWidget();
-  for (auto i = 0; i < count(); i++)
-  {
-    if (tab(i)->isVisibleTo(ParentWidget))
-    {
-      VisibleTabCount++;
-    }
-  }
+	int VisibleTabCount = 0;
+	auto ParentWidget = parentWidget();
+	for (auto i = 0; i < count(); i++)
+	{
+		if (tab(i)->isVisibleTo(ParentWidget))
+		{
+			VisibleTabCount++;
+		}
+	}
 
-  return VisibleTabCount;
+	return VisibleTabCount;
 }
 
 
 //============================================================================
 bool CAutoHideSideBar::hasVisibleTabs() const
 {
-  auto ParentWidget = parentWidget();
-  for (auto i = 0; i < count(); i++)
-  {
-    if (tab(i)->isVisibleTo(ParentWidget))
-    {
-      return true;
-    }
-  }
+	auto ParentWidget = parentWidget();
+	for (auto i = 0; i < count(); i++)
+	{
+		if (tab(i)->isVisibleTo(ParentWidget))
+		{
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 
 //============================================================================
 int CAutoHideSideBar::indexOfTab(const CAutoHideTab& Tab) const
 {
-  for (auto i = 0; i < count(); i++)
-  {
-    if (tab(i) == &Tab)
-    {
-      return i;
-    }
-  }
+	for (auto i = 0; i < count(); i++)
+	{
+		if (tab(i) == &Tab)
+		{
+			return i;
+		}
+	}
 
-  return -1;
+	return -1;
 }
 
 
 //============================================================================
 SideBarLocation CAutoHideSideBar::sideBarLocation() const
 {
-  return d->SideTabArea;
+	return d->SideTabArea;
 }
 
 
 //============================================================================
 void CAutoHideSideBar::saveState(QXmlStreamWriter& s) const
 {
-  if (!count())
-  {
-    return;
-  }
+	if (!count())
+	{
+		return;
+	}
 
-  s.writeStartElement("SideBar");
-  s.writeAttribute("Area", QString::number(sideBarLocation()));
-  s.writeAttribute("Tabs", QString::number(count()));
+	s.writeStartElement("SideBar");
+	s.writeAttribute("Area", QString::number(sideBarLocation()));
+	s.writeAttribute("Tabs", QString::number(count()));
 
-  for (auto i = 0; i < count(); ++i)
-  {
-    auto Tab = tab(i);
-    if (!Tab)
-    {
-      continue;
-    }
+	for (auto i = 0; i < count(); ++i)
+	{
+		auto Tab = tab(i);
+		if (!Tab)
+		{
+			continue;
+		}
 
-    Tab->dockWidget()->autoHideDockContainer()->saveState(s);
-  }
+		Tab->dockWidget()->autoHideDockContainer()->saveState(s);
+	}
 
-  s.writeEndElement();
+	s.writeEndElement();
 }
 
 //===========================================================================
 QSize CAutoHideSideBar::minimumSizeHint() const
 {
-  QSize Size = sizeHint();
-  if (d->isHorizontal())
-  {
-    Size.setWidth(0);
-  }
-  else
-  {
-    Size.setHeight(0);
-  }
-  return Size;
+	QSize Size = sizeHint();
+	if (d->isHorizontal())
+	{
+		Size.setWidth(0);
+	}
+	else
+	{
+		Size.setHeight(0);
+	}
+	return Size;
 }
 
 
 //===========================================================================
 QSize CAutoHideSideBar::sizeHint() const
 {
-  return d->TabsContainerWidget->sizeHint();
+	return d->TabsContainerWidget->sizeHint();
 }
 
 
 //===========================================================================
 int CAutoHideSideBar::spacing() const
 {
-  return d->TabsLayout->spacing();
+	return d->TabsLayout->spacing();
 }
 
 
 //===========================================================================
 void CAutoHideSideBar::setSpacing(int Spacing)
 {
-  d->TabsLayout->setSpacing(Spacing);
+	d->TabsLayout->setSpacing(Spacing);
 }
 
 
 //===========================================================================
 CDockContainerWidget* CAutoHideSideBar::dockContainer() const
 {
-  return d->ContainerWidget;
+	return d->ContainerWidget;
 }
 
 
 //===========================================================================
 int CAutoHideSideBar::tabAt(const QPoint& Pos) const
 {
-  if (!isVisible())
-  {
-    return TabInvalidIndex;
-  }
+	if (!isVisible())
+	{
+		return TabInvalidIndex;
+	}
 
-  if (orientation() == Qt::Horizontal)
-  {
-    if (Pos.x() < tab(0)->geometry().x())
-    {
-      return -1;
-    }
-  }
-  else
-  {
-    if (Pos.y() < tab(0)->geometry().y())
-    {
-      return -1;
-    }
-  }
+	if (orientation() == Qt::Horizontal)
+	{
+		if (Pos.x() < tab(0)->geometry().x())
+		{
+			return -1;
+		}
+	}
+	else
+	{
+		if (Pos.y() < tab(0)->geometry().y())
+		{
+			return -1;
+		}
+	}
 
 
-  for (int i = 0; i < count(); ++i)
-  {
-    if (tab(i)->geometry().contains(Pos))
-    {
-      return i;
-    }
-  }
+	for (int i = 0; i < count(); ++i)
+	{
+		if (tab(i)->geometry().contains(Pos))
+		{
+			return i;
+		}
+	}
 
-  return count();
+	return count();
 }
 
 
 //===========================================================================
 int CAutoHideSideBar::tabInsertIndexAt(const QPoint& Pos) const
 {
-  int Index = tabAt(Pos);
-  if (Index == TabInvalidIndex)
-  {
-    return TabDefaultInsertIndex;
-  }
-  else
-  {
-    return (Index < 0) ? 0 : Index;
-  }
+	int Index = tabAt(Pos);
+	if (Index == TabInvalidIndex)
+	{
+		return TabDefaultInsertIndex;
+	}
+	else
+	{
+		return (Index < 0) ? 0 : Index;
+	}
 }
 
 } // namespace ads
--- Source/ThirdParty/ads/DockWidgetTab.h
+++ Source/ThirdParty/ads/DockWidgetTab.h
@@ -51,140 +51,140 @@ class CDockManager;
  */
 class ADS_EXPORT CDockWidgetTab : public QFrame
 {
-  Q_OBJECT
-  Q_PROPERTY(bool activeTab READ isActiveTab WRITE setActiveTab NOTIFY activeTabChanged)
-  Q_PROPERTY(QSize iconSize READ iconSize WRITE setIconSize)
+	Q_OBJECT
+	Q_PROPERTY(bool activeTab READ isActiveTab WRITE setActiveTab NOTIFY activeTabChanged)
+	Q_PROPERTY(QSize iconSize READ iconSize WRITE setIconSize)
 
 private:
-  DockWidgetTabPrivate* d; ///< private data (pimpl)
-  friend struct DockWidgetTabPrivate;
-  friend class CDockWidget;
-  friend class CDockManager;
-  friend class CAutoHideDockContainer;
-  void onDockWidgetFeaturesChanged();
+	DockWidgetTabPrivate* d; ///< private data (pimpl)
+	friend struct DockWidgetTabPrivate;
+	friend class CDockWidget;
+	friend class CDockManager;
+	friend class CAutoHideDockContainer;
+	void onDockWidgetFeaturesChanged();
 
 private Q_SLOTS:
-  void detachDockWidget();
-  void autoHideDockWidget();
-  void onAutoHideToActionClicked();
+	void detachDockWidget();
+	void autoHideDockWidget();
+	void onAutoHideToActionClicked();
 
 protected:
-  virtual void mousePressEvent(QMouseEvent* ev) override;
-  virtual void mouseReleaseEvent(QMouseEvent* ev) override;
-  virtual void mouseMoveEvent(QMouseEvent* ev) override;
-  virtual void contextMenuEvent(QContextMenuEvent* ev) override;
+	virtual void mousePressEvent(QMouseEvent* ev) override;
+	virtual void mouseReleaseEvent(QMouseEvent* ev) override;
+	virtual void mouseMoveEvent(QMouseEvent* ev) override;
+	virtual void contextMenuEvent(QContextMenuEvent* ev) override;
 
-  /**
-   * Double clicking the tab widget makes the assigned dock widget floating
-   */
-  virtual void mouseDoubleClickEvent(QMouseEvent *event) override;
+	/**
+	 * Double clicking the tab widget makes the assigned dock widget floating
+	 */
+	virtual void mouseDoubleClickEvent(QMouseEvent *event) override;
 
 public:
-  using Super = QFrame;
-  /**
-   * Default Constructor
-   * param[in] DockWidget The dock widget this title bar belongs to
-   * param[in] parent The parent widget of this title bar
-   */
-  CDockWidgetTab(CDockWidget* DockWidget, QWidget* parent = nullptr);
+	using Super = QFrame;
+	/**
+	 * Default Constructor
+	 * param[in] DockWidget The dock widget this title bar belongs to
+	 * param[in] parent The parent widget of this title bar
+	 */
+	CDockWidgetTab(CDockWidget* DockWidget, QWidget* parent = nullptr);
 
-  /**
-   * Virtual Destructor
-   */
-  virtual ~CDockWidgetTab();
+	/**
+	 * Virtual Destructor
+	 */
+	virtual ~CDockWidgetTab();
 
-  /**
-   * Returns true, if this is the active tab
-   */
-  bool isActiveTab() const;
+	/**
+	 * Returns true, if this is the active tab
+	 */
+	bool isActiveTab() const;
 
-  /**
-   * Set this true to make this tab the active tab
-   */
-  void setActiveTab(bool active);
+	/**
+	 * Set this true to make this tab the active tab
+	 */
+	void setActiveTab(bool active);
 
-  /**
-   * Sets the dock area widget the dockWidget returned by dockWidget()
-   * function belongs to.
-   */
-  void setDockAreaWidget(CDockAreaWidget* DockArea);
+	/**
+	 * Sets the dock area widget the dockWidget returned by dockWidget()
+	 * function belongs to.
+	 */
+	void setDockAreaWidget(CDockAreaWidget* DockArea);
 
-  /**
-   * Returns the dock area widget this title bar belongs to.
-   * \return This function returns 0 if the dock widget that owns this title
-   * bar widget has not been added to any dock area yet.
-   */
-  CDockAreaWidget* dockAreaWidget() const;
+	/**
+	 * Returns the dock area widget this title bar belongs to.
+	 * \return This function returns 0 if the dock widget that owns this title
+	 * bar widget has not been added to any dock area yet.
+	 */
+	CDockAreaWidget* dockAreaWidget() const;
 
-  /**
-   * Returns the dock widget this title widget belongs to
-   */
-  CDockWidget* dockWidget() const;
+	/**
+	 * Returns the dock widget this title widget belongs to
+	 */
+	CDockWidget* dockWidget() const;
 
-  /**
-   * Sets the icon to show in title bar
-   */
-  void setIcon(const QIcon& Icon);
+	/**
+	 * Sets the icon to show in title bar
+	 */
+	void setIcon(const QIcon& Icon);
 
-  /**
-   * Returns the icon
-   */
-  const QIcon& icon() const;
+	/**
+	 * Returns the icon
+	 */
+	const QIcon& icon() const;
 
-  /**
-   * Returns the tab text
-   */
-  QString text() const;
+	/**
+	 * Returns the tab text
+	 */
+	QString text() const;
 
-  /**
-   * Sets the tab text
-   */
-  void setText(const QString& title);
+	/**
+	 * Sets the tab text
+	 */
+	void setText(const QString& title);
 
-  /**
-   * Returns true if text is elided on the tab's title
-   */
-  bool isTitleElided() const;
+	/**
+	 * Returns true if text is elided on the tab's title
+	 */
+	bool isTitleElided() const;
 
-  /**
-   * This function returns true if the assigned dock widget is closable
-   */
-  bool isClosable() const;
+	/**
+	 * This function returns true if the assigned dock widget is closable
+	 */
+	bool isClosable() const;
 
-  /**
-  * Track event ToolTipChange and set child ToolTip
-  */
-  virtual bool event(QEvent *e) override;
+	/**
+	* Track event ToolTipChange and set child ToolTip 
+	*/
+	virtual bool event(QEvent *e) override;
 
-  /**
-   * Sets the text elide mode
-   */
-  void setElideMode(Qt::TextElideMode mode);
+	/**
+	 * Sets the text elide mode
+	 */
+	void setElideMode(Qt::TextElideMode mode);
 
-  /**
-   * Update stylesheet style if a property changes
-   */
-  void updateStyle();
+	/**
+	 * Update stylesheet style if a property changes
+	 */
+	void updateStyle();
 
-  /**
-   * Returns the icon size.
-   * If no explicit icon size has been set, the function returns an invalid
-   * QSize
-   */
-  QSize iconSize() const;
+	/**
+	 * Returns the icon size.
+	 * If no explicit icon size has been set, the function returns an invalid
+	 * QSize
+	 */
+	QSize iconSize() const;
 
-  /**
-   * Set an explicit icon size.
-   * If no icon size has been set explicitly, than the tab sets the icon size
-   * depending on the style
-   */
-  void setIconSize(const QSize& Size);
+	/**
+	 * Set an explicit icon size.
+	 * If no icon size has been set explicitly, than the tab sets the icon size
+	 * depending on the style
+	 */
+	void setIconSize(const QSize& Size);
 
-  /**
-   * Returns the current drag state of this tab.
-   * Use this function to determine if the tab is currently being dragged
-   */
-  eDragState dragState() const;
+	/**
+	 * Returns the current drag state of this tab.
+	 * Use this function to determine if the tab is currently being dragged
+	 */
+	eDragState dragState() const;
 
     /**
      * Fills the provided menu with standard entries. If a nullptr is passed, a
@@ -201,15 +201,15 @@ public:
     virtual QMenu *buildContextMenu(QMenu *);
 
 public Q_SLOTS:
-  virtual void setVisible(bool visible) override;
+	virtual void setVisible(bool visible) override;
 
 Q_SIGNALS:
-  void activeTabChanged();
-  void clicked();
-  void closeRequested();
-  void closeOtherTabsRequested();
-  void moved(const QPoint& GlobalPos);
-  void elidedChanged(bool elided);
+	void activeTabChanged();
+	void clicked();
+	void closeRequested();
+	void closeOtherTabsRequested();
+	void moved(const QPoint& GlobalPos);
+	void elidedChanged(bool elided);
 }; // class DockWidgetTab
 }
  // namespace ads
