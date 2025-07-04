95114e3f648d2afac337d5cec15545ba951ceb9d
Theophilus Eriata <theoeriata@gmail.com>
2025-07-04 00:57:57 -0600

    GraphicsCore: Add Render Pass Pin Properties And Serialization.

--- Source/Engine/GraphicsCore/Pipeline/Implementation/RenderPipelineNode.cpp
+++ Source/Engine/GraphicsCore/Pipeline/Implementation/RenderPipelineNode.cpp
@@ -3,11 +3,21 @@
 #include <GraphicsCore/Pipeline/RenderPipelineNode.h>
 
 // clang-format off
-XII_BEGIN_DYNAMIC_REFLECTED_TYPE(xiiRenderPipelineNode, 1, xiiRTTINoAllocator)
-XII_END_DYNAMIC_REFLECTED_TYPE;
-
 XII_BEGIN_STATIC_REFLECTED_TYPE(xiiRenderPipelineNodePin, xiiNoBase, 1, xiiRTTINoAllocator)
 {
+  XII_BEGIN_PROPERTIES
+  {
+    XII_ENUM_MEMBER_PROPERTY("Format", xiiSourceFormat, m_Format)->AddAttributes(new xiiDefaultValueAttribute(xiiSourceFormat::Color4Channel8BitNormalized_sRGB)),
+    XII_ENUM_MEMBER_PROPERTY("SampleCount", xiiGALMSAASampleCount, m_SampleCount)->AddAttributes(new xiiDefaultValueAttribute(xiiGALMSAASampleCount::OneSample)),
+    XII_ENUM_MEMBER_PROPERTY("LoadOperation", xiiGALAttachmentLoadOperation, m_AttachmentLoadOperation)->AddAttributes(new xiiDefaultValueAttribute(xiiGALAttachmentLoadOperation::Load)),
+    XII_ENUM_MEMBER_PROPERTY("StoreOperation", xiiGALAttachmentStoreOperation, m_AttachmentStoreOperation)->AddAttributes(new xiiDefaultValueAttribute(xiiGALAttachmentStoreOperation::Store)),
+    XII_ENUM_MEMBER_PROPERTY("StencilLoadOperation", xiiGALAttachmentLoadOperation, m_AttachmentStencilLoadOperation)->AddAttributes(new xiiDefaultValueAttribute(xiiGALAttachmentLoadOperation::Load)),
+    XII_ENUM_MEMBER_PROPERTY("StencilStoreOperation", xiiGALAttachmentStoreOperation, m_AttachmentStencilStoreOperation)->AddAttributes(new xiiDefaultValueAttribute(xiiGALAttachmentStoreOperation::Store)),
+    XII_MEMBER_PROPERTY("ClearColor", m_ClearColor)->AddAttributes(new xiiExposeColorAlphaAttribute()),
+    XII_MEMBER_PROPERTY("DepthClearValue", m_fDepthClearValue)->AddAttributes(new xiiDefaultValueAttribute(1.0f)),
+    XII_MEMBER_PROPERTY("StencilClearValue", m_uiStencilClearValue)->AddAttributes(new xiiDefaultValueAttribute(0U)),
+  }
+  XII_END_PROPERTIES;
   XII_BEGIN_ATTRIBUTES
   {
    new xiiHiddenAttribute(),
@@ -30,7 +40,44 @@ XII_END_STATIC_REFLECTED_TYPE;
 
 XII_BEGIN_STATIC_REFLECTED_TYPE(xiiRenderPipelineNodePassThroughPin, xiiRenderPipelineNodePin, 1, xiiRTTINoAllocator)
 XII_END_STATIC_REFLECTED_TYPE;
+
+XII_BEGIN_DYNAMIC_REFLECTED_TYPE(xiiRenderPipelineNode, 1, xiiRTTINoAllocator)
+XII_END_DYNAMIC_REFLECTED_TYPE;
 // clang-format on
+
+///////////////////////////////////////////////////////////////////////////////
+
+xiiResult xiiRenderPipelineNodePin::Serialize(xiiStreamWriter& inout_stream) const
+{
+  inout_stream << m_Format;
+  inout_stream << m_SampleCount;
+  inout_stream << m_AttachmentLoadOperation;
+  inout_stream << m_AttachmentStoreOperation;
+  inout_stream << m_AttachmentStencilLoadOperation;
+  inout_stream << m_AttachmentStencilStoreOperation;
+  inout_stream << m_ClearColor;
+  inout_stream << m_fDepthClearValue;
+  inout_stream << m_uiStencilClearValue;
+
+  return XII_SUCCESS;
+}
+
+xiiResult xiiRenderPipelineNodePin::Deserialize(xiiStreamReader& inout_stream)
+{
+  inout_stream >> m_Format;
+  inout_stream >> m_SampleCount;
+  inout_stream >> m_AttachmentLoadOperation;
+  inout_stream >> m_AttachmentStoreOperation;
+  inout_stream >> m_AttachmentStencilLoadOperation;
+  inout_stream >> m_AttachmentStencilStoreOperation;
+  inout_stream >> m_ClearColor;
+  inout_stream >> m_fDepthClearValue;
+  inout_stream >> m_uiStencilClearValue;
+
+  return XII_SUCCESS;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 void xiiRenderPipelineNode::InitializePins()
 {
@@ -56,7 +103,7 @@ void xiiRenderPipelineNode::InitializePins()
     const bool bProviderOnPassThrough = pPin->m_Type.IsSet(xiiRenderPipelineNodePin::Type::PassThrough) && pPin->m_Type.IsSet(xiiRenderPipelineNodePin::Type::TextureProvider);
     if (bMoreThanOneType || bProviderOnPassThrough)
     {
-      XII_REPORT_FAILURE("Pin '{0}' has an invalid type. Do not use xiiRenderPipelineNodePin directly as member but one of its derived types", pProperty->GetPropertyName());
+      XII_REPORT_FAILURE("Pin '{0}' has an invalid type. Do not use xiiRenderPipelineNodePin directly as member but one of its derived types.", pProperty->GetPropertyName());
       continue;
     }
 
--- Source/Engine/GraphicsCore/Pipeline/RenderPipelineNode.h
+++ Source/Engine/GraphicsCore/Pipeline/RenderPipelineNode.h
@@ -2,6 +2,10 @@
 
 #include <GraphicsCore/GraphicsCoreDLL.h>
 
+#include <GraphicsFoundation/Resources/RenderPass.h>
+
+#include <GraphicsCore/Pipeline/Declarations.h>
+
 class xiiRenderPipelineNode;
 
 struct xiiRenderPipelineNodePin
@@ -31,13 +35,29 @@ struct xiiRenderPipelineNodePin
     };
   };
 
+  xiiResult Serialize(xiiStreamWriter& inout_stream) const;
+
+  xiiResult Deserialize(xiiStreamReader& inout_stream);
+
   xiiBitflags<Type>      m_Type;
   xiiUInt8               m_uiInputIndex  = 0xFFU;
   xiiUInt8               m_uiOutputIndex = 0xFFU;
   xiiRenderPipelineNode* m_pParent       = nullptr;
+
+  xiiEnum<xiiSourceFormat>                m_Format                          = xiiSourceFormat::Default;
+  xiiEnum<xiiGALMSAASampleCount>          m_SampleCount                     = xiiGALMSAASampleCount::OneSample;
+  xiiEnum<xiiGALAttachmentLoadOperation>  m_AttachmentLoadOperation         = xiiGALAttachmentLoadOperation::Load;
+  xiiEnum<xiiGALAttachmentStoreOperation> m_AttachmentStoreOperation        = xiiGALAttachmentStoreOperation::Store;
+  xiiEnum<xiiGALAttachmentLoadOperation>  m_AttachmentStencilLoadOperation  = xiiGALAttachmentLoadOperation::Load;
+  xiiEnum<xiiGALAttachmentStoreOperation> m_AttachmentStencilStoreOperation = xiiGALAttachmentStoreOperation::Store;
+  xiiColor                                m_ClearColor                      = xiiColor::Black;
+  float                                   m_fDepthClearValue                = 1.0f;
+  xiiUInt8                                m_uiStencilClearValue             = 0U;
 };
 
 XII_DECLARE_FLAGS_OPERATORS(xiiRenderPipelineNodePin::Type);
+
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodePin);
 
 struct xiiRenderPipelineNodeInputPin : public xiiRenderPipelineNodePin
 {
@@ -46,12 +66,16 @@ struct xiiRenderPipelineNodeInputPin : public xiiRenderPipelineNodePin
   XII_ALWAYS_INLINE xiiRenderPipelineNodeInputPin() { m_Type = Type::Input; }
 };
 
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeInputPin);
+
 struct xiiRenderPipelineNodeOutputPin : public xiiRenderPipelineNodePin
 {
   XII_DECLARE_POD_TYPE();
 
   XII_ALWAYS_INLINE xiiRenderPipelineNodeOutputPin() { m_Type = Type::Output; }
 };
+
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeOutputPin);
 
 struct xiiRenderPipelineNodeInputProviderPin : public xiiRenderPipelineNodeInputPin
 {
@@ -60,6 +84,8 @@ struct xiiRenderPipelineNodeInputProviderPin : public xiiRenderPipelineNodeInput
   XII_ALWAYS_INLINE xiiRenderPipelineNodeInputProviderPin() { m_Type = Type::Input | Type::TextureProvider; }
 };
 
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeInputProviderPin);
+
 struct xiiRenderPipelineNodeOutputProviderPin : public xiiRenderPipelineNodeOutputPin
 {
   XII_DECLARE_POD_TYPE();
@@ -67,12 +93,16 @@ struct xiiRenderPipelineNodeOutputProviderPin : public xiiRenderPipelineNodeOutp
   XII_ALWAYS_INLINE xiiRenderPipelineNodeOutputProviderPin() { m_Type = Type::Output | Type::TextureProvider; }
 };
 
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeOutputProviderPin);
+
 struct xiiRenderPipelineNodePassThroughPin : public xiiRenderPipelineNodePin
 {
   XII_DECLARE_POD_TYPE();
 
   XII_ALWAYS_INLINE xiiRenderPipelineNodePassThroughPin() { m_Type = Type::PassThrough; }
 };
+
+XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodePassThroughPin);
 
 class XII_GRAPHICSCORE_DLL xiiRenderPipelineNode : public xiiReflectedClass
 {
@@ -95,10 +125,3 @@ private:
   xiiDynamicArray<const xiiRenderPipelineNodePin*>               m_OutputPins;
   xiiHashTable<xiiHashedString, const xiiRenderPipelineNodePin*> m_NameToPin;
 };
-
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodePin);
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeInputPin);
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeOutputPin);
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeInputProviderPin);
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodeOutputProviderPin);
-XII_DECLARE_REFLECTABLE_TYPE(XII_GRAPHICSCORE_DLL, xiiRenderPipelineNodePassThroughPin);
