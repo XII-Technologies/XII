a80ce58631b03dd2efe4e05811a3ba224e893f89
Theophilus Eriata <theoeriata@gmail.com>
2025-09-08 00:03:35 -0600

    GraphicsCore: Implement Depth Only Pass And Sprite Renderer.

--- Source/Engine/GraphicsCore/Components/Implementation/SpriteRenderer.cpp
+++ Source/Engine/GraphicsCore/Components/Implementation/SpriteRenderer.cpp
@@ -1,12 +1,15 @@
 #include <GraphicsCore/GraphicsCorePCH.h>
 
+#include <Core/Graphics/Camera.h>
 #include <Foundation/Math/Float16.h>
 #include <Foundation/Types/ScopeExit.h>
 #include <GraphicsCore/Components/SpriteComponent.h>
 #include <GraphicsCore/Components/SpriteRenderer.h>
 #include <GraphicsCore/GPUResourcePool/GPUResourcePool.h>
 #include <GraphicsCore/Pipeline/RenderDataBatch.h>
+#include <GraphicsCore/Shader/ShaderPermutationUtilities.h>
 #include <GraphicsCore/Shader/ShaderResource.h>
+#include <GraphicsCore/Textures/Texture2DResource.h>
 #include <GraphicsFoundation/Resources/Buffer.h>
 #include <GraphicsFoundation/Shader/ShaderUtils.h>
 #include <GraphicsFoundation/Utilities/DeviceUtilities.h>
@@ -46,6 +49,36 @@ void xiiSpriteRenderer::RenderBatch(const xiiRenderViewContext& renderViewContex
   const xiiUInt32            uiBufferSize = xiiMath::RoundUp(batch.GetCount(), 128u);
   xiiSharedPtr<xiiGALBuffer> pSpriteData  = CreateSpriteDataBuffer(uiBufferSize);
   XII_SCOPE_EXIT(DeleteSpriteDataBuffer(pSpriteData));
+
+  renderViewContext.SetShaderPermutationVariable("BLEND_MODE", xiiSpriteBlendMode::GetPermutationValue(pRenderData->m_BlendMode));
+  renderViewContext.SetShaderPermutationVariable("SHAPE_ICON", pRenderData->m_BlendMode == xiiSpriteBlendMode::ShapeIcon ? xiiMakeHashedString("TRUE") : xiiMakeHashedString("FALSE"));
+
+  xiiSharedPtr<xiiGALGraphicsPipelineState> pGraphicsPipelineState = CreatePipelineState(renderViewContext);
+
+  renderViewContext.m_pCommandList->SetPipelineState(pGraphicsPipelineState);
+  renderViewContext.m_pCommandList->ResolveAndSetShaderResourceBufferView("xiiGlobalConstants", renderViewContext.m_CommandListData.m_pGlobalConstants->GetDefaultView(xiiGALBufferViewType::ShaderResource));
+  renderViewContext.m_pCommandList->ResolveAndSetShaderResourceBufferView("spriteData", pSpriteData->GetDefaultView(xiiGALBufferViewType::ShaderResource));
+  {
+    xiiResourceLock<xiiTexture2DResource> pTexture(pRenderData->m_hTexture, xiiResourceAcquireMode::AllowLoadingFallback);
+
+    renderViewContext.m_pCommandList->ResolveAndSetShaderResourceTextureView("spriteTexture", pTexture->GetGALTexture()->GetDefaultView(xiiGALTextureViewType::ShaderResource));
+    renderViewContext.m_pCommandList->ResolveAndSetSampler("spriteTexture", pTexture->GetGALSampler());
+  }
+
+  FillSpriteData(batch);
+
+  if (!m_SpriteData.IsEmpty()) // Instance data might be empty if all render data was filtered.
+  {
+    xiiGALDeviceUtilities::MapAndUpdateBuffer(renderViewContext.m_pCommandList.Borrow(), pSpriteData, 0U, m_SpriteData.GetByteArrayPtr()).AssertSuccess();
+
+    const xiiUInt32 uiVertsPerPrimitive = xiiGALPrimitiveTopology::VerticesPerPrimitive(pGraphicsPipelineState->GetDescription().m_GraphicsPipeline.m_PrimitiveTopology);
+    xiiUInt32       uiPrimitiveCount    = m_SpriteData.GetCount() * 2U * uiVertsPerPrimitive;
+    xiiUInt32       uiInstanceCount     = renderViewContext.m_pCamera->IsStereoscopic() ? 2U : 1U;
+
+    renderViewContext.m_pCommandList->BeginRenderPass({renderViewContext.m_CommandListData.m_pRenderPass, renderViewContext.m_CommandListData.m_pFramebuffer});
+    renderViewContext.m_pCommandList->Draw({uiPrimitiveCount, uiInstanceCount});
+    renderViewContext.m_pCommandList->EndRenderPass();
+  }
 
 #ifdef CORE_ENABLE
   pContext->BindShader(m_hShader);
@@ -95,19 +128,45 @@ void xiiSpriteRenderer::FillSpriteData(const xiiRenderDataBatch& batch) const
   {
     const xiiSpriteRenderData* pRenderData = it;
 
-    auto& spriteData = m_SpriteData.ExpandAndGetRef();
+    xiiPerSpriteData& spriteData = m_SpriteData.ExpandAndGetRef();
 
     spriteData.WorldSpacePosition = pRenderData->m_GlobalTransform.m_vPosition;
     spriteData.Size               = pRenderData->m_fSize;
     spriteData.MaxScreenSize      = pRenderData->m_fMaxScreenSize;
     spriteData.AspectRatio        = pRenderData->m_fAspectRatio;
-    spriteData.ColorRG            = xiiGALShaderUtilities::Float2ToRG16F(xiiVec2(pRenderData->m_color.r, pRenderData->m_color.g));
-    spriteData.ColorBA            = xiiGALShaderUtilities::Float2ToRG16F(xiiVec2(pRenderData->m_color.b, pRenderData->m_color.a));
-    spriteData.TexCoordScale      = xiiGALShaderUtilities::Float2ToRG16F(pRenderData->m_texCoordScale);
-    spriteData.TexCoordOffset     = xiiGALShaderUtilities::Float2ToRG16F(pRenderData->m_texCoordOffset);
+    spriteData.ColorRG            = xiiGALShaderUtilities::Float2ToRG16F(xiiVec2(pRenderData->m_Colour.r, pRenderData->m_Colour.g));
+    spriteData.ColorBA            = xiiGALShaderUtilities::Float2ToRG16F(xiiVec2(pRenderData->m_Colour.b, pRenderData->m_Colour.a));
+    spriteData.TexCoordScale      = xiiGALShaderUtilities::Float2ToRG16F(pRenderData->m_vTextureCoordScale);
+    spriteData.TexCoordOffset     = xiiGALShaderUtilities::Float2ToRG16F(pRenderData->m_vTextureCoordOffset);
     spriteData.GameObjectID       = pRenderData->m_uiUniqueID;
-    spriteData.Reserved           = 0;
+    spriteData.Reserved           = 0U;
   }
+}
+
+xiiSharedPtr<xiiGALGraphicsPipelineState> xiiSpriteRenderer::CreatePipelineState(const xiiRenderViewContext& renderViewContext) const
+{
+  xiiSharedPtr<xiiGALDevice>         pDevice            = xiiGALDevice::GetDefaultDevice();
+  xiiShaderPermutationResourceHandle hShaderPermutation = xiiShaderPermutationUtilities::PreloadSinglePermutation(m_hShader, renderViewContext.GetPermutationVariables(), true);
+
+  xiiGALGraphicsPipelineStateCreationDescription graphicsPipelineStateDescription;
+  graphicsPipelineStateDescription.m_GraphicsPipeline.m_pRenderPass       = renderViewContext.m_CommandListData.m_pRenderPass;
+  graphicsPipelineStateDescription.m_GraphicsPipeline.m_PrimitiveTopology = xiiGALPrimitiveTopology::TriangleList;
+  graphicsPipelineStateDescription.m_GraphicsPipeline.m_uiSampleMask      = 0xFFFFFFFFU;
+
+  {
+    xiiResourceLock<xiiShaderPermutationResource> pShaderPermutation(hShaderPermutation, xiiResourceAcquireMode::BlockTillLoaded);
+
+    graphicsPipelineStateDescription.m_pPipelineResourceSignature = pShaderPermutation->GetPipelineResourceSignature();
+    graphicsPipelineStateDescription.m_pVertexShader              = pShaderPermutation->GetGALShader(xiiGALShaderType::Vertex);
+    graphicsPipelineStateDescription.m_pGeometryShader            = pShaderPermutation->GetGALShader(xiiGALShaderType::Geometry);
+    graphicsPipelineStateDescription.m_pPixelShader               = pShaderPermutation->GetGALShader(xiiGALShaderType::Pixel);
+
+    graphicsPipelineStateDescription.m_GraphicsPipeline.m_pBlendState        = pShaderPermutation->GetBlendState();
+    graphicsPipelineStateDescription.m_GraphicsPipeline.m_pRasterizerState   = pShaderPermutation->GetRasterizerState();
+    graphicsPipelineStateDescription.m_GraphicsPipeline.m_pDepthStencilState = pShaderPermutation->GetDepthStencilState();
+  }
+
+  return pDevice->CreateGraphicsPipelineState(graphicsPipelineStateDescription);
 }
 
 XII_STATICLINK_FILE(GraphicsCore, GraphicsCore_Components_Implementation_SpriteRenderer);
