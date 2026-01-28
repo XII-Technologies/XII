d80ed0cefc590524b602747971042ce7463f70e0
XII Automation <automation@xiitechnologies.com>
2026-01-28 15:10:16 +0000

    Apply Code Format Rules.

--- Source/Engine/GraphicsCore/RenderContext/Implementation/RenderContext.cpp
+++ Source/Engine/GraphicsCore/RenderContext/Implementation/RenderContext.cpp
@@ -7,6 +7,7 @@
 #include <GraphicsCore/Material/MaterialResource.h>
 #include <GraphicsCore/Meshes/DynamicMeshBufferResource.h>
 #include <GraphicsCore/RenderContext/RenderContext.h>
+#include <GraphicsCore/RenderContext/RendererFallbackResources.h>
 #include <GraphicsCore/Shader/ShaderPermutationUtilities.h>
 #include <GraphicsCore/Textures/Texture2DResource.h>
 #include <GraphicsCore/Textures/Texture3DResource.h>
@@ -14,7 +15,6 @@
 #include <GraphicsCore/Textures/TextureUtils.h>
 #include <GraphicsFoundation/Shader/ShaderUtils.h>
 #include <GraphicsFoundation/ShaderCompiler/ShaderManager.h>
-#include <GraphicsCore/RenderContext/RendererFallbackResources.h>
 
 #include <GraphicsCore/../../../Data/Base/Shaders/Common/GlobalConstants.h>
 
