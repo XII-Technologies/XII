40401b646aa10ddb2372b697fbf85343c040dd7d
Theophilus Eriata <theoeriata@gmail.com>
2026-02-01 09:04:59 -0700

    GraphicsCore: Add Renderer Implementation Stubs.

--- Source/Engine/GraphicsCore/GraphicsCorePCH.h
+++ Source/Engine/GraphicsCore/GraphicsCorePCH.h
@@ -2,17 +2,3 @@
 
 #include <Foundation/Basics.h>
 #include <Foundation/Logging/Log.h>
-
-#include <GraphicsFoundation/CommandEncoder/CommandList.h>
-#include <GraphicsFoundation/CommandEncoder/CommandQueue.h>
-#include <GraphicsFoundation/Declarations/Descriptors.h>
-#include <GraphicsFoundation/Resources/Buffer.h>
-#include <GraphicsFoundation/Resources/Framebuffer.h>
-#include <GraphicsFoundation/Resources/RenderPass.h>
-#include <GraphicsFoundation/Resources/Texture.h>
-#include <GraphicsFoundation/ShaderCompiler/Descriptors.h>
-#include <GraphicsFoundation/Tools/MapHelper.h>
-#include <GraphicsFoundation/Tools/ScopedDebugGroup.h>
-#include <GraphicsFoundation/Utilities/DeviceUtilities.h>
-#include <GraphicsFoundation/Utilities/GraphicsUtilities.h>
-#include <GraphicsFoundation/Utilities/TextureUtilities.h>
