08cc83bc1ef4d608a6ca88ffd886d620ed8a0840
Theophilus Eriata <theoeriata@gmail.com>
2026-04-28 11:30:35 -0600

    ToolsFoundationTest: Add Copyright Header.

--- Source/UnitTests/ToolsFoundationTest/FileSystem/FileSystemModelTest.cpp
+++ Source/UnitTests/ToolsFoundationTest/FileSystem/FileSystemModelTest.cpp
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 #include <ToolsFoundationTest/ToolsFoundationTestPCH.h>
 
 #if XII_ENABLED(XII_SUPPORTS_DIRECTORY_WATCHER) && XII_ENABLED(XII_SUPPORTS_FILE_ITERATORS)
--- Source/UnitTests/ToolsFoundationTest/Reflection/ReflectionTest.cpp
+++ Source/UnitTests/ToolsFoundationTest/Reflection/ReflectionTest.cpp
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 #include <ToolsFoundationTest/ToolsFoundationTestPCH.h>
 
 #include <Foundation/Reflection/Reflection.h>
