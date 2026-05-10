cccb0bbbbb4237d09475d882c9869130223cda12
XII Automation <automation@xiitechnologies.com>
2026-05-10 07:43:09 +0000

    Apply copyright header to modified files.

--- Source/UnitTests/GameEngineTest/DataStructures/DynamicOctreeTest.cpp
+++ Source/UnitTests/GameEngineTest/DataStructures/DynamicOctreeTest.cpp
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 #include <GameEngineTest/GameEngineTestPCH.h>
 
 #include <Foundation/Containers/Deque.h>
--- Source/UnitTests/GameEngineTest/GameEngineTestPCH.h
+++ Source/UnitTests/GameEngineTest/GameEngineTestPCH.h
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 #include <TestFramework/Framework/TestFramework.h>
 
 #include <Foundation/Basics.h>
