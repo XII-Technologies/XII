aef23e171c8f99dc2396a29a49d9c08ac433e183
Theophilus Eriata <theoeriata@gmail.com>
2026-05-14 00:57:34 -0600

    GameEngine: Add Fileserve And Inspector As Dependency.

--- Source/Engine/GameEngine/finalize.cmake
+++ Source/Engine/GameEngine/finalize.cmake
@@ -7,3 +7,11 @@ endif()
 if (TARGET GraphicsD3D12)
   add_dependencies(GameEngine GraphicsD3D12)
 endif()
+
+if (TARGET InspectorPlugin)
+  add_dependencies(GameEngine InspectorPlugin)
+endif()
+
+if (TARGET FileservePlugin)
+  add_dependencies(GameEngine FileservePlugin)
+endif()
