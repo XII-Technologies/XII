af97f868d1897a9b2705a30c2fc0ba622d5dca37
Theophilus Eriata <theoeriata@gmail.com>
2026-05-19 01:23:55 -0600

    GraphicsD3D12: Implement Query Lifecycle, Allocation, And Readback.

--- Source/Engine/GraphicsD3D12/Resources/QueryD3D12.h
+++ Source/Engine/GraphicsD3D12/Resources/QueryD3D12.h
@@ -6,6 +6,9 @@
 
 #include <GraphicsFoundation/Resources/Query.h>
 
+class xiiGALCommandListD3D12;
+class xiiGALQueryPoolD3D12;
+
 class XII_GRAPHICSD3D12_DLL xiiGALQueryD3D12 final : public xiiGALQuery
 {
   XII_ADD_DYNAMIC_REFLECTION(xiiGALQueryD3D12, xiiGALQuery);
@@ -14,6 +17,18 @@ public:
   virtual bool GetData(void* pData, xiiUInt32 uiDataSize, bool bAutoInvalidate = true) override final;
 
   virtual void Invalidate() override final;
+
+  [[nodiscard]] XII_ALWAYS_INLINE xiiUInt32 GetQueryPoolIndex(xiiUInt32 uiQueryID) const
+  {
+    XII_ASSERT_DEV(uiQueryID == 0U || (m_Description.m_Type == xiiGALQueryType::Duration && uiQueryID == 1U), "");
+
+    return m_QueryPoolIndices[uiQueryID];
+  }
+
+  [[nodiscard]] XII_ALWAYS_INLINE xiiGALQueryPoolD3D12* GetQueryPoolD3D12() const { return m_pQueryPoolD3D12; }
+
+  bool OnBeginQuery(xiiGALCommandListD3D12* pCommandListD3D12);
+  bool OnEndQuery(xiiGALCommandListD3D12* pCommandListD3D12);
 
 protected:
   friend class xiiGALDeviceD3D12;
@@ -26,4 +41,14 @@ protected:
   virtual xiiResult InitPlatform() override final;
 
 private:
+  bool AllocateQueries();
+  void DiscardQueries();
+
+  [[nodiscard]] bool ReadbackQueryData(xiiUInt32 uiQueryID, void* pDestinationData, xiiUInt32 uiDataSize) const;
+
+  xiiStaticArray<xiiUInt32, 2U> m_QueryPoolIndices;
+  xiiUInt64                     m_uiQueryEndFenceValue = xiiInvalidIndex;
+  xiiBitflags<xiiGALCommandQueueFlags> m_QueryQueueFlags = xiiGALCommandQueueFlags::None;
+
+  xiiGALQueryPoolD3D12* m_pQueryPoolD3D12 = nullptr;
 };
