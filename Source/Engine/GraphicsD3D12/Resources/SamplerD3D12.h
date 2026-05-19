0af6f42df1946cf3f82fb1ca09e1b75469b99dbe
Theophilus Eriata <theoeriata@gmail.com>
2026-05-19 00:57:16 -0600

    GraphicsD3D12: Allocate Sampler Heap From Descriptor Pool.

--- Source/Engine/GraphicsD3D12/Resources/SamplerD3D12.h
+++ Source/Engine/GraphicsD3D12/Resources/SamplerD3D12.h
@@ -6,14 +6,14 @@
 
 #include <GraphicsFoundation/Resources/Sampler.h>
 
-struct ID3D12DescriptorHeap;
-
 class XII_GRAPHICSD3D12_DLL xiiGALSamplerD3D12 final : public xiiGALSampler
 {
   XII_ADD_DYNAMIC_REFLECTION(xiiGALSamplerD3D12, xiiGALSampler);
 
 public:
-  XII_ALWAYS_INLINE D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle() const { return {}; }
+  XII_ALWAYS_INLINE D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle() const { return m_CPUDescriptorHandle; }
+  XII_ALWAYS_INLINE D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle() const { return m_GPUDescriptorHandle; }
+  XII_ALWAYS_INLINE ID3D12DescriptorHeap*       GetDescriptorHeap() const { return m_pDescriptorHeap; }
 
 protected:
   friend class xiiGALDeviceD3D12;
@@ -26,5 +26,7 @@ protected:
   virtual xiiResult InitPlatform() override final;
 
 protected:
-  ID3D12DescriptorHeap* m_pDescriptorHeap = nullptr;
+  ID3D12DescriptorHeap*       m_pDescriptorHeap    = nullptr;
+  D3D12_CPU_DESCRIPTOR_HANDLE m_CPUDescriptorHandle = {};
+  D3D12_GPU_DESCRIPTOR_HANDLE m_GPUDescriptorHandle = {};
 };
