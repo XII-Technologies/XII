019b10750110c9dc7b566b61fba3e4d5f9c7cfeb
Theophilus Eriata <theoeriata@gmail.com>
2026-05-19 08:34:00 -0600

    GraphicsD3D12: Implement Top Level Acceleration Structure.

--- Source/Engine/GraphicsD3D12/Resources/TopLevelASD3D12.h
+++ Source/Engine/GraphicsD3D12/Resources/TopLevelASD3D12.h
@@ -6,11 +6,17 @@
 
 #include <GraphicsFoundation/Resources/TopLevelAS.h>
 
+struct ID3D12Resource;
+
 class XII_GRAPHICSD3D12_DLL xiiGALTopLevelASD3D12 final : public xiiGALTopLevelAS
 {
   XII_ADD_DYNAMIC_REFLECTION(xiiGALTopLevelASD3D12, xiiGALTopLevelAS);
 
 public:
+  [[nodiscard]] XII_ALWAYS_INLINE ID3D12Resource*    GetD3D12Resource() const { return m_pD3D12Resource; }
+  [[nodiscard]] XII_ALWAYS_INLINE xiiD3D12Allocation GetAllocationDescription() const { return m_ResourceAllocation; }
+  [[nodiscard]] xiiUInt64                            GetD3D12GPUVirtualAddress() const;
+
   virtual xiiGALTopLevelASInstanceDescription GetInstanceDescription(xiiStringView sName) const override final;
 
   virtual xiiGALTopLevelASBuildDescription GetBuildDescription() const override final;
@@ -30,4 +36,10 @@ protected:
   virtual void SetDebugNamePlatform(xiiStringView sName) const override final;
 
 private:
+  ID3D12Resource*                                              m_pD3D12Resource = nullptr;
+  xiiD3D12Allocation                                           m_ResourceAllocation = nullptr;
+  xiiUInt64                                                    m_uiAccelerationStructureSize = 0U;
+  xiiGALTopLevelASBuildDescription                             m_BuildDescription;
+  xiiGALScratchBufferSizeDescription                           m_ScratchBufferSizeDescription;
+  xiiHashTable<xiiStringView, xiiGALTopLevelASInstanceDescription> m_NameToInstance;
 };
