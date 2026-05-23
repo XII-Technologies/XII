43ed2a1a9e026f9185945327bde399a553399af7
Theophilus Eriata <theoeriata@gmail.com>
2026-05-23 00:55:00 -0600

    BuildSystem: Fix Shader Compiler Target Reference.

--- Source/Engine/GraphicsD3D12/Device/Implementation/DeviceD3D12.cpp
+++ Source/Engine/GraphicsD3D12/Device/Implementation/DeviceD3D12.cpp
@@ -76,7 +76,7 @@ XII_BEGIN_SUBSYSTEM_DECLARATION(GraphicsD3D12, DeviceFactory)
 
   ON_CORESYSTEMS_STARTUP
   {
-    const xiiGALDeviceImplementationDescription implementation = {.m_APIType = xiiGALGraphicsDeviceType::Direct3D12, .m_sShaderModel = "D3D_SM60", .m_sShaderCompiler = "xiiShaderCompilerDXC" };
+    const xiiGALDeviceImplementationDescription implementation = {.m_APIType = xiiGALGraphicsDeviceType::Direct3D12, .m_sShaderModel = "D3D_SM60", .m_sShaderCompiler = "xiiShaderCompilerDXIL" };
 
     xiiGALDeviceFactory::RegisterImplementation("D3D12", &CreateD3D12Device, implementation);
   }
