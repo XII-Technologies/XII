948d80766db4b4d8e63f26423ab0f22178dc3b80
Theophilus Eriata <theoeriata@gmail.com>
2026-06-27 06:32:35 -0600

    GraphicsD3D12: Update Resource Format Conversions.

--- Source/Engine/GraphicsD3D12/Utilities/Implementation/D3D12TypeConversions_inl.h
+++ Source/Engine/GraphicsD3D12/Utilities/Implementation/D3D12TypeConversions_inl.h
@@ -374,6 +374,12 @@ XII_ALWAYS_INLINE DXGI_FORMAT xiiD3D12TypeConversions::GetFormat(xiiGALResourceF
       return DXGI_FORMAT_BC7_UNORM;
     case xiiGALResourceFormat::BC7UNormalizedSRGB:
       return DXGI_FORMAT_BC7_UNORM_SRGB;
+    case xiiGALResourceFormat::NV12:
+      return DXGI_FORMAT_NV12;
+    case xiiGALResourceFormat::P010:
+      return DXGI_FORMAT_P010;
+    case xiiGALResourceFormat::P016:
+      return DXGI_FORMAT_P016;
 
       XII_DEFAULT_CASE_NOT_IMPLEMENTED;
   }
@@ -584,6 +590,12 @@ XII_ALWAYS_INLINE xiiGALResourceFormat::Enum xiiD3D12TypeConversions::GetGALForm
       return xiiGALResourceFormat::BC7UNormalized;
     case DXGI_FORMAT_BC7_UNORM_SRGB:
       return xiiGALResourceFormat::BC7UNormalizedSRGB;
+    case DXGI_FORMAT_NV12:
+      return xiiGALResourceFormat::NV12;
+    case DXGI_FORMAT_P010:
+      return xiiGALResourceFormat::P010;
+    case DXGI_FORMAT_P016:
+      return xiiGALResourceFormat::P016;
 
       XII_DEFAULT_CASE_NOT_IMPLEMENTED;
   }
