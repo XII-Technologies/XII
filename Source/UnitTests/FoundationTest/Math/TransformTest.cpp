68ebd3dec3901f53715cec2fd4a8f7a49a36c1a3
XII Automation <automation@xiitechnologies.com>
2026-09-15 14:23:26 +0000

    Apply copyright header to modified files.

--- Source/UnitTests/FoundationTest/Math/TransformTest.cpp
+++ Source/UnitTests/FoundationTest/Math/TransformTest.cpp
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 #include <FoundationTest/FoundationTestPCH.h>
 
 #include <Foundation/Math/Transform.h>
--- Utilities/ClangTidy/XII/NameCheck.cpp
+++ Utilities/ClangTidy/XII/NameCheck.cpp
@@ -1,3 +1,5 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
 //===--- MemberVarCheckCheck.cpp - clang-tidy -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
