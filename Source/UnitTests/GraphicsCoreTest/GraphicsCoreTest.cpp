767d9a0318c45d7e20ee764f1eb8a41b03bcec98
Theophilus Eriata <theoeriata@gmail.com>
2026-09-25 07:24:54 -0600

    GraphicsCoreTest: Cover Scene and Meshlet Systems.

---
+++ Source/UnitTests/GraphicsCoreTest/GraphicsCoreTest.cpp
@@ -0,0 +1,8 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
+#include <GraphicsCoreTest/GraphicsCoreTestPCH.h>
+
+#include <TestFramework/Framework/TestFramework.h>
+#include <TestFramework/Utilities/TestSetup.h>
+
+XII_TESTFRAMEWORK_ENTRY_POINT("GraphicsCoreTest", "GraphicsCore Tests")
---
+++ Source/UnitTests/GraphicsCoreTest/GraphicsCoreTestPCH.h
@@ -0,0 +1,6 @@
+/// Copyright (c) Theophilus Eriata. All Rights Reserved.
+
+#pragma once
+
+#include <Foundation/Basics.h>
+#include <TestFramework/Framework/TestFramework.h>
