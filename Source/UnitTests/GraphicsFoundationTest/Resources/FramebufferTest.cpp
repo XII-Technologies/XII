8e6984d6a78fe0860333f1ac616f2eedf24ff172
Theophilus Eriata <theoeriata@gmail.com>
2026-09-26 00:08:50 -0400

    GraphicsFoundationTest: Cover Synchronization And Render Targets

--- Source/UnitTests/GraphicsFoundationTest/Resources/FramebufferTest.cpp
+++ Source/UnitTests/GraphicsFoundationTest/Resources/FramebufferTest.cpp
@@ -1,3 +1,62 @@
 /// Copyright (c) Theophilus Eriata. All Rights Reserved.
 
 #include <GraphicsFoundationTest/GraphicsFoundationTestPCH.h>
+
+#include <GraphicsFoundation/Resources/Framebuffer.h>
+
+XII_CREATE_SIMPLE_TEST(Resources, Framebuffer)
+{
+  XII_TEST_BLOCK(xiiTestBlock::Enabled, "Compatible render target")
+  {
+    for (xiiUInt32 uiImplementation = 0; uiImplementation < xiiGetGPUTestingEnvironmentCount(); ++uiImplementation)
+    {
+      xiiGPUTestingEnvironment environment(xiiGetGPUTestingEnvironmentName(uiImplementation));
+      XII_TEST_BOOL(environment.Initialize().Succeeded());
+      if (environment.GetDevice() == nullptr)
+        continue;
+
+      xiiGALRenderPassCreationDescription renderPassDescription;
+      auto& attachment                = renderPassDescription.m_Attachments.ExpandAndGetRef();
+      attachment.m_Format            = xiiGALResourceFormat::RGBA8UNormalized;
+      attachment.m_uiSampleCount     = 1U;
+      attachment.m_LoadOperation     = xiiGALAttachmentLoadOperation::Clear;
+      attachment.m_StoreOperation    = xiiGALAttachmentStoreOperation::Store;
+      attachment.m_InitialStateFlags = xiiGALResourceStateFlags::Undefined;
+      attachment.m_FinalStateFlags   = xiiGALResourceStateFlags::ShaderResource;
+      renderPassDescription.m_SubPasses.ExpandAndGetRef().m_RenderTargetAttachments.PushBack({0U, xiiGALResourceStateFlags::RenderTarget});
+      xiiSharedPtr<xiiGALRenderPass> pRenderPass = environment.GetDevice()->CreateRenderPass(renderPassDescription);
+      XII_TEST_BOOL(pRenderPass != nullptr);
+      if (pRenderPass == nullptr)
+        continue;
+
+      xiiGALTextureCreationDescription textureDescription;
+      textureDescription.m_Type       = xiiGALResourceDimension::Texture2D;
+      textureDescription.m_Size       = xiiSizeU32(64U, 32U);
+      textureDescription.m_Format     = xiiGALResourceFormat::RGBA8UNormalized;
+      textureDescription.m_BindFlags  = xiiGALBindFlags::RenderTarget | xiiGALBindFlags::ShaderResource;
+      xiiSharedPtr<xiiGALTexture> pTexture = environment.GetDevice()->CreateTexture(textureDescription);
+      XII_TEST_BOOL(pTexture != nullptr);
+      if (pTexture == nullptr)
+        continue;
+
+      xiiSharedPtr<xiiGALTextureView> pRenderTargetView = pTexture->GetDefaultView(xiiGALTextureViewType::RenderTarget);
+      XII_TEST_BOOL(pRenderTargetView != nullptr);
+      if (pRenderTargetView == nullptr)
+        continue;
+
+      xiiGALFramebufferCreationDescription description;
+      description.m_pRenderPass      = pRenderPass;
+      description.m_Attachments.PushBack(pRenderTargetView);
+      description.m_FramebufferSize  = xiiSizeU32(64U, 32U);
+      description.m_uiArraySliceCount = 1U;
+
+      xiiSharedPtr<xiiGALFramebuffer> pFramebuffer = environment.GetDevice()->CreateFramebuffer(description);
+      XII_TEST_BOOL(pFramebuffer != nullptr);
+      if (pFramebuffer != nullptr)
+      {
+        XII_TEST_BOOL(pFramebuffer->GetDescription() == description);
+        XII_TEST_BOOL(pFramebuffer->GetDevice().Borrow() == environment.GetDevice());
+      }
+    }
+  }
+}
--- Source/UnitTests/GraphicsFoundationTest/Resources/QueryTest.cpp
+++ Source/UnitTests/GraphicsFoundationTest/Resources/QueryTest.cpp
@@ -1,3 +1,51 @@
 /// Copyright (c) Theophilus Eriata. All Rights Reserved.
 
 #include <GraphicsFoundationTest/GraphicsFoundationTestPCH.h>
+
+#include <GraphicsFoundation/Resources/Query.h>
+
+XII_CREATE_SIMPLE_TEST(Resources, Query)
+{
+  XII_TEST_BLOCK(xiiTestBlock::Enabled, "Supported query objects")
+  {
+    for (xiiUInt32 uiImplementation = 0; uiImplementation < xiiGetGPUTestingEnvironmentCount(); ++uiImplementation)
+    {
+      xiiGPUTestingEnvironment environment(xiiGetGPUTestingEnvironmentName(uiImplementation));
+      XII_TEST_BOOL(environment.Initialize().Succeeded());
+      if (environment.GetDevice() == nullptr)
+        continue;
+
+      struct QueryFeature
+      {
+        xiiGALQueryType::Enum m_Type;
+        xiiEnum<xiiGALDeviceFeatureState> xiiGALDeviceFeatures::*m_pFeature;
+      };
+
+      const QueryFeature queryFeatures[] = {
+        {xiiGALQueryType::Occlusion, &xiiGALDeviceFeatures::m_OcclusionQueries},
+        {xiiGALQueryType::BinaryOcclusion, &xiiGALDeviceFeatures::m_BinaryOcclusionQueries},
+        {xiiGALQueryType::Timestamp, &xiiGALDeviceFeatures::m_TimestampQueries},
+        {xiiGALQueryType::PipelineStatistics, &xiiGALDeviceFeatures::m_PipelineStatisticsQueries},
+        {xiiGALQueryType::Duration, &xiiGALDeviceFeatures::m_DurationQueries},
+      };
+
+      for (const QueryFeature& queryFeature : queryFeatures)
+      {
+        if (environment.GetDevice()->GetFeatures().*queryFeature.m_pFeature != xiiGALDeviceFeatureState::Enabled)
+          continue;
+
+        xiiGALQueryCreationDescription description;
+        description.m_Type = queryFeature.m_Type;
+        xiiSharedPtr<xiiGALQuery> pQuery = environment.GetDevice()->CreateQuery(description);
+        XII_TEST_BOOL(pQuery != nullptr);
+        if (pQuery != nullptr)
+        {
+          XII_TEST_BOOL(pQuery->GetDescription() == description);
+          XII_TEST_INT(static_cast<xiiUInt32>(pQuery->GetQueryState()), static_cast<xiiUInt32>(xiiGALQuery::QueryState::Inactive));
+          pQuery->Invalidate();
+          XII_TEST_INT(static_cast<xiiUInt32>(pQuery->GetQueryState()), static_cast<xiiUInt32>(xiiGALQuery::QueryState::Inactive));
+        }
+      }
+    }
+  }
+}
