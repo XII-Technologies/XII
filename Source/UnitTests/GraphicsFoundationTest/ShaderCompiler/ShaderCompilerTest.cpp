129f431c26129721bf1165c3df6acb9ae98582dd
Theophilus Eriata <theoeriata@gmail.com>
2026-09-26 01:06:08 -0400

    GraphicsFoundationTest: Cover Shader Binary Persistence

--- Source/UnitTests/GraphicsFoundationTest/ShaderCompiler/ShaderCompilerTest.cpp
+++ Source/UnitTests/GraphicsFoundationTest/ShaderCompiler/ShaderCompilerTest.cpp
@@ -2,9 +2,11 @@
 
 #include <GraphicsFoundationTest/GraphicsFoundationTestPCH.h>
 
+#include <Foundation/IO/MemoryStream.h>
 #include <Foundation/Logging/Log.h>
 #include <GraphicsFoundation/ShaderCompiler/PermutationGenerator.h>
 #include <GraphicsFoundation/ShaderCompiler/ShaderParser.h>
+#include <GraphicsFoundation/ShaderCompiler/ShaderPermutationBinary.h>
 #include <GraphicsFoundation/ShaderCompiler/ShaderTextSectionizer.h>
 
 XII_CREATE_SIMPLE_TEST_GROUP(ShaderCompiler);
@@ -208,5 +210,102 @@ cbuffer Globals : register(b1, space0)
 
     sanityBindings.Find(validBinding.m_sName).Value().m_uiBindIndex = xiiInvalidIndex;
     XII_TEST_BOOL(xiiGALShaderParser::SanityCheckShaderResourceBindings(sanityBindings, &muteLog).Failed());
+  }
+
+  XII_TEST_BLOCK(xiiTestBlock::Enabled, "Shader state parsing and serialization")
+  {
+    xiiGALShaderStateResourceDescriptor state;
+    XII_TEST_BOOL(state.Parse(
+      "AlphaToCoverage = true\n"
+      "IndependentBlend = true\n"
+      "BlendEnable0 = true\n"
+      "SourceBlend0 = BlendFactor_SourceAlpha\n"
+      "DestinationBlend0 = BlendFactor_InverseSourceAlpha\n"
+      "ColorMask0 = 5\n"
+      "FillMode = FillMode_Wireframe\n"
+      "CullMode = CullMode_Front\n"
+      "FrontCounterClockwise = true\n"
+      "DepthBias = -7\n"
+      "DepthBiasClamp = 1.25\n"
+      "SlopeScaledDepthBias = 2.5\n"
+      "DepthEnable = true\n"
+      "DepthWriteEnable = false\n"
+      "ComparisonDepthFunction = ComparisonFunction_GreaterEqual\n"
+      "StencilEnable = true\n"
+      "StencilReadMask = 63\n"
+      "StencilWriteMask = 31\n"
+      "FrontFaceStencilPassOperation = StencilOperation_Replace\n")
+                    .Succeeded());
+
+    XII_TEST_BOOL(state.m_BlendDescription.m_bAlphaToCoverage);
+    XII_TEST_BOOL(state.m_BlendDescription.m_bIndependentBlend);
+    XII_TEST_INT(state.m_BlendDescription.m_RenderTargets.GetCount(), 8U);
+    XII_TEST_BOOL(state.m_BlendDescription.m_RenderTargets[0].m_bBlendEnable);
+    XII_TEST_BOOL(state.m_BlendDescription.m_RenderTargets[0].m_SourceBlend == xiiGALBlendFactor::SourceAlpha);
+    XII_TEST_BOOL(state.m_BlendDescription.m_RenderTargets[0].m_DestinationBlend == xiiGALBlendFactor::InverseSourceAlpha);
+    XII_TEST_INT(state.m_BlendDescription.m_RenderTargets[0].m_ColorMask.GetValue(), 5U);
+    XII_TEST_BOOL(state.m_RasterizerDescription.m_FillMode == xiiGALFillMode::Wireframe);
+    XII_TEST_BOOL(state.m_RasterizerDescription.m_CullMode == xiiGALCullMode::Front);
+    XII_TEST_BOOL(state.m_RasterizerDescription.m_bFrontCounterClockwise);
+    XII_TEST_INT(state.m_RasterizerDescription.m_iDepthBias, -7);
+    XII_TEST_FLOAT(state.m_RasterizerDescription.m_fDepthBiasClamp, 1.25f, 0.0001f);
+    XII_TEST_FLOAT(state.m_RasterizerDescription.m_fSlopeScaledDepthBias, 2.5f, 0.0001f);
+    XII_TEST_BOOL(state.m_DepthStencilDescription.m_bDepthEnable);
+    XII_TEST_BOOL(!state.m_DepthStencilDescription.m_bDepthWriteEnable);
+    XII_TEST_BOOL(state.m_DepthStencilDescription.m_ComparisonDepthFunction == xiiGALComparisonFunction::GreaterEqual);
+    XII_TEST_BOOL(state.m_DepthStencilDescription.m_bStencilEnable);
+    XII_TEST_INT(state.m_DepthStencilDescription.m_uiStencilReadMask, 63U);
+    XII_TEST_INT(state.m_DepthStencilDescription.m_uiStencilWriteMask, 31U);
+    XII_TEST_BOOL(state.m_DepthStencilDescription.m_FrontFace.m_StencilPassOperation == xiiGALStencilOperation::Replace);
+
+    xiiDefaultMemoryStreamStorage storage;
+    xiiMemoryStreamWriter         writer(&storage);
+    state.Save(writer);
+
+    xiiGALShaderStateResourceDescriptor loadedState;
+    xiiMemoryStreamReader                reader(&storage);
+    loadedState.Load(reader);
+    XII_TEST_BOOL(loadedState.m_BlendDescription == state.m_BlendDescription);
+    XII_TEST_BOOL(loadedState.m_RasterizerDescription == state.m_RasterizerDescription);
+    XII_TEST_BOOL(loadedState.m_DepthStencilDescription == state.m_DepthStencilDescription);
+    XII_TEST_INT(loadedState.CalculateHash(), state.CalculateHash());
+  }
+
+  XII_TEST_BLOCK(xiiTestBlock::Enabled, "Permutation binary round trip")
+  {
+    xiiGALShaderPermutationBinary source;
+    source.m_ShaderStageHashes.Insert(xiiGALShaderType::Vertex, 0x12345678U);
+    source.m_ShaderStageHashes.Insert(xiiGALShaderType::Pixel, 0x9ABCDEF0U);
+    source.m_StateDescriptor.m_RasterizerDescription.m_CullMode = xiiGALCullMode::Front;
+    source.m_StateDescriptor.m_DepthStencilDescription.m_bDepthEnable = true;
+
+    xiiGALPermutationVariable& quality = source.m_PermutationVariables.ExpandAndGetRef();
+    quality.m_sName.Assign("QUALITY");
+    quality.m_sValue.Assign("QUALITY_HIGH");
+    xiiGALPermutationVariable& skinned = source.m_PermutationVariables.ExpandAndGetRef();
+    skinned.m_sName.Assign("SKINNED");
+    skinned.m_sValue.Assign("TRUE");
+
+    xiiDefaultMemoryStreamStorage storage;
+    xiiMemoryStreamWriter         writer(&storage);
+    XII_TEST_BOOL(source.Write(writer).Succeeded());
+    XII_TEST_BOOL(storage.GetStorageSize64() > 0U);
+
+    xiiGALShaderPermutationBinary loaded;
+    xiiMemoryStreamReader         reader(&storage);
+    bool                          bOldVersion = true;
+    XII_TEST_BOOL(loaded.Read(reader, bOldVersion).Succeeded());
+    XII_TEST_BOOL(!bOldVersion);
+    XII_TEST_INT(loaded.m_ShaderStageHashes.GetCount(), 2U);
+    XII_TEST_INT(loaded.m_ShaderStageHashes.Find(xiiGALShaderType::Vertex).Value(), 0x12345678U);
+    XII_TEST_INT(loaded.m_ShaderStageHashes.Find(xiiGALShaderType::Pixel).Value(), 0x9ABCDEF0U);
+    XII_TEST_BOOL(loaded.m_StateDescriptor.m_RasterizerDescription == source.m_StateDescriptor.m_RasterizerDescription);
+    XII_TEST_BOOL(loaded.m_StateDescriptor.m_DepthStencilDescription == source.m_StateDescriptor.m_DepthStencilDescription);
+    XII_TEST_INT(loaded.m_PermutationVariables.GetCount(), 2U);
+    XII_TEST_STRING(loaded.m_PermutationVariables[0].m_sName.GetView(), "QUALITY");
+    XII_TEST_STRING(loaded.m_PermutationVariables[0].m_sValue.GetView(), "QUALITY_HIGH");
+    XII_TEST_STRING(loaded.m_PermutationVariables[1].m_sName.GetView(), "SKINNED");
+    XII_TEST_STRING(loaded.m_PermutationVariables[1].m_sValue.GetView(), "TRUE");
+
   }
 }
